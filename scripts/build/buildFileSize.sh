f8585cc4880b2ffa8eb53fc80df18e6eb5d7c930
chgibb <chris.gibb@outlook.com>
2016-10-12 09:59:39 -0400

    Added logging to build scripts.

--- scripts/build/buildFileSize.sh
+++ scripts/build/buildFileSize.sh
@@ -1,5 +1,7 @@
+printf "Building fileSize\n"
 g++ -Wall -fexceptions -fexpensive-optimizations -O3  -c src/fileSize/main.cpp -o main.o
 g++  -o fileSize main.o  -s
+printf "Bundling fileSize\n"
 rm main.o
 cp fileSize dist
 rm fileSize  
--- scripts/build/buildQCReportCopy.sh
+++ scripts/build/buildQCReportCopy.sh
@@ -1,5 +1,7 @@
+printf "Building QCReportCopy\n"
 dmd src/QCReportCopy/main.d src/QCReportCopy/qcartifacts.d -O -release -inline -ofQCReportCopy
 
+printf "Bundling QCReportCopy\n"
 cp QCReportCopy dist
 
 rm QCReportCopy
