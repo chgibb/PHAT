714f9d19ce8288b74e74241d4676c6f6af629678
chgibb <chris.gibb@outlook.com>
2016-10-14 11:35:11 -0400

    Added logging.

--- src/guiTests/input.js
+++ src/guiTests/input.js
@@ -1,3 +1,4 @@
+console.log("started GUI test for input");
 const electron = require('electron');
 const ipc = electron.ipcMain;
 const app = electron.app;
@@ -15,6 +16,7 @@ console.log(BrowserWindow.getAllWindows());
 
 console.log(window.windows["toolBar"]);
 window.windows["toolBar"].webContents.executeJavaScript("document.getElementById('input').click()");
+console.log("Done");
 app.quit();
 
 },2000);
