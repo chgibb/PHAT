f2bc3a8542a3c38b757e0f899b510df83ba00a4a
chgibb <chris.gibb@outlook.com>
2016-10-17 12:57:18 -0400

    Fixed typo

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,6 +2,6 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get install xvfb
   export DISPLAY=:99.0
-  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+  xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
   sleep 3
 fi
