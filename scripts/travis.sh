e84e57f6ae19ae2a981345528b12e53c5da2da36
chgibb <chris.gibb@outlook.com>
2016-10-17 11:42:42 -0400

    Update travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  sudo pat-get install xvfb
   export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
+  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
   sleep 3
 fi
