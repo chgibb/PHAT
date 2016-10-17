5fa94d1ddd186229fde12103e5fb8fe5552985f6
chgibb <chris.gibb@outlook.com>
2016-10-17 13:12:17 -0400

    Remove travis.sh

--- scripts/travis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  sudo apt-get install xvfb
-  export DISPLAY=:99.0
-  xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-  sleep 3
-fi
