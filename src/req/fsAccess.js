42f72cd97a860b55683e2cae3618d88e096c3563
chgibb <chris.gibb@outlook.com>
2016-11-28 13:06:29 -0500

    Change paths to resources in Input, QC and Toolbar. Fix typo in fsAccess

--- src/req/fsAccess.js
+++ src/req/fsAccess.js
@@ -1,14 +1,14 @@
 var url = require('url')
 var path = require('path');
 
-module.exports = function(path)
+module.exports = function(filePath)
 {
     return url.format
     (
         {
             protocol: 'file',
             slashes: true,
-            pathname: path.join(process.cwd(), html)
+            pathname: path.join(process.cwd(), filePath)
         }
     );
 }
