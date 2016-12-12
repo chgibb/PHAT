622cb55a3bd72e73d2c944d8910b8201c9507190
chgibb <chris.gibb@outlook.com>
2016-12-11 19:23:58 -0500

    rename import to viewMgr

--- src/PathogenRenderer.js
+++ src/PathogenRenderer.js
@@ -1,7 +1,7 @@
 const ipc = require('electron').ipcRenderer;
 
 var id = require("./req/renderer/MakeValidID");
-var view = require('./req/renderer/view');
+var viewMgr = require('./req/renderer/view');
 
 var views = new Array();
 var currView = "report";
