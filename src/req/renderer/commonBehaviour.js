34bbf77904bd5665c501f5c74f09fe4d518bfa88
chgibb <chris.gibb@outlook.com>
2016-12-20 12:11:44 -0500

    Add error handling to commonBehaviour module

--- src/req/renderer/commonBehaviour.js
+++ src/req/renderer/commonBehaviour.js
@@ -10,3 +10,19 @@ document.addEventListener
         }
     }
 );
+
+/*
+ Adapted from answer by Fizer Khan
+ http://stackoverflow.com/questions/205688/javascript-exception-handling
+ and
+ user1398498
+ http://stackoverflow.com/questions/2604976/javascript-how-to-display-script-errors-in-a-popup-alert
+*/
+window.onerror = function(message,file,line,col,error)
+{
+    if(message)
+    {
+        alert("Error:\n\t" + message + "\nLine:\n\t" + line + "\nFile:\n\t" + file);
+    }
+    remote.getCurrentWindow().toggleDevTools();
+}
