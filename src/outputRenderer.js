7e6ffbe78704641f4f3805632625b80b3a4e9763
chgibb <chris.gibb@outlook.com>
2016-12-21 13:16:29 -0500

    Begin output window skeleton

---
+++ src/outputRenderer.js
@@ -0,0 +1,12 @@
+const ipc = require("electron").ipcRenderer;
+window.$ = window.jQuery = require('./req/renderer/jquery-2.2.4.js');
+            
+var id = require("./req/renderer/MakeValidID");
+var viewMgr = require("./req/renderer/viewMgr");
+
+$
+(
+    function()
+    {
+    }
+);
