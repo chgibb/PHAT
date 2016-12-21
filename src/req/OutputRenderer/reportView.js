19301f4d022d92f1686f2be334ccc700d1ae7049
chgibb <chris.gibb@outlook.com>
2016-12-21 13:40:44 -0500

    Create filter variables

--- src/req/OutputRenderer/reportView.js
+++ src/req/OutputRenderer/reportView.js
@@ -1,5 +1,15 @@
 var viewMgr = require('./../viewMgr');
 
+module.exports.alias = false;
+module.exports.fullName = false;
+module.exports.sizeInBytes = false;
+module.exports.formattedSize = false;
+module.exports.numberOfSequences = false;
+module.exports.PBSQ = false;
+module.exports.PSQS = false;
+module.exports.SGCC = false;
+module.exports.SDL = false;
+module.exports.ORS = false;
 module.exports.addView = function(arr,div,models)
 {
     arr.push
@@ -20,6 +30,9 @@ module.exports.addView = function(arr,div,models)
                     "<table style='width:100%'>",
                     "<tr>"
                 );
+
+                html.push("</table>");
+                return html.join('');
             }
             postRender(){}
             dataChanged(){}
