8ed93f7e4bc6cdeb09fcc48159f4c19496f132aa
chgibb <chris.gibb@outlook.com>
2016-12-21 14:07:01 -0500

    Add column names

--- src/req/OutputRenderer/reportView.js
+++ src/req/OutputRenderer/reportView.js
@@ -30,7 +30,27 @@ module.exports.addView = function(arr,div,models)
                     "<table style='width:100%'>",
                     "<tr>"
                 );
-
+                if(module.exports.alias)
+                    html.push("<th>","Alias","</th>");
+                if(module.exports.fullName)
+                    html.push("<th>","Directory","</th>");
+                if(module.exports.sizeInBytes)
+                    html.push("<th>","Size In Bytes","</th>");
+                if(module.exports.formattedSize)
+                    html.push("<th>","Formatted Size","</th>");
+                if(module.exports.numberOfSequences)
+                    html.push("<th>","Number of Sequences","</th>");
+                if(module.exports.PBSQ)
+                    html.push("<th>","Per Base Sequence Quality","</th>");
+                if(module.exports.PSQS)
+                    html.push("<th>","Per Sequence Quality Score","</th>");
+                if(module.exports.PSGCC)
+                    html.push("<th>","Per Sequence GC Content","</th>");
+                if(module.exports.SDL)
+                    html.push("<th>","Sequence Duplication Levels","</th>");
+                if(module.exports.ORS)
+                    html.push("<th>","Over Represented Sequences","</th>");
+                html.push("</tr>");
                 html.push("</table>");
                 return html.join('');
             }
