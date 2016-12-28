4ea714dbaae80b48d927a074fbe5198c8b4e0f37
chgibb <chris.gibb@outlook.com>
2016-12-28 11:46:39 -0500

    Prototype html tokenizer

--- HTMLparseTest.js
+++ HTMLparseTest.js
@@ -14,3 +14,32 @@ let html = `<table style="width:100%">
                     <tr><td>L6R12.R1.fastq</td><td>/home/gibbsticks/fastq/L6R12.R1.fastq</td><td>1018672</td><td>1.02mb</td></tr><tr><td>L6R11.R2.fastq</td><td>/home/gibbsticks/fastq/L6R11.R2.fastq</td><td>1253430</td><td>1.25mb</td></tr><tr><td>L6R12.R2.fastq</td><td>/home/gibbsticks/fastq/L6R12.R2.fastq</td><td>1018672</td><td>1.02mb</td></tr>
                     </tbody></table>`;
 
+function tokenizeHTMLString(html)
+{
+    let res = new Array();
+    let breakChars = ["<",">"];
+    let add = true;
+    let str = "";
+    for(let i = 0; i != html.length; ++i)
+    {
+        add = true;
+        if(html[i] == "<")
+        {
+            if(str != "")
+                res.push(str);
+            str = "";
+        }
+        if(html[i] == ">")
+        {
+            str += html[i]
+            if(str != "")
+                res.push(str);
+            str = "";
+            add = false;
+        }
+        if(add)
+            str += html[i];
+    }
+    return res;
+}
+console.log(tokenizeHTMLString(html));
