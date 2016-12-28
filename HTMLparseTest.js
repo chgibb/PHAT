0b4b73d72e08f06421c44065d87f39c5a67d4410
chgibb <chris.gibb@outlook.com>
2016-12-28 11:36:14 -0500

    Create test script to test html tokenization

---
+++ HTMLparseTest.js
@@ -0,0 +1,16 @@
+let html = `<table style="width:100%">
+                    <tbody><tr>
+                        <th>Alias</th>
+                        <th>Directory</th>
+                        <th>Size In Bytes</th>
+                        <th>Formatted Size</th>
+                        
+                        
+                        
+                        
+                        
+                        
+                    </tr>
+                    <tr><td>L6R12.R1.fastq</td><td>/home/gibbsticks/fastq/L6R12.R1.fastq</td><td>1018672</td><td>1.02mb</td></tr><tr><td>L6R11.R2.fastq</td><td>/home/gibbsticks/fastq/L6R11.R2.fastq</td><td>1253430</td><td>1.25mb</td></tr><tr><td>L6R12.R2.fastq</td><td>/home/gibbsticks/fastq/L6R12.R2.fastq</td><td>1018672</td><td>1.02mb</td></tr>
+                    </tbody></table>`;
+
