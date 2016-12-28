3f77035573862521983c7f3bbca3f4de47af2f92
chgibb <chris.gibb@outlook.com>
2016-12-28 12:24:04 -0500

    Break off prototyping functions into modules

---
+++ src/req/renderer/tokenizeHTMLString.js
@@ -0,0 +1,28 @@
+module.exports = function (html)
+{
+    let res = new Array();
+    let breakChars = ["<",">"];
+    let add = true;
+    let str = "";
+    for(let i = 0; i != html.length; ++i)
+    {
+        add = true;
+        if(html[i] == "<")
+        {
+            if(str != "")
+                res.push(str);
+            str = "";
+        }
+        if(html[i] == ">")
+        {
+            str += html[i]
+            if(str != "")
+                res.push(str);
+            str = "";
+            add = false;
+        }
+        if(add)
+            str += html[i];
+    }
+    return res;
+}
