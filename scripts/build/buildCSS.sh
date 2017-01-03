e1cde2de9b20192cb630f792346a7059ba0dd1b5
chgibb <chris.gibb@outlook.com>
2017-01-03 12:30:54 -0500

    Minimal shared scss and scss builder with test implementation

--- scripts/build/buildCSS.sh
+++ scripts/build/buildCSS.sh
@@ -1,1 +1,8 @@
-cp src/styles/* dist/styles
+for f in src/styles/*.scss
+do
+	echo "Building "$f
+	changeExtension=$(echo $f | awk '{gsub("scss","css");print}')
+	./sassc $f > $changeExtension
+done
+cp src/styles/*.css dist/styles
+rm src/styles/*.css
