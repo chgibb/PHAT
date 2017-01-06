e0b5ac9b7448c8959229b67d37f451ed54e479a8
chgibb <chris.gibb@outlook.com>
2017-01-06 16:15:30 -0500

    Extremely early prototype markup wrapper for angularPlasmid. Not feature complete.

--- angularPlasmidTest.js
+++ angularPlasmidTest.js
@@ -14,4 +14,46 @@ genomeWriter.on
         console.log(genomeWriter.contigs);
     }
 )
-genomeWriter.beginRefStream("/home/gibbsticks/refs/3HumanGenomic_Loci.fasta");
+//genomeWriter.beginRefStream("/home/gibbsticks/refs/3HumanGenomic_Loci.fasta");
+
+let plasmidTrack = require("./circularGenome/plasmidTrack");
+let plasmid = require("./circularGenome/plasmid");
+let trackLabel = require("./circularGenome/trackLabel");
+console.log
+(
+    plasmid.add
+    (
+        {
+            sequenceLength : "360",
+            plasmidHeight : "375",
+            plasmidWidth  :"375"
+        }
+    )+
+    plasmidTrack.add
+    (
+        {
+            trackStyle : "fill:#f0f0f0;stroke:#ccc",
+            radius : "120"
+        }
+    )+
+    trackLabel.add
+    (
+        {
+            text : "HSP70",
+            labelStyle : "font-size:20px;font-weight:400"
+        }
+    )+
+        
+    trackLabel.end()+
+    plasmidTrack.end()+
+    plasmidTrack.add
+    (
+        {
+            trackStyle : "fill:rgba(225,225,225,0.5)",
+            radius : "110"
+        }
+    )+
+    plasmidTrack.end()+
+
+    plasmid.end()
+);
---
+++ circularGenome/trackScale.js
@@ -0,0 +1,55 @@
+module.exports.add = function(options)
+{
+    let res = `
+        <trackscale ${
+            (
+                ()=>
+                {
+                    if(options && options.interval)
+                        return `interval="${options.interval}"`;
+                    return "";
+                })()} ${
+                    (
+                        ()=>
+                        {
+                            if(options && options.style)
+                                return `style="${options.style}"`;
+                            return "";
+                        })()} ${
+                        (
+                        ()=>
+                        {
+                            if(options && options.direction)
+                                return `direction="${options.direction}"`;
+                            return "";
+                        })()} ${
+                        (
+                        ()=>
+                        {
+                            if(options && options.tickSize)
+                                return `ticksize="${options.tickSize}"`;
+                            return "";
+                        })()} ${
+                        (
+                        ()=>
+                        {
+                            if(options && options.showlabels)
+                                return `showlabels="${options.showlabels}"`;
+                            return "";
+                        })()} ${
+                        (
+                        ()=>
+                        {
+                            if(options && options.labelStyle)
+                                return `labelstyle="${options.labelStyle}"`;
+                            return "";
+                        })()}>
+            
+    `;
+    return res;
+}
+
+module.exports.end = function()
+{
+    return `</trackscale>`;
+}
