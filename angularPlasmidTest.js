e7c4a6522b078e43de3bad8ba2f3200071c75dc0
chgibb <chris.gibb@outlook.com>
2017-01-06 14:11:59 -0500

    Move genomeWriter into its own module. Prototype event based callbacks

--- angularPlasmidTest.js
+++ angularPlasmidTest.js
@@ -1,54 +1,17 @@
-let fs = require("fs");
-let refStream;
-class Contig
-{
-    constructor()
+let CircularGenomeWriter = require("./circularGenome/circularGenomeWriter");
+let genomeWriter = new CircularGenomeWriter();
+genomeWriter.on
+(
+    "loadedContig",function(contig)
     {
-        this.bp = 0;
-        this.contigName = 0;
+        console.log(contig.name);
     }
-}
-let contigs = new Array();
-let contigIndex = -1;
-function beginRefStream(path)
-{
-    refStream = fs.createReadStream(path,{encoding : "UTF8"});
-    refStream.on
-    (
-        "data",function(data)
-        {
-            for(let i = 0; i != data.length; ++i)
-            {
-                if(data[i] == ">")
-                {
-                    contigs.push(new Contig());
-                    ++contigIndex;
-                    ++i;
-                    while(data[i] != "\n")
-                    {
-                        contigs[contigIndex].contigName += data[i];
-                        ++i;
-                    }
-                    continue;
-                }
-                else if(data[i] == "a" ||
-                data[i] == "A" ||
-                data[i] == "t" ||
-                data[i] == "T" ||
-                data[i] == "c" ||
-                data[i] == "C" ||
-                data[i] == "g" ||
-                data[i] == "G")
-                    contigs[contigIndex].bp += 1;
-            }
-        }
-    )
-    refStream.on
-    (
-        "end",function()
-        {
-            console.log(JSON.stringify(contigs,undefined,4));
-        }
-    )
-}
-beginRefStream("/home/gibbsticks/refs/3HumanGenomic_Loci.fasta");
+)
+genomeWriter.on
+(
+    "doneLoadingContigs",function()
+    {
+        console.log(genomeWriter.contigs);
+    }
+)
+genomeWriter.beginRefStream("/home/gibbsticks/refs/3HumanGenomic_Loci.fasta");
