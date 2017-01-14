e02ff864bf67bb0f8d89bc1a7bfa28f43849ef65
chgibb <chris.gibb@outlook.com>
2017-01-14 10:47:24 -0500

    Add angularPlasmid on install

--- src/circularGenomeBuilderRenderer.js
+++ src/circularGenomeBuilderRenderer.js
@@ -3,6 +3,8 @@ var viewMgr = require('./req/renderer/viewMgr');
 
 require("./req/renderer/commonBehaviour");
 
+require('./angularplasmid.complete.min.js');
+
 window.$ = window.jQuery = require('./req/renderer/jquery-2.2.4.js');
 
 $
