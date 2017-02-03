a0dd15c645a210e81964f24fef084e88949cabf1
Chris Gibb <chris.gibb@outlook.com>
2017-02-03 10:55:19 -0500

    Annotate model class

--- src/req/renderer/model.js
+++ src/req/renderer/model.js
@@ -1,11 +1,31 @@
+/**
+ * Standardizes passing of handlers to data models
+ * @module res/renderer/model
+ */
 module.exports = class
 {
+    /**
+     * @param {string} channel - IPC channel to operate one
+     * @param {any} handlers - object containing handler functions
+     */
     constructor(channel,handlers)
     {
+        /**
+         * @prop {function(string,any)} postHandle - Function to pass IPC messages related to saving data through 
+         * @prop {function(string,any)} spawnHandle - Function to pass IPC messages related to spawning processes through
+         * @prop {function(string)} fsAccess - Function invoked on attempt to access the file system. Returns path to file.
+         * @prop {string} channel - Channel to use in IPC messages
+         */
         this.postHandle = handlers.postStateHandle;
         this.spawnHandle = handlers.spawnHandle;
         this.fsAccess = handlers.fsAccess;
         this.channel = channel;
     }
+    /**
+     * Method to handle replys from spawned processes
+     * @param {string} channel
+     * @param {any} arg
+     * @return {void}
+     */
     spawnReply(channel,arg){}
 }
