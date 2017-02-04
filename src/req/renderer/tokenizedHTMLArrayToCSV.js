f3cd4d96981db79347221863aba3b144655f76e9
Mitch Pynn <mitchell.pynn@gmail.com>
2017-02-04 12:48:11 -0500

    basic CSV implementation

---
+++ src/req/renderer/tokenizedHTMLArrayToCSV.js
@@ -0,0 +1,86 @@
+module.exports = function(html)
+{
+    let isTable = new RegExp("(<table)","i");
+    let isTableEnd = new RegExp("(</table>)","i");
+
+    let isTableRow = new RegExp("(<tr>)","i");
+    let isTableRowEnd = new RegExp("(</tr>)","i");
+
+    let isTableData = new RegExp("(<td>)","i");
+    let isTableDataEnd = new RegExp("(</td>)","i");
+
+    let isTableHeader = new RegExp("(<th>)","i");
+    let isTableHeaderEnd = new RegExp("(</th>)","i");
+
+    let isTBody = new RegExp("<tbody>","i");
+    let isTBodyEnd = new RegExp("</tbody>","i");
+
+    let hasNewLine = new RegExp("\\n","i");
+    let csv = `${(()=>{
+        let res = "";
+        let inCell = false;
+        for(let i = 0; i != html.length; ++i)
+        {
+            if(isTBody.test(html[i]) ||
+                isTBodyEnd.test(html[i] ||
+                    html[i] == "\n")
+            )
+                continue;
+            if(hasNewLine.test(html[i]))
+                continue;
+            if(isTable.test(html[i]))
+            {
+                //res += `<Table>`;
+                continue;
+            }
+            else if(isTableEnd.test(html[i]))
+            {
+                //res += `</Table>`;
+                continue;
+            }
+            else if(isTableRow.test(html[i]))
+            {
+                //res += `<Row>`;
+                continue;
+            }
+            else if(isTableRowEnd.test(html[i]))
+            {
+                //res += `</Row>`;
+                res += '\n';
+                continue;
+            }
+            else if(isTableData.test(html[i]))
+            {
+                //res += `<Cell>`;
+                inCell = true;
+                continue;
+            }
+            else if(isTableDataEnd.test(html[i]))
+            {
+                //res += `</Data></Cell>`;
+                inCell = false;
+                continue;
+            }
+            else if(isTableHeader.test(html[i]))
+            {
+                //res += `<Cell>`;
+                inCell = true;
+                continue;
+            }
+            else if(isTableHeaderEnd.test(html[i]))
+            {
+                //res += `</Data></Cell>`;
+                inCell = false;
+                continue;
+            }
+            else if(html[i] && i != 0 && inCell)
+            {
+                res += `${html[i]},`;
+                continue;
+            }
+        }
+        return res;
+    })()}
+    `;
+    return csv;
+}
