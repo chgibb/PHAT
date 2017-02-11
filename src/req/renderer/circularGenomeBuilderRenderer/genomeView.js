d6d516d31f91b85ca2043fea13089010b7d959e1
Chris Gibb <chris.gibb@outlook.com>
2017-02-11 14:16:59 -0500

    Changed formatting

--- src/req/renderer/circularGenomeBuilderRenderer/genomeView.js
+++ src/req/renderer/circularGenomeBuilderRenderer/genomeView.js
@@ -11,12 +11,12 @@ require("angular");
 require("angularplasmid");
 //adapted from answer by letronje and edited by Peter Mortensen
 //http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
-function getRandColor(brightness){
-
+function getRandColor(brightness)
+{
     // Six levels of brightness from 0 to 5, 0 being the darkest
     var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
     var mix = [brightness*51, brightness*51, brightness*51]; //51 => 255/5
-    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x){ return Math.round(x/2.0)})
+    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x){return Math.round(x/2.0)});
     return "rgb(" + mixedrgb.join(",") + ")";
 }
 let app = angular.module('myApp',['angularplasmid']);
