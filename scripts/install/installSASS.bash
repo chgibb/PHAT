bb2445f16d9e2b32c6fbabfce8a4069f86111bca
chgibb <chris.gibb@outlook.com>
2017-02-18 16:56:04 -0500

    Added conditions for linux vs windows sass compiler installation

--- scripts/install/installSASS.bash
+++ scripts/install/installSASS.bash
@@ -1,4 +1,9 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 git clone https://github.com/eSlider/sassc-binaries
-cp sassc-binaries/dist/sassc .
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    cp sassc-binaries/dist/sassc .
+fi
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    cp sassc-binaries/dist/sassc.exe .
+fi
 rm -rf sassc-binaries
