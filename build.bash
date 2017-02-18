cf20ab47a9f1d811324032167d3d581b4276dea7
chgibb <chris.gibb@outlook.com>
2017-02-18 16:49:58 -0500

    Added bash support for build scrips

--- build.bash
+++ build.bash
@@ -21,6 +21,10 @@ for f in scripts/build/*.sh
 do
 	sh $f
 done
+for f in scripts/build/*.bash
+do
+	bash $f
+done
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	./node_modules/.bin/electron-packager ./dist/ --platform linux --arch x64 --overwrite --ignore=node_modules --ignore=.jsx --ignore=build.sh --ignore=src --ignore=vcs  --ignore=.sh --ignore=notes --ignore=manuscript --ignore=presentation
