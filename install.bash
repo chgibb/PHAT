9f6b2239acd374a9655ae3db676beadf129c65eb
Chris Gibb <chris.gibb@outlook.com>
2017-02-21 11:17:00 -0500

    Added comments

--- install.bash
+++ install.bash
@@ -2,9 +2,10 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 #set -e
 
+#install all deb packages under scripts/install
 sudo dpkg -R -i scripts/install
 
-
+#install the following
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -y
 sudo apt-get install libstdc++6 -y
@@ -12,13 +13,16 @@ sudo apt-get install gcc -y
 sudo apt-get install g++ -y
 sudo apt-get install ant
 
+#remove and remake dist
 rm -rf dist
 
 mkdir dist
 mkdir dist/styles
 
+#install npm dependencies
 npm install
 
+#for each script in scripts/install, run it
 for f in scripts/install/*.sh
 do
 	sh $f
@@ -29,5 +33,5 @@ do
 	bash $f
 done
 
-
+#run packaging script
 bash package.bash
