bcd20689067bf9db9126c3267b095f31af06296b
Chris Gibb <chris.gibb@outlook.com>
2017-02-27 14:58:20 -0500

    Export CircularFigure class

--- src/req/renderer/circularGenomeMgr.js
+++ src/req/renderer/circularGenomeMgr.js
@@ -3,10 +3,11 @@ let model = require("./model");
 let canRead = require("./canRead");
 let FastaContigLoader = require("./circularGenome/fastaContigLoader");
 let fasta = require("./fasta");
-class circularFigure
+module.exports.circularFigure = class
 {
-    constructor(contigs)
+    constructor(contigs,name)
     {
+        this.name = name;
         this.contigs = contigs;
         this.radius = 300;
     }
@@ -32,7 +33,7 @@ class RunningLoader
         this.loader.beginRefStream(fasta.name);
     }
 }
-module.exports = class extends model
+module.exports.Mgr = class extends model
 {
     constructor(channel,handlers)
     {
