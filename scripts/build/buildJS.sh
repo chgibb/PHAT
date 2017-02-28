b8d2aa077dfd666d02bfefa18990163f54f265f2
Chris Gibb <chris.gibb@outlook.com>
2017-02-27 19:42:46 -0500

    Proto ts artifact remover

--- scripts/build/buildJS.sh
+++ scripts/build/buildJS.sh
@@ -1,6 +1,7 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 cp src/pileup.js/style/pileup.css dist/styles/pileup.css
 
+./node_modules/.bin/tsc
 for f in src/*.js
 do
 	printf "Bundling "
@@ -8,4 +9,10 @@ do
 	printf "\n"
 	destination=$(echo $f | awk '{gsub("src/","dist/");print}')
 	./node_modules/.bin/browserify $f --node --debug -o $destination --ignore-missing
+done
+
+for f in src/**/*.js
+	artifact=$(echo $f | awk 'gsub(".ts",".js");print')
+	printf $artifact
+	printf "\n"
 done
