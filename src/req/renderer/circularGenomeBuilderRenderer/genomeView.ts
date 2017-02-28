d558aafd7bb2566b854c4edf97384dd52cb1d432
Chris Gibb <chris.gibb@outlook.com>
2017-02-28 18:36:06 -0500

    Ported file

--- src/req/renderer/circularGenomeBuilderRenderer/genomeView.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/genomeView.ts
@@ -1,5 +1,7 @@
+//// <reference path="jquery.d.ts" />
 import * as viewMgr from "./../viewMgr";
 import {Contig,FastaContigLoader} from "./../circularGenome/fastaContigLoader";
+import {DataModelMgr} from "./../model";
 let plasmidTrack = require("./../circularGenome/plasmidTrack");
 let plasmid = require("./../circularGenome/plasmid");
 let trackLabel = require("./../circularGenome/trackLabel");
@@ -7,11 +9,12 @@ let trackMarker = require("./../circularGenome/trackMarker");
 let markerLabel = require("./../circularGenome/markerLabel");
 let trackScale = require("./../circularGenome/trackScale");
 
+let angular : any;
 require("angular");
 require("angularplasmid");
 //adapted from answer by letronje and edited by Peter Mortensen
 //http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
-function getRandColor(brightness)
+function getRandColor(brightness : number)
 {
     // Six levels of brightness from 0 to 5, 0 being the darkest
     var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
@@ -22,113 +25,106 @@ function getRandColor(brightness)
 let app = angular.module('myApp',['angularplasmid']);
 export class GenomeView extends viewMgr.View
 {
-    public constructor()
+    public genome : any;
+    public constructor(name : string,div : string, model : DataModelMgr)
     {
-        
+        super(name,div,model);
     }
-}
-module.exports.addView = function(arr,div,models)
-{
-    arr.push
-    (
-        new class extends viewMgr.View
+    public onMount() : void{}
+    public onUnMount() : void{}
+    public renderView() : string
+    {
+        if(this.genome)
         {
-            constructor()
+            //Remove the div this view is bound to
+            document.body.removeChild(document.getElementById(this.div));
+            $("#"+this.div).remove();
+
+            let totalBP = 0;
+            for(let i = 0; i != this.genome.contigs.length; ++i)
             {
-                super("genomeView",div,models);
+                totalBP += this.genome.contigs[i].bp;
             }
-            onMount(){}
-            onUnMount(){}
-            renderView()
-            {    
-                if(this.genome)
-                {
-                    //Remove the div this view is bound to
-                    document.body.removeChild(document.getElementById(this.div));
-                    $("#"+this.div).remove();
-
-                    let totalBP = 0;
-                    for(let i = 0; i != this.genome.contigs.length; ++i)
+            //This is an unholy mess adapted from the example given inline in the
+            //angular source code https://github.com/angular/angular.js/blob/master/src/auto/injector.js
+            //We remove the div this view is bound to, recreate it and re render the angular template into it
+            //Then we pass the div into angular to compile the templates and then finally inject it all back into
+            //the page
+            let $div = $
+            (
+                `<div id="${this.div}">
+                    ${plasmid.add(
                     {
-                        totalBP += this.genome.contigs[i].bp;
-                    }
-                    //This is an unholy mess adapted from the example given inline in the
-                    //angular source code https://github.com/angular/angular.js/blob/master/src/auto/injector.js
-                    //We remove the div this view is bound to, recreate it and re render the angular template into it
-                    //Then we pass the div into angular to compile the templates and then finally inject it all back into
-                    //the page
-                    let $div = $
-                    (
-                        `<div id="${this.div}">
-                            ${plasmid.add(
-                            {
-                                sequenceLength : totalBP.toString(),
-                                plasmidHeight : "300",
-                                plasmidWidth : "300"
-                            })}
-                                ${plasmidTrack.add(
-                                {
-                                    trackStyle : "fill:#f0f0f0;stroke:#ccc",
-                                    radius : "120"
-                                })}
-                                    ${trackLabel.add(
-                                    {
-                                        text : this.genome.contigs[0].name,
-                                        labelStyle : "font-size:20px;font-weight:400"
-                                    })}
-                                    ${trackLabel.end()}
-                                    ${(()=>
-                                        {
-                                            let res = "";
-                                            let lastLocation = 0;
-                                            for(let i = 0; i != this.genome.contigs.length; ++i)
-                                            {
-                                                res += `
-                                                    ${trackMarker.add(
-                                                    {
-                                                        start : lastLocation.toString(),
-                                                        end : (lastLocation + this.genome.contigs[i].bp).toString(),
-                                                        markerStyle : "fill:"+getRandColor(1)
-                                                    })}
-                                                        ${markerLabel.add(
-                                                        {
-                                                            type : "path",
-                                                            text : this.genome.contigs[i].name
-                                                        })}
-                                                        ${markerLabel.end()}
-                                                    ${trackMarker.end()}
-                                                `;
-                                                lastLocation = lastLocation + this.genome.contigs[i].bp;
-                                            }
-                                            return res; 
-                                        })()}
-                                        ${trackScale.add(
-                                            {
-                                                interval : "100",
-                                                vAdjust : "5",
-                                                showLabels : "1"
-                                            }
-                                        )}
-                                        ${trackScale.end()}
-                                ${plasmidTrack.end()}
-                            ${plasmid.end()}
-                        </div>
-                    `);
-                    $(document.body).append($div);
-                    angular.element(document).injector().invoke
-                    (
-                        function($compile)
+                        sequenceLength : totalBP.toString(),
+                        plasmidHeight : "300",
+                        plasmidWidth : "300"
+                    })}
+                        ${plasmidTrack.add(
                         {
-                            let scope = angular.element($div).scope();
-                            $compile($div)(scope);
-                        }
-                    );
-
+                            trackStyle : "fill:#f0f0f0;stroke:#ccc",
+                            radius : "120"
+                        })}
+                            ${trackLabel.add(
+                            {
+                                text : this.genome.contigs[0].name,
+                                labelStyle : "font-size:20px;font-weight:400"
+                            })}
+                            ${trackLabel.end()}
+                            ${(()=>
+                            {
+                                let res = "";
+                                let lastLocation = 0;
+                                for(let i = 0; i != this.genome.contigs.length; ++i)
+                                {
+                                    res += `
+                                        ${trackMarker.add(
+                                        {
+                                            start : lastLocation.toString(),
+                                            end : (lastLocation + this.genome.contigs[i].bp).toString(),
+                                            markerStyle : "fill:"+getRandColor(1)
+                                        })}
+                                            ${markerLabel.add(
+                                            {
+                                                type : "path",
+                                                text : this.genome.contigs[i].name
+                                            })}
+                                            ${markerLabel.end()}
+                                        ${trackMarker.end()}
+                                    `;
+                                    lastLocation = lastLocation + this.genome.contigs[i].bp;
+                                }
+                                return res; 
+                            })()}
+                            ${trackScale.add(
+                            {
+                                interval : "100",
+                                vAdjust : "5",
+                                showLabels : "1"
+                            }
+                            )}
+                            ${trackScale.end()}
+                        ${plasmidTrack.end()}
+                    ${plasmid.end()}
+                </div>
+            `);
+            $(document.body).append($div);
+            angular.element(document).injector().invoke
+            (
+                function($compile : any)
+                {
+                    let scope = angular.element($div).scope();
+                    $compile($div)(scope);
                 }
-            }
-            postRender(){}
-            dataChanged(){}
-            divClickEvents(event){}
+            );
+
         }
-    );
+        return undefined;
+    }
+    public postRender() : void{}
+    public dataChanged() : void{}
+    public divClickEvents(event : JQueryEventObject) : void{}
+}
+export function addView(arr : Array<viewMgr.View>,div : string,model : DataModelMgr)
+{
+    arr.push(new GenomeView("genomeView",div,model));
 }
