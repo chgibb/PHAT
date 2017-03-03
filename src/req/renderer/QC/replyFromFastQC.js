802003531044b27498775e11b671c642b10af5e0
Chris Gibb <chris.gibb@outlook.com>
2017-03-03 15:39:46 -0500

    Rewrite path conversion with plat dependent index use

--- src/req/renderer/QC/replyFromFastQC.js
+++ src/req/renderer/QC/replyFromFastQC.js
@@ -8,7 +8,20 @@ module.exports = function(channel,arg,model)
     if(arg.done)
     {
 
+        let idx = -1;
+        if(process.platform == "linux")
+            idx = 0;
+        else if(process.platform == "win32")
+            idx = 1;
 
+        //trim the file name off of the full path
+        let trimmed = trimPath(arg.args[idx]);
+        //extract just the full path up to the  last slash
+        let remainder = arg.args[idx].substr(0,arg.args[idx].length-trimmed.length);
+        //convert from .fastq file ending to _fastqc directory name
+        trimmed = trimmed.replace(new RegExp('(.fastq)','g'),'_fastqc');
+
+        
         /*
         //trim the file name off of the full path
         var trimmed = trimPath(arg.args[0]);
