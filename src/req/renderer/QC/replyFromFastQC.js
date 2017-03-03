c7659d08084e16de2926268af45ce34a489835e6
Chris Gibb <chris.gibb@outlook.com>
2017-03-03 15:54:15 -0500

    Proto refactor with fse

--- src/req/renderer/QC/replyFromFastQC.js
+++ src/req/renderer/QC/replyFromFastQC.js
@@ -2,6 +2,7 @@ let fse = require('fs-extra');
 
 var id = require('./../MakeValidID.js');
 var trimPath = require('./../trimPath.js');
+var QCReportSummary = require('./QCReportSummary.js');
 module.exports = function(channel,arg,model)
 {
     
@@ -21,7 +22,56 @@ module.exports = function(channel,arg,model)
         //convert from .fastq file ending to _fastqc directory name
         trimmed = trimmed.replace(new RegExp('(.fastq)','g'),'_fastqc');
 
-        
+        let srcDir = remainder + trimmed;
+        let destDir = model.fsAccess('resources/app/rt/QCReports/'+id.makeValidID(arg.args[idx]));
+
+        fse.copy
+        (
+            srcDir+"/fastqc_report.html",destDir+"/fastqc_report.html",{overwrite : true},function(err)
+            {
+                if(err)
+                {
+                    model.runningReport = false;
+                    throw new Error("Could not move "+srcDir+"/fastqc_report.html");
+                }
+                fse.removeSync(srcDir+"/fastqc_report.html"); 
+                fse.copy
+                (
+                    srcDir+"/summary.txt",destDir+"/summary.txt",{overwrite : true},function(err)
+                    {
+                        if(err)
+                        {
+                            model.runningReport = false;
+                            throw new Error("Could not move "+srcDir+"/summary.txt");
+                        }
+                        fse.removeSync(srcDir+"/summary.txt");
+                        fse.copy
+                        (
+                            srcDir+"/fastqc_data.txt",destDir+"/fastqc_data.txt",{overwrite : true},function(err)
+                            {
+                                if(err)
+                                {
+                                    model.runningReport = false;
+                                    throw new Error("Could not move "+srcDir+"/fastqc_data.txt");
+                                }
+                                fse.removeSync(srcDir+"/fastqc_data.txt");
+                                for(let i = 0; i != model.QCData.length; ++i)
+                                {
+                                    if(model.QCData[i].name == srcDir)
+                                    {
+                                        model.QCData[i].QCReport = model.fsAccess("rt/QCReports/"+model.QCData[i].validID);
+                                        model.QCData[i].summary = QCReportSummary.getQCReportSummaries(destDir);
+                                        model.QCData[i].runningReport = false;
+                                        model.postQCData();
+                                        return;
+                                    }
+                                }
+                            }
+                        );    
+                    }
+                );
+            }
+        );
         /*
         //trim the file name off of the full path
         var trimmed = trimPath(arg.args[0]);
