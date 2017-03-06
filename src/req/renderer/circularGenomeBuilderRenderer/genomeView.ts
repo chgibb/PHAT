5f12d8fc9a3bb2aa0f2731f592b1f5c0ee54e072
Chris Gibb <chris.gibb@outlook.com>
2017-03-05 19:01:39 -0500

    Changed vars to lets

--- src/req/renderer/circularGenomeBuilderRenderer/genomeView.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/genomeView.ts
@@ -17,9 +17,9 @@ require("angularplasmid");
 function getRandColor(brightness : number)
 {
     // Six levels of brightness from 0 to 5, 0 being the darkest
-    var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-    var mix = [brightness*51, brightness*51, brightness*51]; //51 => 255/5
-    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x){return Math.round(x/2.0)});
+    let rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+    let mix = [brightness*51, brightness*51, brightness*51]; //51 => 255/5
+    let mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x){return Math.round(x/2.0)});
     return "rgb(" + mixedrgb.join(",") + ")";
 }
 let app : any = angular.module('myApp',['angularplasmid']);
