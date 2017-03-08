68ae6edd74584a6b932c90e83def8d1cc4d846d8
Chris Gibb <chris.gibb@outlook.com>
2017-03-08 14:42:50 -0500

    Proto ported file

--- src/req/main/Input.ts
+++ src/req/main/Input.ts
@@ -1,29 +1,36 @@
-const electron = require('electron');
+import * as electron from "electron";
 const ipc = electron.ipcMain;
-const app = electron.app;
-var window = require('./window');
-var fsAccess = require("./../fsAccess").default;
-var getState = require('./getState');
-var postState = require('./postState');
+import * as winMgr from "./winMgr";
+import fsAccess from "./../fsAccess";
 
 ipc.on
 (
 	"input" ,function(event,arg)
 	{
-		postState.postStateIPC("input",event,arg);
-		getState.getStateIPC("input",event,arg);
+		//postState.postStateIPC("input",event,arg);
+		//getState.getStateIPC("input",event,arg);
 	}
 );
-window.windowCreators["input"] = 
+winMgr.windowCreators["input"] = 
 {
 	Create : function() 
 	{
-		window.windows.push
+		/*window.windows.push
 		(
 			{
 				name : "input",
 				window : window.createWithDefault("Input","input",928,300,fsAccess("resources/app/Input.html"),false, false, 500, 150)
 			}
+		);*/
+		winMgr.pushWindow(
+			"input",
+			winMgr.createWithDefault(
+				"Input",
+				"input",
+				928,300,
+				fsAccess("resources/app/Input.html"),
+				false,false, 500, 150
+			)
 		);
 	}
 };
