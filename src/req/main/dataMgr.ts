0b8725e6d030a4af887e36b16a25c40406f41462
Chris Gibb <chris.gibb@outlook.com>
2017-03-08 09:28:12 -0500

    proto datamgr

---
+++ src/req/main/dataMgr.ts
@@ -0,0 +1,86 @@
+import * as fs from "fs";
+const jsonFile = require("jsonfile");
+
+let data : any = {};
+let dataPath : string = "";
+
+export function loadData(path : string) : void
+{
+    dataPath = path;
+    data = jsonFile.readFileSync(path);
+}
+
+export function saveData() : void
+{
+    try
+    {
+	    jsonFile.writeFileSync(dataPath,data,{spaces : 4});
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+
+export function getChannel(channel : string) : any | undefined
+{
+    try
+    {
+        return data[channel];
+    }
+    catch(err)
+    {
+        console.log(err);
+        return undefined;
+    }
+}
+export function createChannel(channel : string) : boolean
+{
+    try
+    {
+        data[channel] = {};
+        return true;
+    }
+    catch(err)
+    {
+        console.log(err);
+        return false;
+    }
+}
+export function getKey(channel : string,key : string) : any | undefined
+{
+    try
+    {
+        return data[channel][key];
+    }
+    catch(err)
+    {
+        console.log(err);
+        return undefined;
+    }
+}
+export function createKey(channel : string,key : string) : boolean
+{
+    try
+    {
+        if(!getChannel(channel))
+        {
+            createChannel(channel);
+        }
+        data[channel][key] = {};
+        return true;
+    }
+    catch(err)
+    {
+        console.log(err);
+        return false;
+    }
+}
+
+export function stringifyData(
+    replacer?: (key: string, value: any) => any,
+    space?: string | number
+) : string
+{
+    return JSON.stringify(data,replacer,space);
+}
