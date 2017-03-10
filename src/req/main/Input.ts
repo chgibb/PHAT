51234e2949a11c3a0e324da81698072ed0308640
Chris Gibb <chris.gibb@outlook.com>
2017-03-10 16:51:48 -0500

    Removed commented out code

--- src/req/main/Input.ts
+++ src/req/main/Input.ts
@@ -3,25 +3,10 @@ const ipc = electron.ipcMain;
 import * as winMgr from "./winMgr";
 import fsAccess from "./../fsAccess";
 
-ipc.on
-(
-	"input" ,function(event,arg)
-	{
-		//postState.postStateIPC("input",event,arg);
-		//getState.getStateIPC("input",event,arg);
-	}
-);
 winMgr.windowCreators["input"] = 
 {
 	Create : function() 
 	{
-		/*window.windows.push
-		(
-			{
-				name : "input",
-				window : window.createWithDefault("Input","input",928,300,fsAccess("resources/app/Input.html"),false, false, 500, 150)
-			}
-		);*/
 		winMgr.pushWindow(
 			"input",
 			winMgr.createWithDefault(
