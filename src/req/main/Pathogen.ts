ae580aebb88609afeeba334c04af01cfa333d03c
Mitch Pynn <mitchell.pynn@gmail.com>
2017-03-11 18:18:54 -0500

    TS adjustments for /src/req/main

--- src/req/main/Pathogen.ts
+++ src/req/main/Pathogen.ts
@@ -1,5 +1,3 @@
-import * as electron from "electron";
-const ipc = electron.ipcMain;
 import * as winMgr from "./winMgr";
 import fsAccess from "./../fsAccess";
 
@@ -8,13 +6,6 @@ winMgr.windowCreators["pathogen"] =
 {
 	Create : function() 
 	{
-		/*window.windows.push
-		(
-			{
-				name : "pathogen",
-				window : window.createWithDefault("Pathogen","pathogen",1000,800,fsAccess("resources/app/Pathogen.html"),false, false, 500, 300)
-			}
-		);*/
 		winMgr.pushWindow(
 			"pathogen",
 			winMgr.createWithDefault(
