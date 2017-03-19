c0fa9cb721469387b186d83abfff28e6218d145a
Chris Gibb <chris.gibb@outlook.com>
2017-03-19 16:45:03 -0400

    Removed old code

--- src/guiTests/input.ts
+++ src/guiTests/input.ts
@@ -1,38 +1,3 @@
-/*const electron = require('electron');
-const ipc = electron.ipcMain;
-const app = electron.app;
-const jsonFile = require('jsonfile');
-const BrowserWindow = electron.BrowserWindow;
-
-var assert = require("./../req/tests/assert");
-
-var window = require('./../req/main/window');
-
-require('./../req/main/main');
-
-console.log("Started test");
-assert.runningEvents += 1;
-
-
-
-assert.assert(function(){
-    for(let i = 0; i != window.windows.length; ++i)
-    {
-        if(window.windows[i].name == "input")
-            return true;
-    }
-    return false;
-},"Opened Input window",0);
-
-assert.runAsserts();
-
-setTimeout(function(){
-    window.windows["toolBar"].webContents.executeJavaScript("document.getElementById('input').click()");
-    setTimeout(function(){
-        assert.runningEvents -= 1;
-    },10000);
-},3000);
-*/
 console.log("started GUI test for input");
 import * as electron from "electron";
 const ipc = electron.ipcMain;
