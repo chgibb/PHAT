27a57a299095784ab2a26664e6171cbdef75a111
Chris Gibb <chris.gibb@outlook.com>
2017-03-20 09:12:50 -0400

    Ported file

--- src/req/renderer/bowTie2AlignmentReportParser.ts
+++ src/req/renderer/bowTie2AlignmentReportParser.ts
@@ -1,56 +1,37 @@
-/*
-Where report is a string of the full report text.
-options is of the form
+export interface Bowtie2Report
 {
-    reads : bool,
-    mates : bool,
-    overallAlignmentRate : bool
+    reads : number;
+    mates : number;
+    overallAlignmentRate : number;
 }
-Each true property will indicate that it is selected to be parsed from the report
-Will return an object of the form
+export function parseBowTie2AlignmentReport(report : string) : Bowtie2Report
 {
-    reads : int,
-    mates : int,
-    overallAlignmentRate : float
-}
-*/
-module.exports.parseBowTie2AlignmentReport = function(report)
-{
-    //console.log(report);
-   /* if(!options)
-        return;*/
-    //var res = options;
-
-    var res = {};
-
-    var tokens = report;
+    let res : Bowtie2Report;
+    let tokens : Array<string>;
 
     //There's a bug in JobIPC's unbuffering of Bowtie2's output.
     //It picks up an undefined value at the beginning of the buffer which gets coerced to a string and 
     //prefixed to the text. Trim it off before we parse the report.
     //<hack>
     if(report.match(new RegExp("(undefined)","g")))
-        tokens = tokens.substring(9,tokens.length);
+        report = report.substring(9,tokens.length);
     //</hack>
-    tokens = tokens.split(new RegExp("[ ]|[\n]|[\t]"));
-
-    //console.log(tokens);
+    tokens = report.split(new RegExp("[ ]|[\n]|[\t]"));
     
-    for(var i = 0; i != tokens.length; i++)
+    for(let i = 0; i != tokens.length; i++)
     {
-            if(tokens[i].match(new RegExp("(reads;)","g")))
-            {
-                res.reads = parseInt(tokens[i-1]);
-            }
-            if(tokens[i].match(new RegExp("(mates)","g")))
-            {
-                res.mates = parseInt(tokens[i-1]);
-            }
-            if(tokens[i].match(new RegExp("(overall)","g")))
-            {
-                res.overallAlignmentRate = parseFloat(tokens[i-1].substring(0,tokens[i-1].length-1));
-            }
+        if(tokens[i].match(new RegExp("(reads;)","g")))
+        {
+            res.reads = parseInt(tokens[i-1]);
+        }
+        if(tokens[i].match(new RegExp("(mates)","g")))
+        {
+            res.mates = parseInt(tokens[i-1]);
+        }
+        if(tokens[i].match(new RegExp("(overall)","g")))
+        {
+            res.overallAlignmentRate = parseFloat(tokens[i-1].substring(0,tokens[i-1].length-1));
+        }
     }
     return res;
-    
 }
