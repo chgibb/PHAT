5aba9ca3643412294d3ba98ab8eacdcfc9c9a694
chgibb <chris.gibb@outlook.com>
2017-03-20 11:18:29 -0400

    Attempt task for installer

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,14 @@
 module.exports = function(grunt)
 {
+    grunt.initConfig({
+        'create-windows-installer': {
+        x64: {
+        appDirectory: 'phat-win32-x64',
+        outputDirectory: 'phatinst',
+        authors: 'My App Inc.',
+        exe: 'phat.exe'
+    }
+    }
+});
+    grunt.loadNpmTasks('grunt-electron-installer');
 }
