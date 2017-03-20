6f1dd1419c792cc7776c022fa138374ea9f4294e
Chris Gibb <chris.gibb@outlook.com>
2017-03-20 09:24:34 -0400

    Ported file

--- src/req/renderer/Align/replyFromSamTools.ts
+++ src/req/renderer/Align/replyFromSamTools.ts
@@ -1,7 +1,10 @@
-var replyFromSamToolsView = require('./replyFromSamToolsView');
-var replyFromSamToolsSort = require('./replyFromSamToolsSort');
-var replyFromSamToolsIndex = require('./replyFromSamToolsIndex');
-module.exports = function(channel,arg,model)
+import {SpawnRequestParams} from "./../../JobIPC";
+import AlignMgr from "./../Align";
+
+import replyFromSamToolsView from "./replyFromSamToolsView";
+import replyFromSamToolsSort from "./replyFromSamToolsSort";
+import replyFromSamToolsIndex from "./replyFromSamToolsIndex";
+export default function replyFromSamTools(channel : string,arg : SpawnRequestParams,model : AlignMgr) : void
 {
     if(arg.args[0] == "view")
         replyFromSamToolsView(channel,arg,model);
