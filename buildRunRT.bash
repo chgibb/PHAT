af12411269d1a6cfa802ae16a45bed0a60491706
Mitch Pynn <mitchell.pynn@gmail.com>
2017-03-26 14:32:52 -0400

    Update buildRunFresh for cross platform

--- buildRunRT.bash
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
-
-#remove rt folder
-rm -rf phat-linux-x64/resources/app/rt
-
-#build then run
-bash build.bash
-sh run.sh
