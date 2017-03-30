0f16bc502121afa87edb28f1e751784b8944e851
Chris Gibb <chris.gibb@outlook.com>
2017-03-30 14:02:05 -0400

    Ported file

--- src/req/renderer/OutputRenderer/masterView.ts
+++ src/req/renderer/OutputRenderer/masterView.ts
@@ -1,116 +1,116 @@
-var viewMgr = require('./../viewMgr');
+import * as viewMgr from "./../viewMgr";
+import {DataModelMgr} from "./../model";
+import * as reportView from "./reportView";
 
-var addReportView = require("./reportView");
-
-var XLSExportDialog = require("./XLSExportDialog");
-var CSVExportDialog = require("./CSVExportDialog");
-module.exports.addView = function(arr,div,models)
+import XLSExportDialog from "./XLSExportDialog";
+import CSVExportDialog from "./CSVExportDialog";
+export class MasterView extends viewMgr.View
 {
-    arr.push
-    (
-        new class extends viewMgr.View
+    public views : Array<viewMgr.View>;
+    public firstRender : boolean;
+    public rightPanelOpen : boolean;
+    public constructor(div : string,model? : DataModelMgr)
+    {
+        super("masterReportView",div,model);
+        this.views = new Array<viewMgr.View>();
+        this.firstRender = true;
+        this.rightPanelOpen = false;
+    }
+    onMount()
+    { 
+        reportView.addView(this.views,"report");
+        for(let i = 0; i != this.views.length; ++i)
         {
-            constructor()
-            {
-                super("masterReportView",div,models);
-                this.views = new Array();
-                this.firstRender = true;
-                this.righPanelOpen = false;
-            }
-            onMount()
-            {
-                addReportView.addView(this.views,"report");
-                for(let i = 0; i != this.views.length; ++i)
-                {
-                    this.views[i].onMount();
-                }
-            }
-            onUnMount(){}
-            renderView()
-            {
-                if(this.firstRender)
-                {
-                    this.firstRender = false;
-                    return `
-                        <button id="optionsButton" class="optionsButton">Options</button>
+            this.views[i].onMount();
+        }
+    }
+    onUnMount(){}
+    renderView()
+    {
+        if(this.firstRender)
+        {
+            this.firstRender = false;
+            return `
+                <button id="optionsButton" class="optionsButton">Options</button>
                         
-                        <div id="rightSlideOutPanel" class="rightSlideOutPanel">
-                            <input type="checkbox" id="alias">Alias</input>
-                            <input type="checkbox" id="fullName">Full Path</input>
-                            <input type="checkbox" id="sizeInBytes">Size In Bytes</input>
-                            <br />
-                            <input type="checkbox" id="formattedSize">Formatted Size</input>
-                            <input type="checkbox" id="numberOfSequences">Number of Sequences</input>
-                            <br />
-                            <input type="checkbox" id="PBSQ">Per Base Sequence Quality</input>
-                            <br />
-                            <input type="checkbox" id="PSQS">Per Sequence Quality Score</input>
-                            <br />
-                            <input type="checkbox" id="PSGCC">Per Sequence GC Content</input>
-                            <br />
-                            <input type="checkbox" id="SDL">Sequence Duplication Levels</input>
-                            <br />
-                            <input type="checkbox" id="ORS">Over Represented Sequences</input>
-                            <br />
-                            <p>Export To:</p>
-			                <input type="radio" name="exportOptions" id="exportToXLS">Excel</input>
-			                <input type="radio" name="exportOptions" id="exportToCSV">CSV</input>
-			                <button id="exportData">Export</button>
-                        </div>
-                        <div id="report">
-                        </div>
-                    `;
-                }
-                for(let i = 0; i != this.views.length; ++i)
+                <div id="rightSlideOutPanel" class="rightSlideOutPanel">
+                    <input type="checkbox" id="alias">Alias</input>
+                    <input type="checkbox" id="fullName">Full Path</input>
+                    <input type="checkbox" id="sizeInBytes">Size In Bytes</input>
+                    <br />
+                    <input type="checkbox" id="formattedSize">Formatted Size</input>
+                    <input type="checkbox" id="numberOfSequences">Number of Sequences</input>
+                    <br />
+                    <input type="checkbox" id="PBSQ">Per Base Sequence Quality</input>
+                    <br />
+                    <input type="checkbox" id="PSQS">Per Sequence Quality Score</input>
+                    <br />
+                    <input type="checkbox" id="PSGCC">Per Sequence GC Content</input>
+                    <br />
+                    <input type="checkbox" id="SDL">Sequence Duplication Levels</input>
+                    <br />
+                    <input type="checkbox" id="ORS">Over Represented Sequences</input>
+                    <br />
+                    <p>Export To:</p>
+			        <input type="radio" name="exportOptions" id="exportToXLS">Excel</input>
+			        <input type="radio" name="exportOptions" id="exportToCSV">CSV</input>
+			        <button id="exportData">Export</button>
+                </div>
+                <div id="report">
+                </div>
+            `;
+        }
+        for(let i = 0; i != this.views.length; ++i)
+        {
+            this.views[i].render();
+        }
+        return undefined;
+    }
+    postRender(){}
+    dataChanged(){}
+    divClickEvents(event : JQueryEventObject)
+    {
+        if(!event || !event.target || !event.target.id)
+            return;
+        if(event.target.id == "optionsButton")
+        {
+            let me = this;
+            $("#rightSlideOutPanel").animate
+            (
                 {
-                    this.views[i].render();
-                }
-                
-                
-            }
-            postRender(){}
-            dataChanged(){}
-            divClickEvents(event)
-            {
-                if(!event || !event.target || !event.target.id)
-                    return;
-                if(event.target.id == "optionsButton")
-                {
-                    let me = this;
-                    $("#rightSlideOutPanel").animate
+                    "margin-right" : 
                     (
+                        function()
                         {
-                            "margin-right" : 
-                            (
-                                function()
-                                {
-                                    me.rightPanelOpen = !me.rightPanelOpen;
-                                    return (me.rightPanelOpen == true ? "+" : "-")+"=50%";
-                                }
-                            )()
+                            me.rightPanelOpen = !me.rightPanelOpen;
+                            return (me.rightPanelOpen == true ? "+" : "-")+"=50%";
                         }
-                    );
+                    )()
                 }
-                if(document.getElementById(event.target.id) && 
-                   document.getElementById(event.target.id).type == "checkbox")
-                {
-                     viewMgr.getViewByName("report",this.views)[event.target.id] = $("#"+event.target.id).is(":checked");
-                     viewMgr.render();
-                }
-                if(event.target.id == "exportData")
-                {
-                    if($("#exportToXLS").is(":checked"))
-                    {
-                        XLSExportDialog(viewMgr.getViewByName("report",this.views).renderView());
-                    }
+            );
+        }
+        if(document.getElementById(event.target.id) && 
+            (<HTMLInputElement>document.getElementById(event.target.id)).type == "checkbox")
+        {
+            (<any>viewMgr.getViewByName("report",this.views))[event.target.id] = $("#"+event.target.id).is(":checked");
+            viewMgr.render();
+        }
+        if(event.target.id == "exportData")
+        {
+            if($("#exportToXLS").is(":checked"))
+            {
+                XLSExportDialog(viewMgr.getViewByName("report",this.views).renderView());
+            }
 
-                    if($("#exportToCSV").is(":checked"))
-                    {
-                        CSVExportDialog(viewMgr.getViewByName("report", this.views).renderView());
-                    }
-
-                }
+            if($("#exportToCSV").is(":checked"))
+            {
+                CSVExportDialog(viewMgr.getViewByName("report", this.views).renderView());
             }
         }
-    );
+    }
+}
+
+export function addView(arr : Array<viewMgr.View>,div : string,model? : DataModelMgr) : void
+{
+    arr.push(new MasterView(div,model));
 }
