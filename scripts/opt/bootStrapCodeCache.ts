20bf7c3283e3d709fafb607059cef66492a9475e
Chris Gibb <chris.gibb@outlook.com>
2017-04-03 09:27:43 -0400

    Removed old code and added comments

--- scripts/opt/bootStrapCodeCache.ts
+++ scripts/opt/bootStrapCodeCache.ts
@@ -88,16 +88,27 @@ function compileCache(jsFile : string,cdata : string) : number
 		return 1;
 	}
 }
-
+/*
+    Trys to load cached code from cdata.
+    On failure, will try to compile jsFile and write it to cdata, then load from cdata.
+    On failure to compile jsFile, failure to write cdata or failure to load cdata,
+    falls back on calling require(jsModule)
+*/
 function bootStrapCodeCache(jsFile : string,jsModule : string,cdata : string) : void
 {
 	let cacheStatus : number = loadFromCache(jsFile,cdata);
+	//cdata exists, is valid and has been executed
 	if(cacheStatus == 0)
 		return;
+	//either the cache does not exist, or is invalid for some reason
 	if(cacheStatus == 1 || cacheStatus == 3)
 	{
+		//try to compile a new cache
 		let compilerStatus = compileCache(jsFile,cdata);
+		//try to load the new cache
 		let secondTry = loadFromCache(jsFile,cdata);
+		//if there was a failure in either operation then stop trying and just
+		//load the JS the old fashioned way
 		if(secondTry != 0 || compilerStatus != 0)
 		{
 			console.log("Falling back to require");
@@ -105,86 +116,10 @@ function bootStrapCodeCache(jsFile : string,jsModule : string,cdata : string) : 
 			return;
 		}
 	}
+	//the file specified by jsFile does not exist. This is fatal and will break all other operations. 
 	if(cacheStatus == 2)
 	{
 		throw new Error("Fatal error: Could not load file "+jsFile);
 	}
 }
-/*
-    Trys to load cached code from cdata.
-    On failure, will try to compile jsFile and write it to cdata, then load from cdata.
-    On failure to compile jsFile, failure to write cdata or failure to load cdata,
-    falls back on calling require(jsModule)
-*/
-/*
-function bootStrapCodeCache(jsFile,jsModule,cdata)
-{
-    let cache;
-	try
-	{
-	    cache = fs.readFileSync(cdata);
-		let compiledCode = new vm.Script(
-		fs.readFileSync(jsFile),{
-		    filename : jsFile,
-		    lineOffset : 0,
-		    displayErrors : true,
-		    cachedData : cache
-		});
-		if(!compiledCode.cachedDataRejected)
-		{
-		    console.log("Successfully loaded from "+cdata);
-			compiledCode.runInThisContext();
-		}
-		else
-		{
-		    console.log("Cached code "+cdata+" was rejected.");
-            console.log("Falling back to require");
-			require(jsModule);
-		}
-	}
-	catch(err)
-	{
-	    console.log("Could not load "+cdata);
-		let code = fs.readFileSync(jsFile);
-		let compiler = new vm.Script(
-		code,{
-		    filename : jsFile,
-			lineOffset : 0,
-			displayErrors : true,
-			produceCachedData : true
-		});
-		if(compiler.cachedDataProduced)
-		{
-		    cache = compiler.cachedData
-			console.log("Successfully compiled "+jsFile);
-			try
-			{
-			    fs.writeFileSync(cdata,cache);
-				bootStrapCode(jsFile,jsModule,cdata);
-			}
-			catch(err)
-			{
-			    console.log("Failed to write "+cdata);
-                try
-                {
-                    fs.mkdirSync("resources/app/cdata");
-                    fs.writeFileSync(cdata,cache);
-				    bootStrapCode(jsFile,jsModule,cdata);
-                }
-                catch(err)
-                {
-                    console.log("Failed to make cache directory");
-                    console.log("Falling back to require");
-                    require(jsModule);
-                }
-			}
-		}
-		else
-		{
-		    console.log("Could not compile "+jsFile);
-            console.log("Falling back to require");
-			require(jsModule);
-		}
-	}
-}*/
 module.exports = bootStrapCodeCache;
