252f1f74deeb1e17c2c14098f1547f014e4df1ac
Chris Gibb <chris.gibb@outlook.com>
2017-04-03 09:58:02 -0400

    Implemented AOT cache compilation step

--- optPackage.bash
+++ optPackage.bash
@@ -1,4 +1,29 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    target="phat-linux-x64/resources/app"
+fi
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    target="phat-win32-x64/resources/app"
+fi
+
 bash scripts/opt/collapseBundle.bash
 bash scripts/opt/optIIFE.bash
 bash scripts/opt/asi.bash
 bash scripts/opt/minify.bash
+
+cd "$target"
+
+mv main.js realMain.js
+
+mv AOTCompileCodeCaches.js main.js
+
+cd ../
+
+cd ../
+
+./phat
+
+cd resources
+
+cd app
+
+mv realMain.js main.js
