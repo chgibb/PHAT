9c637b535bcb7e48bd79aa755264cf3b37a76645
chgibb <chris.gibb@outlook.com>
2017-04-07 13:20:44 -0400

    Added fields

--- buildWinInstaller.js
+++ buildWinInstaller.js
@@ -4,7 +4,9 @@ resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './phat-win32-x64',
     outputDirectory: './installer64',
     authors: 'My App Inc.',
-    exe: 'phat.exe'
+    exe: 'phat.exe',
+    iconUrl: './icons/phat.ico',
+    setupIcon: './icons/phat.ico'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
