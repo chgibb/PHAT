0a1ca510ebd3c6dc109f46366586dec222587c5d
chgibb <chris.gibb@outlook.com>
2017-04-07 10:44:13 -0400

    Added description and disabled msi generation

--- buildWinInstaller.js
+++ buildWinInstaller.js
@@ -4,9 +4,11 @@ resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './phat-win32-x64',
     outputDirectory: './installer64',
     authors: 'My App Inc.',
+    description: "Pathogen Host Analysis Tool",
     exe: 'phat.exe',
-    iconUrl: './icons/phat.ico',
-    setupIcon: './icons/phat.ico'
+    noMsi: true
+   // iconUrl: './icons/phat.ico',
+    //setupIcon: './icons/phat.ico'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
