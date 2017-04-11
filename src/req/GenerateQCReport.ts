b4e5670525f2cafa21c10d7d6959897cd803d66f
Chris Gibb <chris.gibb@outlook.com>
2017-04-11 12:08:35 -0400

    Changed references to this to self

--- src/req/GenerateQCReport.ts
+++ src/req/GenerateQCReport.ts
@@ -104,9 +104,9 @@ export class GenerateQCReport extends atomic.AtomicOperation
 						{
 							try
 							{
-								fse.copySync(`${this.srcDir}/fastqc_report.html`,`${this.destDir}/fastqc_report.html`);
-								fse.copySync(`${this.srcDir}/summary.txt`,`${this.destDir}/fastqc_report.html`);
-								fse.copySync(`${this.srcDir}/fastqc_data.txt`,`${this.destDir}/fastqc_report.html`);
+								fse.copySync(`${self.srcDir}/fastqc_report.html`,`${self.destDir}/fastqc_report.html`);
+								fse.copySync(`${self.srcDir}/summary.txt`,`${self.destDir}/fastqc_report.html`);
+								fse.copySync(`${self.srcDir}/fastqc_data.txt`,`${self.destDir}/fastqc_report.html`);
 							}
 							catch(err)
 							{
