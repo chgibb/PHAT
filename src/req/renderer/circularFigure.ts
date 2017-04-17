d68df0abae45b5c7747240c9da88410a7338834b
Chris Gibb <chris.gibb@outlook.com>
2017-04-16 22:38:10 -0400

    Added uuid to class and fixed assignment

--- src/req/renderer/circularFigure.ts
+++ src/req/renderer/circularFigure.ts
@@ -1,7 +1,9 @@
+const uuidv1 : () => string = require("uuid/v1");
 import {Contig} from "./../fastaContigLoader";
 export class CircularFigure
 {
     public uuid : string;
+    public uuidFasta : string;
     public name : string;
     public contigs : Array<Contig>;
     public radius : number;
@@ -9,6 +11,8 @@ export class CircularFigure
     public width : number;
     constructor(name : string,uuid : string,contigs : Array<Contig>)
     {
+        this.uuidFasta = uuid;
+        this.uuid = uuidv1();
         this.name = name;
         this.contigs = contigs;
         this.radius = 120;
