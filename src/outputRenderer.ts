ea83ce9078c880804ba76824acceaa4befcff23e
Chris Gibb <chris.gibb@outlook.com>
2017-04-20 14:17:47 -0400

    Removed old refs to qcdata

--- src/outputRenderer.ts
+++ src/outputRenderer.ts
@@ -2,8 +2,6 @@ import * as electron from "electron";
 const ipc = electron.ipcRenderer;
 
 import {GetKeyEvent,KeySubEvent} from "./req/ipcEvents";
-import Fastq from "./req/renderer/fastq";
-import {QCData} from "./req/renderer/QCData";
 import * as viewMgr from "./req/renderer/viewMgr";
 
 import * as masterView from "./req/renderer/OutputRenderer/masterView";
@@ -29,8 +27,6 @@ $
                 {
                     if(arg.key == "fastqInputs" && arg.val !== undefined)
                         (<reportView.ReportView>viewMgr.getViewByName("report",(<masterView.MasterView>viewMgr.getViewByName("masterReportView")).views)).fastqInputs = arg.val;
-                    if(arg.key == "QCData" && arg.val !== undefined)
-                        (<reportView.ReportView>viewMgr.getViewByName("report",(<masterView.MasterView>viewMgr.getViewByName("masterReportView")).views)).QC.QCData = arg.val;
                 }
                 viewMgr.render();
             }
