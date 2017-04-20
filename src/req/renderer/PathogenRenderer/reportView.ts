c2088687ac554171e6020d772a98b0d70da9d066
chgibb <chris.gibb@outlook.com>
2017-04-20 13:46:50 -0400

    Removed commented out code

--- src/req/renderer/PathogenRenderer/reportView.ts
+++ src/req/renderer/PathogenRenderer/reportView.ts
@@ -19,56 +19,6 @@ export class ReportView extends viewMgr.View
     onUnMount(){}
     renderView()
     {
-        /*let html = new Array();
-        html.push
-        (
-            "<table style='width:100%;'>",
-            "<tr>",
-            "<th>Name</th>",
-            "<th>Reads</th>",
-            "<th>Mates</th>",
-            "<th>Overall Alignment Rate %</th>",
-            "<th>Date Ran</th>",
-            "</tr>"
-        );
-        for(let i = 0; i != this.aligns.length; ++i)
-        {
-            if(this.aligns[i].type == "path")
-            {/*
-                let sources = this.aligns[i].UUID.split(';');
-                let found : number = 0;
-                for(let k : number = 0; k != this.selectedFastqInputs.length; ++k)
-                {
-                    if(this.selectedFastqInputs[k].alias == sources[0] || this.selectedFastqInputs[k].alias == sources[1])
-                        found++;
-                    if(found == 2)
-                        break;
-                }
-                for(let k : number = 0; k != this.selectedFastaInputs.length; ++k)
-                {
-                    if(this.selectedFastaInputs[k].alias == sources[2])
-                    {
-                        found++;
-                        break;
-                    }
-                }
-                if(found == 3)
-                {
-                    html.push
-                    (
-                        "<tr>",
-                        "<td><p id='",this.aligns[i].UUID,"' >",this.aligns[i].alias,"</p></td>",
-                        "<td>",this.aligns[i].summary.reads,"</td>",
-                        "<td>",this.aligns[i].summary.mates,"</td>",
-                        "<td>",this.aligns[i].summary.overallAlignmentRate,"</td>",
-                        "<td>",this.aligns[i].dateStampString,"</td>",
-                        "</tr>"
-                    )
-                }
-            }
-        }
-        html.push("</table>");
-        return html.join('');*/
         return `
             <table style='width:100%'>
                 <tr>
