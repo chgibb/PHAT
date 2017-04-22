caea3fe8f23652f71b9bc60b83ba12f38380655e
Chris Gibb <chris.gibb@outlook.com>
2017-04-22 15:26:13 -0400

    Patched to work under vanilla Node

--- scripts/opt/bootStrapCodeCache.ts
+++ scripts/opt/bootStrapCodeCache.ts
@@ -3,6 +3,18 @@
 import * as vm from "vm";
 import * as fs from "fs";
 
+//This is probably not okay.
+//It however ensures that this module will function correctly if invoked to bootstrap some other
+//script transitively. I.e. node bootStrapper -> actualScript
+//where bootStrapper simply calls require("bootStrapCodeCache")("actualScript",etc)
+//vm.runInThisContext does NOT expose local variables/enclosing params. This actually binds 
+//the require param (from Node's module.wrapper) to the global scope of jsFile. This will probably blow up in my face
+//at some point. require is passed along just fine under Electron(in renderer mode) if this module is invoked from within <script> tags in html.
+//This leads me to believe Electron(in renderer mode) is therefore binding Node's real, global require to the process' global at startup.
+//This fix is only required when run under Node. Maybe an environment check should be made prior to actually doing this. 
+if(!(<any>global).require)
+	(<any>global).require = require;
+
 function loadFromCache(jsFile : string,cdata : string) : number
 {
 	let cache : Buffer;
