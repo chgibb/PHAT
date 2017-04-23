212b1204ec29f173de01236b387269efa858ef7c
Chris Gibb <chris.gibb@outlook.com>
2017-04-23 14:21:51 -0400

    proto file count reporting

--- src/req/operations/InstallUpdate.ts
+++ src/req/operations/InstallUpdate.ts
@@ -3,12 +3,41 @@ import * as cp from "child_process";
 import * as atomic from "./atomicOperations";
 export class InstallUpdate extends atomic.AtomicOperation
 {
+    public installUpdateJob : cp.ChildProcess;
+    public installUpdateFlags : atomic.CompletionFlags;
+    public filesInUpdate : number = 0;
     constructor()
     {
         super();
     }
-    public setData(data : any){}
+    public setData(data : any)
+    {
+        this.generatedArtifacts.push("phat-linux-x64.tar.gz");
+    }
     public run() : void
     {
+        let self = this;
+        try
+        {
+            this.installUpdateJob = cp.fork("resources/app/installUpdate.js");
+            this.installUpdateJob.on
+            (
+                "message",function(data : any)
+                {
+                    if(data.totalFiles)
+                    {
+                        self.filesInUpdate = data.totalFiles;
+                        self.update();
+                        self.setSuccess(self.flags);
+                    }
+                }
+            );
+
+        }
+        catch(err)
+        {
+            this.abortOperationWithMessage(err);
+            return;
+        }
     }
 }
