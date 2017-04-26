53504fb70d82c3840d60feb60330cb6d001b1ea4
Chris Gibb <chris.gibb@outlook.com>
2017-04-26 11:31:01 -0400

    Added circular figure dir

--- src/req/main/main.ts
+++ src/req/main/main.ts
@@ -81,6 +81,7 @@ app.on
 			fs.mkdirSync("resources/app/rt/QCReports");
 			fs.mkdirSync("resources/app/rt/indexes");
 			fs.mkdirSync("resources/app/rt/AlignmentArtifacts");
+			fs.mkdirSync("resources/app/rt/circularFigures");
 		}
 		catch(err){}
 
