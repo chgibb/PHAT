3deb24698e3503bdc4913b7a5994ea54e4fc246e
Chris Gibb <chris.gibb@outlook.com>
2017-04-28 14:06:50 -0400

    Render already rendered tracks

--- src/req/renderer/circularGenomeBuilderRenderer/rightPanel/selectCoverageTracks.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/rightPanel/selectCoverageTracks.ts
@@ -8,7 +8,7 @@ import * as cf from "./../../circularFigure";
 export class SelectCoverageTracks extends viewMgr.View
 {
     public genome : cf.CircularFigure;
-    public alignData : Array<alignData>;
+    public selectedAlignment : alignData;
     public constructor(name : string,div : string)
     {
         super(name,div);
@@ -17,7 +17,27 @@ export class SelectCoverageTracks extends viewMgr.View
     public onUnMount() : void{}
     public renderView() : string
     {
-        return " ";
+        let masterView = <masterView.View>viewMgr.getViewByName("masterView");
+        let genomeView = <GenomeView>viewMgr.getViewByName("genomeView",masterView.views);
+        let rightPanel = <RightPanel>viewMgr.getViewByName("rightPanel",masterView.views);
+        this.genome = genomeView.genome;
+        this.selectedAlignment = rightPanel.selectedAlignment;
+        return `
+            <h2>Available Tracks</h2>
+            ${(()=>{
+                let res = "";
+                for(let i = 0; i != this.genome.renderedCoverageTracks.length; ++i)
+                {
+                    for(let k = 0; k != this.genome.contigs.length; ++k)
+                    {
+                        if(this.genome.renderedCoverageTracks[i].uuidContig == this.genome.contigs[k].uuid)
+                        {
+                            res += `<input type="checkbox" id="${this.genome.renderedCoverageTracks[i].path}" /><h3>${this.genome.contigs[k].name}</h3>`;
+                        }
+                    }
+                }
+            })()}
+        `;
     }
     public postRender() : void{}
     public dataChanged() : void{}
