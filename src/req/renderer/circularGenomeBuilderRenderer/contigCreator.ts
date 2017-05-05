c75fe12871db9bff45fda5e9934b52b45d013c42
Chris Gibb <chris.gibb@outlook.com>
2017-05-05 12:47:02 -0400

    Implemented proto contig creation

--- src/req/renderer/circularGenomeBuilderRenderer/contigCreator.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/contigCreator.ts
@@ -15,19 +15,51 @@ import {GenomeView} from "./genomeView";
 
 export class ContigCreator extends viewMgr.View
 {
+    public genome : cf.CircularFigure;
+    public contigStart : number = -1;
+    public contigEnd : number = -1;
     public constructor(name : string,div : string)
     {
         super(name,div);
     }
     public onMount() : void{}
     public onUnMount() : void{}
+     public forceReRender()
+    {
+        cf.cacheBaseFigure(this.genome);
+        let masterView = <masterView.View>viewMgr.getViewByName("masterView");
+        let genomeView = <GenomeView>viewMgr.getViewByName("genomeView",masterView.views);
+        genomeView.firstRender = true;
+        masterView.firstRender = true;
+        masterView.dataChanged();
+        viewMgr.render();
+    }
     public show() : void
     {
         this.mount();
+        this.contigStart = -1;
+        this.contigEnd = -1;
         document.getElementById(this.div).style.display = "block";
     }
     public hide() : void
     {
+        let inputStart : number = parseInt((<HTMLInputElement>document.getElementById("contigStart")).value);
+        let inputEnd : number = parseInt((<HTMLInputElement>document.getElementById("contigEnd")).value);
+
+        if(inputStart && inputEnd && inputEnd != 0)
+        {
+            this.contigStart = inputStart;
+            this.contigEnd = inputEnd
+            let contig : cf.Contig = new cf.Contig();
+            cf.initContigForDisplay(contig,true);
+            contig.start = this.contigStart;
+            contig.end = this.contigEnd;
+            this.genome.customContigs.push(contig);
+            this.forceReRender();
+            console.log("rerendered");
+        }
+        console.log(inputStart+" "+inputEnd);
+    
         document.getElementById(this.div).style.display = "none";
         this.unMount();
     }
@@ -35,11 +67,17 @@ export class ContigCreator extends viewMgr.View
     {
         try
         {
+            let masterView = <masterView.View>viewMgr.getViewByName("masterView");
+            let genomeView = <GenomeView>viewMgr.getViewByName("genomeView",masterView.views);
+            this.genome = genomeView.genome;
             return `
                 <div class="modalContent">
                     <div class="modalHeader">
                         <span id="closeCreator" class="modalCloseButton">&times;</span>
-                        <h2 id="contigAlias" style="display:inline-block;">${"a"}</h2>
+                        <input type="text" id="contigStart"style="display:inline-block;" />
+                        <p style="display:inline-block;">Start</p>
+                        <input type="text" id="contigEnd" style="display:inline-block;" />
+                        <p style="display:inline-block;">End</p>
                     </div>
                     <div class="modalBody">
                     </div>
