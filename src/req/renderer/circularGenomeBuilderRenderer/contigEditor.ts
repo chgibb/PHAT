3c2145f710ca5f91997ab0d01bd2942a2c25a582
Chris Gibb <chris.gibb@outlook.com>
2017-05-05 09:27:44 -0400

    Layed out class skeleton

---
+++ src/req/renderer/circularGenomeBuilderRenderer/contigEditor.ts
@@ -0,0 +1,29 @@
+/// <reference types="jquery" />
+import * as fs from "fs";
+import * as util from "util";
+
+import * as electron from "electron";
+const ipc = electron.ipcRenderer;
+
+import * as viewMgr from "./../viewMgr";
+import * as cf from "./../circularFigure";
+import * as masterView from "./masterView";
+
+export class ContigEditor extends viewMgr.View
+{
+    public genome : cf.CircularFigure;
+    public constructor(name : string,div : string)
+    {
+        super(name,div);
+    }
+    public onMount() : void{}
+    public onUnMount() : void{}
+    public renderView() : string{}
+    public postRender() : void{}
+    public dataChanged() : void{}
+    public divClickEvents(event : JQueryEventObject) : void{}
+}
+export function addView(arr : Array<viewMgr.View>,div : string)
+{
+    arr.push(new ContigEditor("contigEditor",div));
+}
