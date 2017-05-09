808810de3ec2f1dbc1459766eaf25ba2bb252095
Chris Gibb <chris.gibb@outlook.com>
2017-05-09 14:57:16 -0400

    Pasted from download code from testing

--- src/installUpdateProcess.ts
+++ src/installUpdateProcess.ts
@@ -47,3 +47,58 @@ process.on
         }
     }
 );
+
+/*
+const fs = require("fs");
+
+//Adapted from https://github.com/roblourens/vscode-ripgrep/blob/master/dist/download.js
+let GitHub = require("github-api");
+let GitHubReleases = require("github-releases");
+
+let ghapi = new GitHub({token : ""});
+
+function getLatest()
+{
+    let totalSize = 0;
+    return new Promise((resolve,reject) => {
+        ghapi.getRepo("chgibb","PHAT").listReleases((error,result,request)=>{
+            if(error)
+                console.log(error);
+        }).then((arg)=>{
+            //console.log(arg.data);
+            console.log(arg.data[0].tag_name);
+            
+            let ghr = new GitHubReleases({user : "chgibb",repo : "PHAT",token : ""});
+            const asset = arg.data[0].assets.find(asset => asset.name === "phat-linux-x64.tar.gz");
+            totalSize = asset.size;
+            //console.log(JSON.stringify(asset,undefined,4));
+            ghr.getReleases({tag_name : arg.data[0].tag_name},function(err,releases){
+                if(err)
+                    return reject(err);
+                //console.log(releases);
+                ghr.downloadAsset(asset,(error,istream) => {
+                    if(error)
+                        return reject(error);
+                    let progress = 0;
+                    istream.on("data",(chunk) => {
+                        progress += chunk.length;
+                        console.log(progress+"/"+totalSize);
+                    });
+                    const ostream = fs.createWriteStream("phat-linux-x64.tar.gz");
+                    istream.pipe(ostream);
+                    istream.on("error",reject);
+                    ostream.on("error",reject);
+                    ostream.on("close",() =>{resolve("")});
+                });
+            });
+        }).catch((arg)=>{
+            console.log("catch "+arg);
+        });
+    });
+}
+
+
+getLatest().catch((err)=>{console.log(err+"from catch");});
+
+
+*/
