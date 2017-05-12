8ed329ef7985ae4cc5966b6d87fe39968d660b45
Chris Gibb <chris.gibb@outlook.com>
2017-05-11 22:58:09 -0400

    Stop updating package lists

--- scripts/install.bash
+++ scripts/install.bash
@@ -7,7 +7,7 @@ sudo dpkg -R -i scripts/install
 
 #install the following
 #sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -y
+#sudo apt-get update -y
 #sudo apt-get install libstdc++6 -y
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
