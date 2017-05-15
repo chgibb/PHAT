be2bca11f5a1431e382273d10a0c28843bcb4a13
chgibb <chris.gibb@outlook.com>
2017-05-15 17:11:58 -0400

    Added error logging

--- src/InstallUpdateProcess/installUpdate.cs
+++ src/InstallUpdateProcess/installUpdate.cs
@@ -12,6 +12,7 @@ namespace phat
         //Adapted from https://github.com/icsharpcode/SharpZipLib/wiki/GZip-and-Tar-Samples
         public static void Main(String[] args)
         {
+            try{
             Thread.Sleep(5000);
             Stream inStream = File.OpenRead("phat.update");
     	    Stream gzipStream = new GZipInputStream(inStream);
@@ -23,7 +24,17 @@ namespace phat
     	    gzipStream.Close();
     	    inStream.Close();
 
-            Process.Start(new ProcessStartInfo("phat.exe"));
+            Process.Start(new ProcessStartInfo("phat.exe"));}
+            //Adapted from answer by ekad http://stackoverflow.com/questions/21307789/how-to-save-exception-in-txt-file
+            catch(Exception ex)
+            {
+                using (StreamWriter writer = new StreamWriter("updateerrorlog.txt", true))
+                {
+                    writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
+                    "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+                    writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                }
+            }
         }
     }
 }
