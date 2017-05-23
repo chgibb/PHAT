bf644ad4f66715b83d2b93c7efb2c4848e0a7f9b
Chris Gibb <chris.gibb@outlook.com>
2017-05-23 12:25:00 -0400

    Change to wgetting prebuilt version

--- scripts/install/getPileup.bash
+++ scripts/install/getPileup.bash
@@ -1,2 +1,7 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 
+wget "https://github.com/chgibb/pileup.js/releases/download/v0.6.8/pileup.js"
+wget "https://github.com/chgibb/pileup.js/releases/download/v0.6.8/pileup.css"
+
+mv pileup.js forDist
+mv pileup.css forDist/styles/pileup.css
