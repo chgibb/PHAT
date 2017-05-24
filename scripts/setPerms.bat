498be93c008a1839a07362f48c6c2cf9816c445f
chgibb <chris.gibb@outlook.com>
2017-05-24 09:52:53 -0400

    Silence icacls output

--- scripts/setPerms.bat
+++ scripts/setPerms.bat
@@ -1,2 +1,4 @@
-icacls phat-win32-x64\* /q /c /t /reset
-icacls phat-win32-x64\* /grant Everyone:(OI)(CI)F /T
+@echo Setting permissions
+@icacls phat-win32-x64\* /q /c /t /reset >nul 2>&1
+@icacls phat-win32-x64\* /grant Everyone:(OI)(CI)F /T >nul 2>&1
+@echo Done setting permissions
