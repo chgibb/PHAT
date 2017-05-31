ca3b66034f1ee4feb9e19e15d7f704a3c75b0036
chgibb <chris.gibb@outlook.com>
2017-05-31 09:48:42 -0400

    configured for building samtools on windows on install

--- scripts/install/buildSamTools.bash
+++ scripts/install/buildSamTools.bash
@@ -1,12 +1,24 @@
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
+
 wget "https://github.com/samtools/samtools/releases/download/1.4.1/samtools-1.4.1.tar.bz2"
 
 tar jxf samtools-1.4.1.tar.bz2
 cd samtools-1.4.1
-make
-cd ../
-cp samtools-1.4.1/samtools forDist/linux/samtools
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    make
+    cd ../
+    cp samtools-1.4.1/samtools forDist/linux/samtools
+fi
+
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    #Windows isn't even supported by samtools so this is a huge hack to begin with
+    #It's too much work (for now) to figure out how to build with bz2 and lzma support so just disable it
+    #Also disable curses because we don't even use tview (for now)
+    #This has the effect of disabling support for tview as well as any kind of CRAM handling
+    ./configure --without-curses --disable-bz2 --disable-lzma
+    make
+    cd ../
+    cp samtools-1.4.1/samtools.exe forDist/linux/samtools.exe
+fi
 rm -rf samtools-1.4.1
 rm samtools-1.4.1.tar.bz2
-
-fi
