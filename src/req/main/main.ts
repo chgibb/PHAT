523bfae449b4a2f5d9df959560425db37bb0d7cb
Chris Gibb <chris.gibb@outlook.com>
2017-06-02 10:55:36 -0400

    Explicitly ignore error codes on unlinck

--- src/req/main/main.ts
+++ src/req/main/main.ts
@@ -59,11 +59,12 @@ catch(err)
 	let jobErrorLog = dataMgr.getKey("application","jobErrorLog");
 	let jobVerboseLog = dataMgr.getKey("application","jobVerboseLog");
 
-	fs.unlink(jobErrorLog);
-	fs.unlink(jobVerboseLog);
+	fs.unlink(jobErrorLog,function(err : NodeJS.ErrnoException){});
+	fs.unlink(jobVerboseLog,function(err : NodeJS.ErrnoException){});
 
 	dataMgr.setKey("application","jobErrorLog","jobErrorLog.txt");
 	dataMgr.setKey("application","jobVerboseLog","jobVerboseLog.txt");
+
 }
 
 app.on
