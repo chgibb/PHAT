9e93e837ac958bae8871eec45a0caf808374c215
chgibb <chris.gibb@outlook.com>
2017-06-01 22:37:59 -0400

    break bowtie2build into file

---
+++ src/req/operations/indexFasta/bowTie2Build.ts
@@ -0,0 +1,58 @@
+import * as fs from "fs";
+
+import {IndexFasta} from "./../indexFasta";
+
+import {SpawnRequestParams} from "./../../JobIPC";
+import {Job,JobCallBackObject} from "./../../main/Job";
+
+export function bowTie2Build(op : IndexFasta) : Promise<{}>
+{
+    return new Promise((resolve,reject) => {
+        let jobCallBack : JobCallBackObject = {
+            send(channel : string,params : SpawnRequestParams)
+            {
+                if(params.done && params.retCode !== undefined)
+                {
+                    if(params.retCode == 0)
+                    {
+                        setTimeout(
+                            function(){
+                                try
+                                {
+                                    for(let i : number = 0; i != op.bowtieIndices.length; ++i)
+                                    {
+                                        fs.accessSync(`${op.bowtieIndices[i]}`,fs.constants.F_OK | fs.constants.R_OK);
+                                    }
+                                }
+                                catch(err)
+                                {
+                                    reject(`Failed to write all bowtie2 indices for ${op.fasta.alias}`);
+                                }
+                            },5000
+                        );
+                    }
+                }
+            }
+        }
+        let bowTieArgs : Array<string> = new Array<string>();
+        if(process.platform == "linux")
+            bowTieArgs = [op.fasta.path,op.bowTieIndexPath];
+        else if(process.platform == "win32")
+        {
+            bowTieArgs = [
+                `resources/app/bowtie2-build`,
+                `"${op.fasta.path}"`,
+                `"${op.bowTieIndexPath}"`
+            ];
+        }
+        op.bowtieJob = new Job(op.bowtie2BuildExe,bowTieArgs,"",true,jobCallBack,{});
+        try
+        {
+            op.bowtieJob.Run();
+        }
+        catch(err)
+        {
+            return reject(err);
+        }
+    });
+}
