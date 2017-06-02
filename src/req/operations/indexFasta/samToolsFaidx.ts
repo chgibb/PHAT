179497be13b3ffa890bf1796e6ea85bb734d1943
chgibb <chris.gibb@outlook.com>
2017-06-01 22:27:16 -0400

    Break samtools faidx into file

---
+++ src/req/operations/indexFasta/samToolsFaidx.ts
@@ -0,0 +1,60 @@
+import * as fs from "fs";
+
+const fse = require("fs-extra");
+
+import {IndexFasta} from "./../indexFasta";
+
+import {SpawnRequestParams} from "./../../JobIPC";
+import {Job,JobCallBackObject} from "./../../main/Job";
+
+export function samToolsFaidx(op : IndexFasta) : Promise<{}>
+{
+    return new Promise((resolve,reject) => {
+        let jobCallBack : JobCallBackObject = {
+            send(channel : string,params : SpawnRequestParams)
+            {
+                if(params.done && params.retCode !== undefined)
+                {
+                    if(params.retCode == 0)
+                    {
+                        setTimeout(
+                            function(){
+                                try
+                                {
+                                    fse.copy(`${op.fasta.path}.fai`,op.faiPath,function(err : string){
+                                        if(err)
+                                            reject(err);
+                                        resolve();
+                                    });
+                                }
+                                catch(err)
+                                {
+                                    return reject(err);
+                                }
+                            },1000
+                        );
+                    }
+                    else
+                    {
+                        return reject(`Failed to create fai index for ${op.fasta.alias}`);
+                    }
+                }
+            }
+        }
+        op.faiJob = new Job(
+            op.samToolsExe,
+            <Array<string>>[
+                "faidx",
+                op.fasta.path
+            ],"",true,jobCallBack,{}
+        );
+        try
+        {
+            op.faiJob.Run();
+        }
+        catch(err)
+        {
+            return reject(err);
+        }
+    });
+}
