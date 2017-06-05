5ee571cc416b5e8ddd6d0fbc85f8fccfeef281d6
Chris Gibb <chris.gibb@outlook.com>
2017-06-05 09:26:46 -0400

    Fixed file names

--- src/RenderSNPTrack.ts
+++ src/RenderSNPTrack.ts
@@ -1,2 +1,2 @@
 import requireDyn from "./req/requireDyn";
-requireDyn("./bootStrapCodeCache")("resources/app/RenderCoverageTrackProcess.js","./RenderCoverageTrackProcess","resources/app/cdata/RenderCoverageTrackProcess.cdata");
+requireDyn("./bootStrapCodeCache")("resources/app/RenderSNPTrackProcess.js","./RenderSNPTrackProcess","resources/app/cdata/RenderSNPTrackProcess.cdata");
