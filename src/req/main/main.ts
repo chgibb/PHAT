10e558098770ed99872cc98a905e7e8c60e064a9
Chris Gibb <chris.gibb@outlook.com>
2017-06-07 10:51:05 -0400

    Removed old code

--- src/req/main/main.ts
+++ src/req/main/main.ts
@@ -116,15 +116,6 @@ app.on
 			fs.mkdirSync("resources/app/cdata");
 		}
 		catch(err){}
-		/*try
-		{
-			fs.mkdirSync("resources/app/rt");
-			fs.mkdirSync("resources/app/rt/QCReports");
-			fs.mkdirSync("resources/app/rt/indexes");
-			fs.mkdirSync("resources/app/rt/AlignmentArtifacts");
-			fs.mkdirSync("resources/app/rt/circularFigures");
-		}
-		catch(err){}*/
 
 		const menuTemplate: Array<Electron.MenuItemOptions> = [
 		{
