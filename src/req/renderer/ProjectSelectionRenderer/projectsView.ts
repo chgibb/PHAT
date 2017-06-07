54226136e75f19c15e605309bca837be863e2604
Chris Gibb <chris.gibb@outlook.com>
2017-06-07 10:01:28 -0400

    proto display projects

--- src/req/renderer/ProjectSelectionRenderer/projectsView.ts
+++ src/req/renderer/ProjectSelectionRenderer/projectsView.ts
@@ -20,6 +20,23 @@ export class ProjectsView extends View
     {
         return `
             <button id="createNewProject">Create New Project</button>
+            ${(()=>{
+                let res = "";
+                if(!this.projects)
+                    "<h2>You have no projects</h2>";
+                if(this.projects)
+                {
+                    for(let i = 0; i != this.projects.length; ++i)
+                    {
+                        res += `
+                            <h4>${this.projects[i].alias}</h4><br />
+                            <h6>Last Opened: ${this.projects[i].lastOpened}</h6><br />
+                            <h6>Created: ${this.projects[i].created}</h6><br />
+                        `;
+                    }
+                }
+                return res;
+            })()}
         `;
     }
     public postRender() : void
