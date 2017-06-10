adc9fac10069ab717335016c09d45a82e38486f0
Chris Gibb <chris.gibb@outlook.com>
2017-06-10 12:10:51 -0400

    fixed paths

--- src/req/operations/CheckForUpdate.ts
+++ src/req/operations/CheckForUpdate.ts
@@ -3,6 +3,7 @@ import * as cp from "child_process";
 
 import * as atomic from "./atomicOperations";
 import {AtomicOperationForkEvent,AtomicOperationIPC} from "./../atomicOperationsIPC";
+import {getReadable,getReadableAndWritable} from "./../getAppPath";
 export class CheckForUpdate extends atomic.AtomicOperation
 {
     public token : string;
@@ -21,7 +22,7 @@ export class CheckForUpdate extends atomic.AtomicOperation
     public run() : void
     {
         let self = this;
-        this.checkForUpdateProcess = cp.fork("resources/app/CheckForUpdate.js");
+        this.checkForUpdateProcess = cp.fork(getReadable("CheckForUpdate.js"));
         this.checkForUpdateProcess.on(
             "message",function(ev : AtomicOperationForkEvent)
             {
