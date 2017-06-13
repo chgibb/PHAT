d63b30e292cbca344c7ae4dcd3fecb37a95d9150
Chris Gibb <chris.gibb@outlook.com>
2017-06-13 11:08:18 -0400

    Moved interfaces to classes

--- src/req/renderer/OutputRenderer/rightPanel.ts
+++ src/req/renderer/OutputRenderer/rightPanel.ts
@@ -1,43 +1,75 @@
 import * as viewMgr from "./../viewMgr";
 import * as masterView from "./masterView";
 
-export interface FastQInfoSelection
+export class FastQInfoSelection
 {
-    alias : boolean;
-    fullName : boolean;
-    sizeInBytes : boolean;
-    formattedSize : boolean;
-    numberOfSequences : boolean;
-    PBSQ : boolean;
-    PSQS : boolean;
-    PSGCC : boolean;
-    SDL : boolean;
-    ORS : boolean;
+    public alias : boolean;
+    public fullName : boolean;
+    public sizeInBytes : boolean;
+    public formattedSize : boolean;
+    public numberOfSequences : boolean;
+    public PBSQ : boolean;
+    public PSQS : boolean;
+    public PSGCC : boolean;
+    public SDL : boolean;
+    public ORS : boolean;
+    [index : string] : boolean;
+    public constructor()
+    {
+        this.alias = false;
+        this.fullName = false;
+        this.sizeInBytes = false;
+        this.formattedSize = false;
+        this.numberOfSequences = false;
+        this.PBSQ = false;
+        this.PSQS = false;
+        this.PSGCC = false;
+        this.SDL = false;
+        this.ORS = false;
+    }
+    
+}
+
+export class FastaInfoSelection
+{
     [index : string] : boolean;
 }
 
-export interface FastaInfoSelection
+export class AlignmentInfoSelection
 {
+    public alias : boolean;
+    public sizeInBytes : boolean;
+    public formattedSize : boolean;
+    public reads : boolean;
+    public mates : boolean;
+    public overallAlignmentRate : boolean;
+    public minimumCoverage : boolean;
+    public minimumVariableFrequency : boolean;
+    public minimumAverageQuality : boolean;
+    public pValueThreshold : boolean;
+    public SNPsPredicted : boolean;
+    public indelsPredicted : boolean;
+    public dateRan : boolean;
+    public SNPPositions : boolean;
     [index : string] : boolean;
-}
-
-export interface AlignmentInfoSelection
-{
-    alias : boolean;
-    sizeInBytes : boolean;
-    formattedSize : boolean;
-    reads : boolean;
-    mates : boolean;
-    overallAlignmentRate : boolean;
-    minimumCoverage : boolean;
-    minimumVariableFrequency : boolean;
-    minimumAverageQuality : boolean;
-    pValueThreshold : boolean;
-    SNPsPredicted : boolean;
-    indelsPredicted : boolean;
-    dateRan : boolean;
-    SNPPositions : boolean;
-    [index : string] : boolean;
+    public constructor()
+    {
+        this.alias = false;
+        this.fullName = false;
+        this.sizeInBytes = false;
+        this.formattedSize = false;
+        this.reads = false;
+        this.mates = false;
+        this.overallAlignmentRate = false;
+        this.minimumCoverage = false;
+        this.minimumVariableFrequency = false;
+        this.minimumAverageQuality = false;
+        this.pValueThreshold = false;
+        this.SNPsPredicted = false;
+        this.indelsPredicted = false;
+        this.dateRan = false;
+        this.SNPPositions = false;
+    }
 }
 
 export class RightPanel extends viewMgr.View
@@ -48,6 +80,9 @@ export class RightPanel extends viewMgr.View
     public constructor(name : string,div : string)
     {
         super(name,div);
+        this.fastQInfoSelection = new FastQInfoSelection();
+        this.refSeqInfoSelection = new FastaInfoSelection();
+        this.alignmentInfoSelection = new AlignmentInfoSelection();
     }
 
     public onMount() : void{}
