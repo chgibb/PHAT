5fc2e019cb462bbcf61295a4f75c0bfcd5a1039e
Chris Gibb <chris.gibb@outlook.com>
2017-06-13 12:27:42 -0400

    table header for alignments

--- src/req/renderer/OutputRenderer/reportView.ts
+++ src/req/renderer/OutputRenderer/reportView.ts
@@ -25,18 +25,43 @@ export class View extends viewMgr.View
                 {
                     res += `
                         <table style="width:100%">
-                        <tr>
-                            ${rightPanel.fastQInfoSelection.alias != false ? "<th>Alias</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.fullName != false ? "<th>Full Path</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.sizeInBytes != false ? "<th>Size In Bytes</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.formattedSize != false ? "<th>Formatted Size</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.numberOfSequences != false ? "<th>Number of Sequences</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.PBSQ != false ? "<th>Per Base Sequence Quality</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.PSQS != false ? "<th>Per Sequence Quality Score</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.PSGCC != false ? "<th>Per Sequence GC Content</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.SDL != false ? "<th>Sequence Duplication Levelsias</th>" : ""}
-                            ${rightPanel.fastQInfoSelection.ORS != false ? "<th>Over Represented Sequences</th>" : ""}
-                        </tr>
+                            <tr>
+                                ${rightPanel.fastQInfoSelection.alias != false ? "<th>Alias</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.fullName != false ? "<th>Full Path</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.sizeInBytes != false ? "<th>Size In Bytes</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.formattedSize != false ? "<th>Formatted Size</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.numberOfSequences != false ? "<th>Number of Sequences</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.PBSQ != false ? "<th>Per Base Sequence Quality</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.PSQS != false ? "<th>Per Sequence Quality Score</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.PSGCC != false ? "<th>Per Sequence GC Content</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.SDL != false ? "<th>Sequence Duplication Levelsias</th>" : ""}
+                                ${rightPanel.fastQInfoSelection.ORS != false ? "<th>Over Represented Sequences</th>" : ""}
+                            </tr>
+
+                        </table>
+                    `;
+                }
+
+                if(masterView.displayInfo == "AlignmentInfo")
+                {
+                    res += `
+                        <table style="width:100%">
+                            <tr>
+                                ${rightPanel.alignmentInfoSelection.alias != false ? "<th>Alias</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.sizeInBytes != false ? "<th>Size In Bytes</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.formattedSize != false ? "<th>Formatted Size</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.reads != false ? "<th>Reads</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.mates != false ? "<th>Mates</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.overallAlignmentRate != false ? "<th>Overall Alignment Rate %</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.minimumCoverage != false ? "<th>Minimum Coverage</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.minimumVariableFrequency != false ? "<th>Minimum Variable Frequency</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.minimumAverageQuality != false ? "<th>Minimum Average Quality</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.pValueThreshold != false ? "<th>P-Value Threshold</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.SNPsPredicted != false ? "<th>SNPs Predicted</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.indelsPredicted != false ? "<th>Indels Predicted</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.dateRan != false ? "<th>Date Ran</th>" : ""}
+                                ${rightPanel.alignmentInfoSelection.SNPPositions != false ? "<th>SNP Positions</th>" : ""}
+                            </tr>
 
                         </table>
                     `;
