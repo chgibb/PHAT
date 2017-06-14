548acdf4027c4a447af2743ddfc34f7738ce5cd3
Chris Gibb <chris.gibb@outlook.com>
2017-06-14 11:32:27 -0400

    render vcf rows into table

--- src/req/renderer/OutputRenderer/reportView/renderSNPPositionsTable.ts
+++ src/req/renderer/OutputRenderer/reportView/renderSNPPositionsTable.ts
@@ -1,8 +1,9 @@
 import * as viewMgr from "./../../viewMgr";
 import * as masterView from "./../masterView";
 import * as rightPanel from "./../rightPanel";
+import {VCF2JSONRow} from "./../../../varScanMPileup2SNPVCF2JSON";
 
-export function renderSNPPositionsTable() : string
+export function renderSNPPositionsTable(rows : Array<VCF2JSONRow>) : string
 {
     let masterView = <masterView.View>viewMgr.getViewByName("masterView");
     let rightPanel = <rightPanel.View>viewMgr.getViewByName("rightPanel",masterView.views);
@@ -27,6 +28,51 @@ export function renderSNPPositionsTable() : string
                     ${rightPanel.snpPositionsInfoSelection.consCovReads1Reads2FreqPValue2 != false ? "<th>Cons:Cov:Reads1:Reads2:Freq:P-value</th>" : ""}
                 </tr>
             `;
+            return res;
+        })()}
+        ${(()=>{
+            let res = "";
+            
+            for(let i = 0; i != rows.length-1; ++i)
+            {
+                res += "<tr>";
+
+                if(rightPanel.snpPositionsInfoSelection.chrom)
+                    res += `<td>${rows[i].chrom}</td>`;
+                
+                if(rightPanel.snpPositionsInfoSelection.position)
+                    res += `<td>${rows[i].position}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.ref)
+                    res += `<td>${rows[i].ref}</td>`;
+                
+                if(rightPanel.snpPositionsInfoSelection.var)
+                    res += `<td>${rows[i].var}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.consCovReads1Reads2FreqPValue)
+                    res += `<td>${rows[i].consCovReads1Reads2FreqPValue}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.strandFilterR1R1R2R2pVal)
+                    res += `<td>${rows[i].strandFilterR1R1R2R2pVal}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.samplesRef)
+                    res += `<td>${rows[i].samplesRef}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.samplesHet)
+                    res += `<td>${rows[i].samplesHet}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.samplesHom)
+                    res += `<td>${rows[i].samplesHom}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.samplesNC)
+                    res += `<td>${rows[i].samplesNC}</td>`;
+
+                if(rightPanel.snpPositionsInfoSelection.consCovReads1Reads2FreqPValue2)
+                    res += `<td>${rows[i].consCovReads1Reads2FreqPValue2}</td>`;
+
+                res += "</tr>";
+            }
+
             return res;
         })()}
         </table>
