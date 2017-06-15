ffc238db57dbc9afafc571d8634ee2d647964057
Chris Gibb <chris.gibb@outlook.com>
2017-06-15 11:55:39 -0400

    changed refs

--- src/Pileup.ts
+++ src/Pileup.ts
@@ -2,7 +2,7 @@ import {getReadable,getReadableAndWritable} from "./req/getAppPath";
 import {bootStrapCodeCache} from "./req/bootStrapCodeCache";
 
 bootStrapCodeCache(
-    getReadable("PathogenRenderer.js"),
-    "./PathogenRenderer",
-    getReadableAndWritable("PathogenRenderer.cdata")
+    getReadable("PileupRenderer.js"),
+    "./PileupRenderer",
+    getReadableAndWritable("PileupRenderer.cdata")
 );
