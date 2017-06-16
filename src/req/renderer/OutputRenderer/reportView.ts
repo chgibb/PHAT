6497718e2230ec9d4311f96402dea1b5010b66f1
Chris Gibb <chris.gibb@outlook.com>
2017-06-16 10:39:37 -0400

    fixed spacing

--- src/req/renderer/OutputRenderer/reportView.ts
+++ src/req/renderer/OutputRenderer/reportView.ts
@@ -37,8 +37,6 @@ export class View extends viewMgr.View
         let masterView = <masterView.View>viewMgr.getViewByName("masterView");
         let rightPanel = <rightPanel.View>viewMgr.getViewByName("rightPanel",masterView.views);
 
-        
-
         //if we're looking at SNP position, refresh table information if the alignment being inspected has changed
         if(masterView.displayInfo == "SNPPositions")
         {
