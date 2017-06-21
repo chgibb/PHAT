abd6407e94a1a0d82f9474399a769d9925a47238
Chris Gibb <chris.gibb@outlook.com>
2017-06-21 10:30:28 -0400

    handle loading internal and external tarballs

--- src/req/openProject.ts
+++ src/req/openProject.ts
@@ -4,18 +4,29 @@ import * as rimraf from "rimraf";
 const tarfs = require("tar-fs");
 const tarStream = require("tar-stream");
 const gunzip = require("gunzip-maybe");
+const jsonFile = require("jsonfile");
 
 import {getReadableAndWritable} from "./getAppPath";
 import {ProjectManifest,getTarBallPath} from "./projectManifest";
 import {rebuildRTDirectory} from "./main/rebuildRTDirectory"
 import * as dataMgr from "./main/dataMgr";
 
-export function openProject(proj : ProjectManifest,cb : (toUnpack : number,unPacked : number) => void) : Promise<{}>
+export function openProject(
+    proj : ProjectManifest,
+    cb : (toUnpack : number,unPacked : number) => void,
+    externalProjectPath? : string
+) : Promise<undefined>
 {
     return new Promise((resolve,reject) => {
         dataMgr.clearData();
         rimraf.sync(getReadableAndWritable("rt"));
-        let projectTarBall = getTarBallPath(proj);
+
+        let projectTarBall : string;
+
+        if(!externalProjectPath)
+            projectTarBall = getTarBallPath(proj);
+        else
+            projectTarBall = externalProjectPath;
         try
         {
             fs.accessSync(projectTarBall)
@@ -44,6 +55,13 @@ export function openProject(proj : ProjectManifest,cb : (toUnpack : number,unPac
                     }
                 });
                 extract.on("finish",() => {
+                    if(externalProjectPath)
+                    {
+                        let rt = jsonFile.readFileSync(getReadableAndWritable("rt/rt.json"));
+                        rt.application.project.isExternal = true;
+                        rt.application.project.externalPath = externalProjectPath;
+                        jsonFile.writeFileSync(getReadableAndWritable("rt/rt.json"));   
+                    }
                     resolve();
                 });
                 let unPackStream = fs.createReadStream(projectTarBall).pipe(gunzip()).pipe(extract);
