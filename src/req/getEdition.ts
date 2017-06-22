1c4e15b2a8d95123a45e56c39ff41cfd7d4736ad
chgibb <chris.gibb@outlook.com>
2017-06-22 14:02:47 -0400

    reimplement getapppath algo

--- src/req/getEdition.ts
+++ src/req/getEdition.ts
@@ -1,23 +1,28 @@
 import * as fs from "fs";
 import * as path from "path";
 
-import {getReadable} from "./getAppPath";
 let editionString : string = undefined;
-//Assume not testing/CI
-//let editionSource = path.dirname(process.execPath)+"/edition";
-let editionSource = getReadable("edition.txt");
+
+/*
+    reimplementation of algorithm in getAppPath.ts
+    this is done in order to prevent a recursive dependence.
+*/
+function getEditionSource() : string
+{
+    let baseDir = "";
+    if(process.versions["electron"])
+        baseDir = path.dirname(process.execPath)+"/resources/app";
+    else
+        baseDir = process.cwd()+"/resources/app";
+    return path.resolve(path.normalize(baseDir+"/edition.txt"))
+} 
 
 function getEditionString()
 {
     if(!editionString)
     {
-        editionString = (<any>fs.readFileSync(editionSource));
+        editionString = (<any>fs.readFileSync(getEditionSource()).toString());
     }
-}
-
-export function setEditionSource(path : string)
-{
-    editionSource = path;
 }
 
 export function getEdition() : string
