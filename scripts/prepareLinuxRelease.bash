7a6c2106acf048339f20f367a52155a839f790f8
Chris Gibb <chris.gibb@outlook.com>
2017-06-21 22:40:15 -0400

    Stop downloading and installing snapcraft manually

--- scripts/prepareLinuxRelease.bash
+++ scripts/prepareLinuxRelease.bash
@@ -19,9 +19,9 @@ mv deb/*.deb .
 #sudo add-apt-repository ppa:snappy-dev/tools -y
 #sudo apt-get update
 #sudo apt-get install snapcraft
-wget https://launchpad.net/ubuntu/+source/snapcraft/2.26+17.04.1/+build/11947357/+files/snapcraft_2.26+17.04.1_all.deb
+#wget https://launchpad.net/ubuntu/+source/snapcraft/2.26+17.04.1/+build/11947357/+files/snapcraft_2.26+17.04.1_all.deb
 
-sudo dpkg -i snapcraft_2.26+17.04.1_all.deb
+#sudo dpkg -i snapcraft_2.26+17.04.1_all.deb
 
 node_modules/.bin/electron-builder --prepackaged=phat-linux-x64 --linux snap
 
