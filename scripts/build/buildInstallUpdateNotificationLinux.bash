ba031b9632a5fc06405f40ebf36cb4239f65cc95
Chris Gibb <chris.gibb@outlook.com>
2017-06-26 10:55:51 -0400

    build into dist

--- scripts/build/buildInstallUpdateNotificationLinux.bash
+++ scripts/build/buildInstallUpdateNotificationLinux.bash
@@ -4,7 +4,7 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     gcc -Wall -c src/InstallUpdateProcess/installUpdateNotificationLinux.c `pkg-config --cflags gtk+-2.0` \
     `pkg-config --libs gtk+-2.0`
-    gcc -o installUpdateNotificationLinux installUpdateNotificationLinux.o `pkg-config --cflags gtk+-2.0` \
+    gcc -o dist/installUpdateNotificationLinux installUpdateNotificationLinux.o `pkg-config --cflags gtk+-2.0` \
     `pkg-config --libs gtk+-2.0`
 
     rm installUpdateNotificationLinux.o
