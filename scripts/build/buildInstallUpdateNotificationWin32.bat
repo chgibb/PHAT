b4d3c93d750b5bef296a76f8e5155c06f54ec615
chgibb <chris.gibb@outlook.com>
2017-06-29 14:59:37 -0400

    Add mingw/bin to path before build

--- scripts/build/buildInstallUpdateNotificationWin32.bat
+++ scripts/build/buildInstallUpdateNotificationWin32.bat
@@ -1,3 +1,5 @@
+set path=%path%;C:\mingw\bin
+
 C:\MinGW\bin\gcc -Wall -c -static src/InstallUpdateProcess/installUpdateNotificationWin32.c
 C:\MinGW\bin\gcc -static -static-libgcc -o dist/installUpdateNotificationWin32.exe installUpdateNotificationWin32.o -mwindows
 
