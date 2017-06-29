3444a8e629a726033cd49f952061cfed08339028
chgibb <chris.gibb@outlook.com>
2017-06-29 14:46:37 -0400

    loop after completion

--- scripts/build/buildInstallUpdateNotificationWin32.bat
+++ scripts/build/buildInstallUpdateNotificationWin32.bat
@@ -2,3 +2,7 @@ C:\MinGW\bin\gcc -Wall -c -static src/InstallUpdateProcess/installUpdateNotifica
 C:\MinGW\bin\gcc -static -static-libgcc -o dist/installUpdateNotificationWin32.exe installUpdateNotificationWin32.o -mwindows
 
 rm installUpdateNotificationWin32.o
+
+:start
+
+goto start
