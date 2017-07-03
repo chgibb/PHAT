ce8205a868e965eb33e430b549046b13ff1cafe6
Chris Gibb <chris.gibb@outlook.com>
2017-07-03 08:59:51 -0400

    added logging

--- src/guiTests/req/input/indexSuccess.ts
+++ src/guiTests/req/input/indexSuccess.ts
@@ -12,6 +12,7 @@ export async function indexSuccess() : Promise<boolean>
             }
             if(op.flags.done && op.flags.success)
             {
+                console.log("succeeded indexing");
                 resolve(true);
             }
         });
