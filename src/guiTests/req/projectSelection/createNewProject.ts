b88e673ac27a5a6993ffe3bb7a059891ae8e8fcd
Chris Gibb <chris.gibb@outlook.com>
2017-07-03 08:56:57 -0400

    added logging

--- src/guiTests/req/projectSelection/createNewProject.ts
+++ src/guiTests/req/projectSelection/createNewProject.ts
@@ -5,6 +5,7 @@ export async function createNewProject() : Promise<void>
     return new Promise<void>((resolve,reject) => {
         let projSelection = winMgr.getWindowsByName("projectSelection");
         setTimeout(function(){
+            console.log("creating new project");
             projSelection[0].webContents.executeJavaScript(`
                 const electron = require("electron");
                 const ipc = electron.ipcRenderer;
