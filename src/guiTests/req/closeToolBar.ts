d606c2c3b54660b657b9539d7d67bb22bb040a11
Chris Gibb <chris.gibb@outlook.com>
2017-07-03 08:59:20 -0400

    added logging

--- src/guiTests/req/closeToolBar.ts
+++ src/guiTests/req/closeToolBar.ts
@@ -4,6 +4,7 @@ export async function closeToolBar() : Promise<void>
 {
     return new Promise<void>((resolve,reject) => {
         setTimeout(function(){
+            console.log("closing toolbar");
             let toolBar = winMgr.getWindowsByName("toolBar");
             if(!toolBar || toolBar.length > 1 || toolBar.length == 0)
             {
