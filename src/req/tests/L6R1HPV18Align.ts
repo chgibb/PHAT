a9cb2d6719edadc086e3f7ab3f450994005caf86
Chris Gibb <chris.gibb@outlook.com>
2017-07-05 15:35:02 -0400

    added module for l6r1hpv18 align

---
+++ src/req/tests/L6R1HPV18Align.ts
@@ -0,0 +1,13 @@
+import {alignData} from "./../alignData";
+
+let L6R1HPV18Align : alignData;
+
+export function get() : alignData
+{
+    return L6R1HPV18Align;
+}
+
+export function set(align : alignData) : void
+{
+    L6R1HPV18Align = align;
+}
