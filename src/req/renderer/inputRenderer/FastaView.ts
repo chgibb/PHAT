eed1d43b2d2accc4573c3835b8e257d2e0f9c283
Chris Gibb <chris.gibb@outlook.com>
2017-07-05 10:49:04 -0400

    Removed patho/host distinction

--- src/req/renderer/inputRenderer/FastaView.ts
+++ src/req/renderer/inputRenderer/FastaView.ts
@@ -72,10 +72,6 @@ export class FastaView extends View
             {
                 $('#'+this.model.fastaInputs[i].uuid).prop("checked",true);
             }
-            if(this.model.fastaInputs[i].host)
-                $('#'+this.model.fastaInputs[i].uuid+'_host').prop("checked",true);
-            if(this.model.fastaInputs[i].pathogen)
-                $('#'+this.model.fastaInputs[i].uuid+'_path').prop("checked",true);
             if(this.searchFilter.test(this.model.fastaInputs[i].alias))
             {
                 if(!this.model.fastaInputs[i].checked)
@@ -151,38 +147,6 @@ export class FastaView extends View
                 {
                     if(this.model.fastaInputs[i].checked)
                         this.model.indexFasta(this.model.fastaInputs[i]);
-                }
-            }
-        }
-        //host/patho radios are identified by _host or _path appended to the end
-        //of the item's .validID property
-        var type = event.target.id.substr(event.target.id.length-5,event.target.id.length);
-        if(type == "_host" || type == "_path" && (<HTMLInputElement>event.target).checked)
-        {
-            console.log('identified radio');
-            //the user clicked a host/patho radio button
-            //extract the id actual id of the item that was clicked
-            var ID = event.target.id.substr(0,event.target.id.length-5);
-            for(let i = 0; i != this.model.fastaInputs.length; ++i)
-            {
-                if(this.model.fastaInputs[i].uuid == ID)
-                {
-                    if(type == "_host")
-                    {
-                        this.model.fastaInputs[i].host = true;
-                        this.model.fastaInputs[i].pathogen = false;
-                        this.model.fastaInputs[i].type = "host";
-                        this.dataChanged();
-                        return;
-                    }
-                    if(type == "_path")
-                    {
-                        this.model.fastaInputs[i].host = false;
-                        this.model.fastaInputs[i].pathogen = true;
-                        this.model.fastaInputs[i].type = "path";
-                        this.dataChanged();
-                        return;
-                    }
                 }
             }
         }
