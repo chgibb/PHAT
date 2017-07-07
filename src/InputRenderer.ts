459af4d2090ce8e54d060f027a314695b99237b5
Chris Gibb <chris.gibb@outlook.com>
2017-07-07 10:12:46 -0400

    added post render

--- src/InputRenderer.ts
+++ src/InputRenderer.ts
@@ -6,10 +6,26 @@ import {GetKeyEvent,KeySubEvent} from "./req/ipcEvents";
 import * as viewMgr from "./req/renderer/viewMgr";
 
 import * as masterView from "./req/renderer/inputRenderer/masterView";
+import * as fastqView from "./req/renderer/inputRenderer/FastqView";
 import * as $ from "jquery";
 (<any>window).$ = $;
 require("./req/renderer/commonBehaviour");
 
+function postRender(view : viewMgr.View) : void
+{
+    let fastqView = $("#fastqView");
+    fastqView.css("height",$(window).height()/2+"px");
+    let fastaView = $("#fastaView");
+    fastaView.css("height",$(window).height()/2+"px");
+}
+viewMgr.setPostRender(postRender);
+$(window).resize
+(
+	function()
+	{
+        viewMgr.render();
+    }
+);
 $
 (
     function()
@@ -67,11 +83,13 @@ $
                 //reply from call to getState
 			    if(arg.action == "getKey" || arg.action == "keyChange")
 				{
+                    let masterView = <masterView.View>viewMgr.getViewByName("masterView");
+                    let fastqView = <fastqView.View>viewMgr.getViewByName("fastqView",masterView.views);
 					if(arg.key == 'fastqInputs')
 					{
                         if(arg.val !== undefined)
                         {
-                            //input.fastqInputs = arg.val;
+                            fastqView.fastqInputs = arg.val;
                         }
                     }
                     if(arg.key == 'fastaInputs')
