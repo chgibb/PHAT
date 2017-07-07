cafc185b59d8c9f3b066eaacb525f5edd7df3fd3
Chris Gibb <chris.gibb@outlook.com>
2017-07-07 10:34:39 -0400

    add each selected file

--- src/req/renderer/inputRenderer/fastaBrowseDialog.ts
+++ src/req/renderer/inputRenderer/fastaBrowseDialog.ts
@@ -1,7 +1,9 @@
 import * as electron from "electron";
 const dialog = electron.remote.dialog;
+const ipc = electron.ipcRenderer;
 
-export default function showFastaBrowseDialog() : void
+import {AtomicOperationIPC} from "./../../atomicOperationsIPC";
+export function fastaBrowseDialog() : void
 {
     dialog.showOpenDialog
 	(
@@ -31,20 +33,14 @@ export default function showFastaBrowseDialog() : void
 			{
 				for(let i : number = 0; i != files.length; ++i)
 				{
-                	//create new items if not already existing
-					/*if(!input.fastaExists(files[i]))
-					{
-						try
-						{
-                    		input.addFasta(files[i]);
+					ipc.send(
+						"runOperation",
+						<AtomicOperationIPC>{
+							opName : "inputFastaFile",
+							filePath : files[i]
 						}
-						catch(err)
-						{
-							alert("Bowtie2 will reject fasta files with commas in their paths. Please remove all commas from the path and try again.");
-						}
-					}*/
+					);
                 }
-				//input.postFastaInputs();
 			}
 		}
 	);
