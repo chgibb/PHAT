9eda29dedbacdf15bdaa829cf2b8725ff98e6e62
Chris Gibb <chris.gibb@outlook.com>
2017-07-08 13:53:24 -0400

    removed old test steps

--- src/guiTests/2inputRefs.ts
+++ src/guiTests/2inputRefs.ts
@@ -5,8 +5,6 @@ import {openProjectsView} from "./req/projectSelection/openProjectsView";
 import {openFirstProject} from "./req/projectSelection/openFirstProject";
 import {openInputWindow} from "./req/input/openInputWindow";
 import {inputHPV16Ref} from "./req/input/inputHPV16Ref";
-import {openRefSeqView} from "./req/input/openRefSeqView";
-import {selectAllRefs} from "./req/input/selectAllRefs";
 import {indexRefs} from "./req/input/indexRefs";
 import {indexSuccess} from "./req/input/indexSuccess";
 import {closeToolBar} from "./req/closeToolBar";
@@ -15,11 +13,8 @@ async function runTest() : Promise<void>
 {
     await openProjectsView();
     await openFirstProject();
-
     await openInputWindow();
     await inputHPV16Ref();
-    await openRefSeqView();
-    await selectAllRefs();
     await indexRefs();
     await indexSuccess().then(async (val) => {
         if(val === true)
