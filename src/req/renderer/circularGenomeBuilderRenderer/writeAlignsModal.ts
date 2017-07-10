63cdfa7d730bf30c60ad7db58263d18519515da7
Chris Gibb <chris.gibb@outlook.com>
2017-07-10 13:32:50 -0400

    Prompt to open figure if none is open

--- src/req/renderer/circularGenomeBuilderRenderer/writeAlignsModal.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/writeAlignsModal.ts
@@ -1,23 +1,47 @@
 import * as viewMgr from "./../viewMgr";
 import * as masterView from "./masterView";
+import * as genomeView from "./genomeView";
 export function writeAlignsModal() : void
 {
     let masterView = <masterView.View>viewMgr.getViewByName("masterView");
+    let genomeView = <genomeView.GenomeView>viewMgr.getViewByName("genomeView",masterView.views);
 
     let title = `Coverage Options`;
 
-    let footer = `
-        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="footerClose">Close</button>
-        <button type="button" class="btn btn-primary" id="footerSave">Save changes</button>
-    `;
+    let body = ``;
+    if(!genomeView.genome)
+    {
+        body = `
+            <p>You must select a figure to edit before you can view it's coverage options.</p>
+        `;
+    }
+
+    let footer = ``;
+    if(!genomeView.genome)
+    {
+        footer = `
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" id="footerClose">Got It</button>
+        `;
+    }
+    if(genomeView.genome)
+    {
+        footer += `
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" id="footerClose">Cancel</button>
+            <button type="button" class="btn btn-primary" id="footerSave">Save changes</button>
+        `;
+    }
 
     document.getElementById("modalTitle").innerHTML = title;
+    document.getElementById("modalBody").innerHTML = body;
     document.getElementById("modalFooter").innerHTML = footer;
     document.getElementById("footerClose").onclick = function(this : HTMLElement,ev : MouseEvent){
                 //alert("footerClose");
     }
-    document.getElementById("footerSave").onclick = function(this : HTMLElement,ev : MouseEvent){
-        //alert("footerSave");
-        masterView.dismissModal();
+    if(genomeView.genome)
+    {
+        document.getElementById("footerSave").onclick = function(this : HTMLElement,ev : MouseEvent){
+            //alert("footerSave");
+            masterView.dismissModal();
+        }
     }
 }
