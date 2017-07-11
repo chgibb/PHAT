30a2e9dc4de20f53cd53936608e2ff9fe038e664
Chris Gibb <chris.gibb@outlook.com>
2017-07-11 15:29:42 -0400

    Make figure rendering async

---
+++ src/req/renderer/circularGenomeBuilderRenderer/displayFigure.ts
@@ -0,0 +1,103 @@
+import * as fs from "fs";
+
+import * as cf from "./../circularFigure";
+import * as plasmid from "./../circularGenome/plasmid";
+import * as viewMgr from "./../viewMgr";
+import * as masterView from "./masterView";
+import {GenomeView} from "./genomeView";
+export function displayFigure(self : GenomeView) : Promise<void>
+{
+    return new Promise<void>((resolve,reject) => {
+
+        let masterView = <masterView.View>viewMgr.getViewByName("masterView");
+        try
+        {
+            document.body.removeChild(document.getElementById("controls"));
+        }
+        catch(err){}
+        try
+        {
+            //Remove the div this view is bound to
+            document.body.removeChild(document.getElementById(self.div));
+        }
+        catch(err){}
+        $("#"+self.div).remove();
+
+        let totalBP = 0;
+        for(let i = 0; i != self.genome.contigs.length; ++i)
+        {
+            totalBP += self.genome.contigs[i].bp;
+        }
+
+        //This is an unholy mess adapted from the example given inline in the
+        //angular source code https://github.com/angular/angular.js/blob/master/src/auto/injector.js
+        //We remove the div this view is bound to, recreate it and re render the angular template into it
+        //Then we pass the div into angular to compile the templates and then finally inject it all back into
+        //the page
+        let $div = $(
+            `
+                <div id="${self.div}" style="z-index=-1;">
+                    ${plasmid.add(
+                    {
+                        sequenceLength : totalBP.toString(),
+                        plasmidHeight : "{{genome.height}}",
+                        plasmidWidth : "{{genome.width}}"
+                    })}
+                        ${cf.getBaseFigureFromCache(self.genome)}
+                        ${(()=>{
+                            let res = "";
+                            for(let i = 0; i != self.genome.renderedCoverageTracks.length; ++i)
+                            {
+                                if(self.genome.renderedCoverageTracks[i].checked)
+                                {
+                                   res += (<any>fs.readFileSync(
+                                            cf.getCachedCoverageTrackPath(
+                                                self.genome.renderedCoverageTracks[i]
+                                            )));
+                                }
+                            }
+                            return res;
+                        })()}
+                        ${(()=>{
+                            let res = "";
+                            for(let i = 0; i != self.genome.renderedSNPTracks.length; ++i)
+                            {
+                                if(self.genome.renderedSNPTracks[i].checked)
+                                {
+                                    res += (<any>fs.readFileSync(
+                                            cf.getCachedSNPTrackPath(
+                                                self.genome.renderedSNPTracks[i]
+                                            )));
+                                }
+                            }
+                            return res;
+                        })()}
+                    ${plasmid.end()}
+                </div>
+                `
+            );
+        $(document.body).append($div);
+        angular.element(document).injector().invoke
+        (
+            function($compile : any)
+            {
+                //This should probably be done with an actual angular scope instead 
+                //of mutating the existing scope
+                let scope = angular.element($div).scope();
+                scope.genome = self.genome;
+                scope.alignData = self.alignData;
+                scope.markerOnClick = self.markerOnClick;
+                scope.figureNameOnClick = self.figureNameOnClick;
+                scope.inputRadiusOnChange = self.inputRadiusOnChange;
+                scope.showBPTrackOnChange = self.showBPTrackOnChange;
+                scope.exportSVG = self.exportSVG;
+                scope.showContigCreator = self.showContigCreator;
+                scope.postRender = self.postRender;
+                scope.firstRender = self.firstRender;
+                scope.div = self.div;
+                $compile($div)(scope);
+                resolve();
+            }
+        );
+    });
+}
--- src/req/renderer/circularGenomeBuilderRenderer/genomeView.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/genomeView.ts
@@ -12,7 +12,7 @@ import * as viewMgr from "./../viewMgr";
 import * as masterView from "./masterView";
 import {alignData} from "./../../alignData";
 import * as cf from "./../circularFigure";
-import * as plasmid from "./../circularGenome/plasmid";
+import {displayFigure} from "./displayFigure";
 
 import {writeLoadingModal} from "./writeLoadingModal";
 
@@ -117,104 +117,23 @@ export class GenomeView extends viewMgr.View
             
             //Only render markup when we explicitly need to
             //All figure updates are handled through angular bindings
-            if(this.firstRender){
-            masterView.loadingModal = true;
-            writeLoadingModal();
-            masterView.showModal();
+            if(this.firstRender)
+            {
+                masterView.loadingModal = true;
+                writeLoadingModal();
+                masterView.showModal();
+                
+                let self = this;
+                setTimeout(function(){
+                    displayFigure(self).then(() => {
+                        masterView.loadingModal = false;
+                        masterView.dismissModal();
+                    });
+                },10);
             
-            try
-            {
-                document.body.removeChild(document.getElementById("controls"));
+                this.firstRender = false;
             }
-            catch(err){}
-            try
-            {
-                //Remove the div this view is bound to
-                document.body.removeChild(document.getElementById(this.div));
-            }
-            catch(err){}
-            $("#"+this.div).remove();
-
-            let totalBP = 0;
-            for(let i = 0; i != this.genome.contigs.length; ++i)
-            {
-                totalBP += this.genome.contigs[i].bp;
-            }
-
-            //This is an unholy mess adapted from the example given inline in the
-            //angular source code https://github.com/angular/angular.js/blob/master/src/auto/injector.js
-            //We remove the div this view is bound to, recreate it and re render the angular template into it
-            //Then we pass the div into angular to compile the templates and then finally inject it all back into
-            //the page
-            let $div = $(
-                `
-                <div id="${this.div}" style="z-index=-1;">
-                    ${plasmid.add(
-                    {
-                        sequenceLength : totalBP.toString(),
-                        plasmidHeight : "{{genome.height}}",
-                        plasmidWidth : "{{genome.width}}"
-                    })}
-                        ${cf.getBaseFigureFromCache(this.genome)}
-                        ${(()=>{
-                            let res = "";
-                            for(let i = 0; i != self.genome.renderedCoverageTracks.length; ++i)
-                            {
-                                if(self.genome.renderedCoverageTracks[i].checked)
-                                {
-                                   res += (<any>fs.readFileSync(
-                                            cf.getCachedCoverageTrackPath(
-                                                self.genome.renderedCoverageTracks[i]
-                                            )));
-                                }
-                            }
-                            return res;
-                        })()}
-                        ${(()=>{
-                            let res = "";
-                            for(let i = 0; i != self.genome.renderedSNPTracks.length; ++i)
-                            {
-                                if(self.genome.renderedSNPTracks[i].checked)
-                                {
-                                    res += (<any>fs.readFileSync(
-                                            cf.getCachedSNPTrackPath(
-                                                self.genome.renderedSNPTracks[i]
-                                            )));
-                                }
-                            }
-                            return res;
-                        })()}
-                    ${plasmid.end()}
-                </div>
-                `
-            );
-            $(document.body).append($div);
-            angular.element(document).injector().invoke
-            (
-                function($compile : any)
-                {
-                    //This should probably be done with an actual angular scope instead 
-                    //of mutating the existing scope
-                    let scope = angular.element($div).scope();
-                    scope.genome = self.genome;
-                    scope.alignData = self.alignData;
-                    scope.markerOnClick = self.markerOnClick;
-                    scope.figureNameOnClick = self.figureNameOnClick;
-                    scope.inputRadiusOnChange = self.inputRadiusOnChange;
-                    scope.showBPTrackOnChange = self.showBPTrackOnChange;
-                    scope.exportSVG = self.exportSVG;
-                    scope.showContigCreator = self.showContigCreator;
-                    scope.postRender = self.postRender;
-                    scope.firstRender = self.firstRender;
-                    scope.div = self.div;
-                    $compile($div)(scope);
-                    masterView.loadingModal = false;
-                    masterView.dismissModal();
-                }
-            );
-            
-            this.firstRender = false;
-        }}
+        }
         return undefined;
     }
     public postRender() : void
