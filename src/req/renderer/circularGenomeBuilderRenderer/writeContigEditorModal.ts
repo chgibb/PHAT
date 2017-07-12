2f723ed05c2f0d1aacee68cd589ce5f9a1169b98
Chris Gibb <chris.gibb@outlook.com>
2017-07-12 16:19:49 -0400

    rewrite base figure templates onsave

--- src/req/renderer/circularGenomeBuilderRenderer/writeContigEditorModal.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/writeContigEditorModal.ts
@@ -1,8 +1,14 @@
+const Dialogs = require("dialogs");
+const dialogs = Dialogs();
+
 import * as viewMgr from "./../viewMgr";
 import * as masterView from "./masterView";
 import * as genomeView from "./genomeView";
 import * as cf from "./../circularFigure";
+import {reCacheBaseFigure} from "./reCacheBaseFigure";
+import {writeLoadingModal} from "./writeLoadingModal";
 let contig : cf.Contig;
+let editedAlias = "";
 export function setSelectedContigByUUID(uuid : string) : void
 {
     let masterView = <masterView.View>viewMgr.getViewByName("masterView");
@@ -13,6 +19,7 @@ export function setSelectedContigByUUID(uuid : string) : void
         if(genomeView.genome.contigs[i].uuid == uuid)
         {
             contig = genomeView.genome.contigs[i];
+            editedAlias = contig.alias;
             return;
         }
     }
@@ -22,10 +29,12 @@ export function setSelectedContigByUUID(uuid : string) : void
         if(genomeView.genome.customContigs[i].uuid == uuid)
         {
             contig = genomeView.genome.customContigs[i];
+            editedAlias = contig.alias;
             return;
         }
     }
 }
+
 export function writeContigEditorModal() : void
 {
     if(!contig)
@@ -36,7 +45,11 @@ export function writeContigEditorModal() : void
     let title = `Edit Contig ${contig.name}`;
     let body = ``;
     body += `
-    `;
+        <h4>Name in Reference:</h4>
+        <h5>${contig.name}</h5>
+        <h4>Display Name:</h4>
+        <h5 id="contigAlias" class="activeHover">${editedAlias}</h5>
+    `; 
     let footer = `
         <button type="button" class="btn btn-secondary" data-dismiss="modal" id="footerClose">Cancel</button>
         <button type="button" class="btn btn-primary" id="footerSave">Save changes</button>
@@ -51,6 +64,35 @@ export function writeContigEditorModal() : void
     }
 
     document.getElementById("footerSave").onclick = function(this : HTMLElement,ev : MouseEvent){
+        contig.alias = editedAlias;
+        masterView.contigEditorModalOpen = false;
+        masterView.dismissModal();
+
+        masterView.dataChanged();
+
+        masterView.loadingModal = true;
+        writeLoadingModal();
+
+        setTimeout(function(){
+            reCacheBaseFigure(genomeView.genome).then(() => {
+                masterView.loadingModal = false;
+                masterView.dismissModal();
+                genomeView.firstRender = true;
+                viewMgr.render();
+            });
+        },10);
+
+        viewMgr.render();
     
+    }
+
+    document.getElementById("contigAlias").onclick = function(this : HTMLElement,ev : MouseEvent){
+        masterView.dismissModal();
+        dialogs.prompt("Contig Name",editedAlias,function(text : string){
+            if(text)
+                editedAlias = text;
+            masterView.showModal();
+            viewMgr.render();
+        });
     }
 }
