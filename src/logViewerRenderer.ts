25ef7e3557948908939deba769dc835190235cd9
Chris Gibb <chris.gibb@outlook.com>
2017-07-14 15:36:36 -0400

    display sent logRecord

--- src/logViewerRenderer.ts
+++ src/logViewerRenderer.ts
@@ -1,4 +1,26 @@
+import * as fs from "fs";
+
 import * as electron from "electron";
 const ipc = electron.ipcRenderer;
 
+import {getReadableAndWritable} from "./req/getAppPath";
+import {LogRecord} from "./req/operations/atomicOperations";
 require("./req/renderer/commonBehaviour");
+
+ipc.on(
+    "logViewer",
+    function(event,arg){
+        let logRecord : LogRecord = arg.logRecord;
+        document.body.innerHTML = `
+            <p>Log for: ${logRecord.name}</p>
+            <p>${logRecord.status}</p>
+            <p>${logRecord.runTime}ms</p>
+            <p>${new Date(logRecord.startEpoch)}</p>
+            <pre>${
+                (<any>fs.readFileSync(
+                    getReadableAndWritable(`logs/${logRecord.uuid}/log`)
+                ).toString())
+            }</pre>
+        `;
+    }
+);
