b196776794ba2d890d0502828b0ee9b5b45d1e69
Chris Gibb <chris.gibb@outlook.com>
2017-07-14 16:51:41 -0400

    try to detect and highlight failure output

--- src/req/renderer/LogViewerRenderer/streamLogToNode.ts
+++ src/req/renderer/LogViewerRenderer/streamLogToNode.ts
@@ -4,6 +4,25 @@ import * as readline from "readline";
 import {getReadableAndWritable} from "./../../getAppPath";
 import {LogRecord} from "./../../operations/atomicOperations";
 
+function scanForError(line : string) : boolean
+{
+    let hasRetCode = /retCode/;
+    let hasError = /error/i;
+    let hasException = /exception/i;
+    let hasCrashed = /crashed/i;
+    let hasNo = /no/i;
+
+    if(hasError.test(line))
+        return true;
+    if(hasException.test(line))
+        return true;
+    if(hasCrashed.test(line))
+        return true;
+    if(hasNo.test(line))
+        return true;
+    return false;
+}
+
 export async function streamLogToNode(logRecord : LogRecord,node : HTMLElement) : Promise<void>
 {
     return new Promise<void>((resolve,reject) => {
@@ -16,7 +35,17 @@ export async function streamLogToNode(logRecord : LogRecord,node : HTMLElement) 
         );
 
         rl.on("line",function(line : string){
-            node.innerHTML += `<pre>${line}</pre>`;
+            if(logRecord.status != "failure")
+                node.innerHTML += `<pre>${line}</pre>`;
+            else
+            {
+                if(scanForError(line))
+                {
+                    node.innerHTML += `<pre><span style="color:red;font-size:x-large;"><b>${line}</b></span></pre>`;
+                }
+                else
+                    node.innerHTML += `<pre>${line}</pre>`;
+            }
         });
         
         rl.on("close",function(){
