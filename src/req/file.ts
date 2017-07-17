3673e9dbc047fe7d7d65655e68f1ae0f5a86ba74
Chris Gibb <chris.gibb@outlook.com>
2017-07-17 10:06:26 -0400

    refactored to use promises

--- src/req/file.ts
+++ src/req/file.ts
@@ -29,16 +29,27 @@ export class File
     }
 }
 
-export function importIntoProject(file : File) : void
+export function importIntoProject(file : File) : Promise<Error> | undefined
 {
     if(file.imported)
-        return;
-
-    fs.mkdirSync(getReadableAndWritable(`rt/imported/${file.uuid}`));
-    fse.copySync(getPath(file),getReadableAndWritable(`rt/imported/${file.uuid}/${file.alias}`));
-    file.path = `rt/imported/${file.uuid}/${file.alias}`;
-    file.imported = true;
-    
+        return undefined;
+    return new Promise<Error>((resolve,reject) => {
+        fs.mkdirSync(getReadableAndWritable(`rt/imported/${file.uuid}`));
+        fse.copy(
+            getPath(file),
+            getReadableAndWritable(`rt/imported/${file.uuid}/${file.alias}`),
+            function(err : Error){
+                if(err)
+                    reject(err);
+                else
+                {
+                    file.path = `rt/imported/${file.uuid}/${file.alias}`;
+                    file.imported = true;
+                    resolve(undefined);
+                }
+            }
+        );
+    });
 }
 
 export function getPath(file : File) : string
