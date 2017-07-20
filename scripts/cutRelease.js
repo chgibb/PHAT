0433a13b0aaa8af1b6a45ee12d06c2ea7bdaeefe
Chris Gibb <chris.gibb@outlook.com>
2017-07-20 14:44:27 -0400

    refactored into one entry point

--- scripts/cutRelease.js
+++ scripts/cutRelease.js
@@ -1,26 +1,20 @@
 const GitHubAPI = require("github-api");
-let args = process.argv.slice(2);
-const token = args[0];
-const tag_name = args[1];
-async function cutBetaRelease(user,repo,token,tag_name)
+module.exports = async function cutRelease(user,repo,token,tag_name,branch)
 {
     return new Promise((resolve,reject) => {
         const ghapi = new GitHubAPI({token : token});
         ghapi.getRepo(user,repo).createRelease({
             tag_name : tag_name,
-            target_commitish : "beta",
+            target_commitish : branch,
             name : tag_name,
             body : "",
             draft : false,
             prerelease : true
         }).then(() => {
-            console.log("success");
+            console.log(`Cut ${user}/${repo}@${tag_name} from ${branch}`);
             resolve();
         }).catch((err) => {
             reject(err);
         })
     });
 }
-(async function(){
-    await cutBetaRelease("chgibb","PHAT",args[0],args[1]);
-})();
---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+const generateDownloadTemplate = require("./generateDownloadTemplate");
+const cutRelease = require("./cutRelease");
+
+let args = process.argv.slice(2);
+const token = args[0];
+const tag_name = args[1];
+const type = args[2];
+
+(async function(){
+    await generateDownloadTemplate(type,tag_name);
+    await cutRelease("chgibb","PHAT",args[0],args[1],type);
+})();
