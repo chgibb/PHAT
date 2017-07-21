b5733b61e8b39608555aee65e558a287da071016
Chris Gibb <chris.gibb@outlook.com>
2017-07-21 11:14:55 -0400

    proto module to add commits to bottom of changelog on release

--- docs/docs/betaChangeLog.md
+++ docs/docs/betaChangeLog.md
@@ -1,2 +1,2 @@
 * Fixes element hovering in QC window [#449](https://github.com/chgibb/PHAT/issues/449)
-* Fix for log viewer not displaying [#532](https://github.com/chgibb/PHAT/issues/532)
+* Fix for log viewer not displaying [#532](https://github.com/chgibb/PHAT/issues/532) 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,8 @@
-const generateDownloadTemplate = require("./generateDownloadTemplate");
-const cutRelease = require("./cutRelease");
+const appendNewCommitsToChangeLog = require("./appendNewCommitsToChangeLog");
 const generateReleaseDocsFromLatest = require("./generateReleaseDocsFromLatest");
+const generateDownloadTemplate = require("./generateDownloadTemplate");
 const appendToAllReleases = require("./appendToAllReleases");
+const cutRelease = require("./cutRelease");
 
 let args = process.argv.slice(2);
 const token = args[0];
@@ -9,6 +10,7 @@ const tag_name = args[1];
 const type = args[2];
 
 (async function(){
+    await appendNewCommitsToChangeLog("chgibb","PHAT",args[0],args[1],type);
     await generateReleaseDocsFromLatest(tag_name);
     await generateDownloadTemplate(type,tag_name);
     await appendToAllReleases(tag_name,type);
