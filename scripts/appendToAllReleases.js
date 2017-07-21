8dffbf7d699d829719db33874622d02ba2d03466
Chris Gibb <chris.gibb@outlook.com>
2017-07-21 09:19:11 -0400

    changed link location and fixed linebreak

--- scripts/appendToAllReleases.js
+++ scripts/appendToAllReleases.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 
 module.exports = function appendToAllReleases(tag_name,branch)
 {
-    fs.appendFileSync(`docs/allReleases.md`,`[${new Date}: PHAT@${tag_name} cut from ${branch}](https://chgibb.github.io/PHAT/releases/${tag_name}/index)`);
+    fs.appendFileSync(`docs/allReleases.md`,`${new Date}: [PHAT@${tag_name}](https://chgibb.github.io/PHAT/releases/${tag_name}/index) cut from ${branch}  ${"\n"}`);
 }
