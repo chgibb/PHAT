dbfec254e258bc44f5fbbd5356e9b21a038a1d80
Chris Gibb <chris.gibb@outlook.com>
2017-07-21 09:23:14 -0400

    added comments

--- scripts/generateReleaseDocsFromLatest.js
+++ scripts/generateReleaseDocsFromLatest.js
@@ -7,6 +7,9 @@ module.exports = async function generateReleaseDocsFromLatest(tag_name)
     return new Promise((resolve,reject) => {
         let walker = walk.walk(`docs/docs/latest`,{});
         fs.mkdirSync(`docs/docs/releases/${tag_name}`);
+
+        //Patch all doc links to point from latest to the version of this deployment
+        //Also patch occurences of $TAGNAME$ with this version
         walker.on("file",function(root,fileStats,next){
             let file = fs.readFileSync(`${root}/${fileStats.name}`).toString();
             file = file.replace(/\/latest\//g,`/releases/${tag_name}/`);
