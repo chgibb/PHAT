bf6993ef4b41f55beecd7d7d0c10bbbab936a8e9
Chris Gibb <chris.gibb@outlook.com>
2017-07-21 09:26:05 -0400

    removed logging

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,13 +9,9 @@ const tag_name = args[1];
 const type = args[2];
 
 (async function(){
-    console.log("1");
     await generateReleaseDocsFromLatest(tag_name);
-    console.log("2");
     await generateDownloadTemplate(type,tag_name);
-    console.log("3");
     await appendToAllReleases(tag_name,type);
-    console.log("4");
     await cutRelease("chgibb","PHAT",args[0],args[1],type);
 
 })().catch((err) => {
