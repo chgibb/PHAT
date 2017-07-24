0cb029fd3354ec3477fce3ea2326cfa078d62956
chgibb <chris.gibb@outlook.com>
2017-07-24 11:18:22 -0400

    removed trailing char

--- scripts/install/copyBootstrap.bash
+++ scripts/install/copyBootstrap.bash
@@ -1,3 +1,3 @@
-(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility\
+(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 
 cp node_modules/bootstrap/dist/css/bootstrap.min.css forDist/bootstrap.min.css
