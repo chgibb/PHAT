45be6c87d002c32ab72c9db39bf303d619da5148
Chris Gibb <chris.gibb@outlook.com>
2017-07-31 16:13:03 -0400

    Stop trying to exit on failure

--- src/guiTests/req/closeToolBar.ts
+++ src/guiTests/req/closeToolBar.ts
@@ -5,13 +5,13 @@ export async function closeToolBar() : Promise<void>
     return new Promise<void>((resolve,reject) => {
         setTimeout(function(){
             console.log("closing toolbar");
-            setTimeout(function(){
+            setTimeout(function(){  
                 let toolBar = winMgr.getWindowsByName("toolBar");
                 if(!toolBar || toolBar.length > 1 || toolBar.length == 0)
                 {
                     console.log("Failed to open tool bar!");
                     console.log(toolbar);
-                    process.exit(1);
+                    //process.exit(1);
                 }
                 toolBar[0].close();
                 resolve();
