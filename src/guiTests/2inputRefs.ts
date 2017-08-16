082dae2557d6854b6202ab3ae33a55cbc69f7b3a
Chris Gibb <chris.gibb@outlook.com>
2017-08-16 11:33:41 -0400

    open ref seq tab as part of test

--- src/guiTests/2inputRefs.ts
+++ src/guiTests/2inputRefs.ts
@@ -4,6 +4,7 @@ require("./../req/main/main");
 import {openProjectsView} from "./req/projectSelection/openProjectsView";
 import {openFirstProject} from "./req/projectSelection/openFirstProject";
 import {openInputWindow} from "./req/input/openInputWindow";
+import {openRefSeqTab} from "./req/input/openRefSeqTab";
 import {inputHPV16Ref} from "./req/input/inputHPV16Ref";
 import {indexRefs} from "./req/input/indexRefs";
 import {indexSuccess} from "./req/input/indexSuccess";
@@ -14,6 +15,7 @@ async function runTest() : Promise<void>
     await openProjectsView();
     await openFirstProject();
     await openInputWindow();
+    await openRefSeqTab();
     await inputHPV16Ref();
     await indexRefs();
     await indexSuccess().then(async (val) => {
