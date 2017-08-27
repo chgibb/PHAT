3a2429e3c2ca552ab755cf0418cc81e26e2bef7e
Chris Gibb <chris.gibb@outlook.com>
2017-08-27 12:02:27 -0400

    test link status

--- src/req/tests/testL6R1HPV16AlignImportedImporting.ts
+++ src/req/tests/testL6R1HPV16AlignImportedImporting.ts
@@ -1,9 +1,12 @@
 import * as fs from "fs";
 
 import * as atomic from "./../operations/atomicOperations";
+import {Fasta} from "./../fasta";
 import {InputBamFile} from "./../operations/InputBamFile";
+import * as hpv16Ref from "./hpv16Ref";
 import * as L6R1HPV16AlignImported from "./L6R1HPV16AlignImported";
 import {getUnSortedBam,getSortedBam,getSortBamIndex,} from "./../alignData";
+import {getLinkableRefSeqs} from "./../getLinkableRefSeqs";
 export async function testL6R1HPV16AlignImportedImporting() : Promise<void>
 {
     return new Promise<void>((resolve,reject) => {
@@ -31,6 +34,21 @@ export async function testL6R1HPV16AlignImportedImporting() : Promise<void>
                     console.log(`${op.alignData.alias} has correct number of unmapped reads`);
                 else
                     return reject();
+
+                let res = getLinkableRefSeqs(<Array<Fasta>>[hpv16Ref.get()],op.alignData);
+
+                if(!res)
+                {
+                    console.log(`Failed to determine link status for ${op.alignData.alias}`);
+                    return reject();
+                }
+                if(res[0].linkable == true && res[0].uuid == hpv16Ref.get().uuid)
+                    console.log(`Successfully determined link status to HPV16 for ${op.alignData.alias}`);
+                else
+                {
+                    console.log(`${op.alignData.alias} is not linkable to HPV16`);
+                    return reject();
+                }
 
                 L6R1HPV16AlignImported.set(op.alignData);
 
