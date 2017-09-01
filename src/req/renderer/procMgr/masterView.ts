b5c96d3269cf11539c3fa786354a8aa10f160c2a
Chris Gibb <chris.gibb@outlook.com>
2017-09-01 13:44:39 -0400

    proto table rendering

--- src/req/renderer/procMgr/masterView.ts
+++ src/req/renderer/procMgr/masterView.ts
@@ -1,24 +1,48 @@
 import * as viewMgr from "./../viewMgr";
 import * as procView from "./procView";
+import {PIDInfo} from "./../../PIDInfo";
 export class View extends viewMgr.View
 {
     public views : Array<viewMgr.View>
+    public pids : Array<PIDInfo>;
+    public currentView : "procView";
+    public firstRender : boolean;
     public constructor(div : string)
     {
         super("masterView",div);
         this.views = new Array<viewMgr.View>();
+        this.pids = new Array<PIDInfo>();
+        this.currentView = "procView";
+        this.firstRender = true;
     }
     public onMount() : void
     {
-        procView.addView(this.views,"view");
+        procView.addView(this.views,"procView");
     }
     public onUnMount() : void{}
     public renderView() : string
     {
-        return ``;
+        if(this.firstRender)
+        {
+            this.firstRender = false;
+            return `
+                <div id="procView" style="width:100%">
+                </div>
+            `;
+        }
+        else
+        {
+            let idx = viewMgr.getIndexOfViewByName(this.currentView,this.views);
+            this.views[idx].render();
+            return undefined;
+        }
     }
     public postRender() : void{}
-    public dataChanged() : void{}
+    public dataChanged() : void
+    {
+        let procView = <procView.View>viewMgr.getViewByName("procView",this.views);
+        procView.pids = this.pids;
+    }
     public divClickEvents(event : JQueryEventObject) : void{}
 }
 export function addView(arr : Array<viewMgr.View>,div : string) : void
--- src/req/renderer/procMgr/procView.ts
+++ src/req/renderer/procMgr/procView.ts
@@ -1,16 +1,66 @@
 import * as viewMgr from "./../viewMgr";
-
+import {PIDInfo} from "./../../PIDInfo";
+import trimPath from "./../../trimPath";
+import formatByteString from "./../formatByteString";
 export class View extends viewMgr.View
 {
+    public pids : Array<PIDInfo>;
     public constructor(div : string)
     {
         super("procView",div);
+        this.pids = new Array<PIDInfo>();
     }
     public onMount() : void{}
     public onUnMount() : void{}
     public renderView() : string
     {
-        return ``;
+        this.pids = this.pids.sort(function(a : PIDInfo,b : PIDInfo) : number{
+            if(!a.memory || !b.memory)
+                return 0;
+            return a.memory - b.memory;
+        });
+        return `
+            <div id="procTable" style="width:100%">
+                <table style="width:100%;">
+                    <tr>
+                        <th>Name</th>
+                        <th>First Arg</th>
+                        <th>CPU %</th>
+                        <th>RAM</th>
+                    </tr>
+
+                ${(()=>{
+                    let res = "";
+                    for(let i = 0; i != this.pids.length; ++i)
+                    {
+                        res += `<tr>`;
+                        if(!this.pids[i].isPHAT)
+                            res += `<td>${this.pids[i].command ? trimPath(this.pids[i].command) : ""}</td>`;
+                        else
+                        {
+                            res += `<td>phat</td>`;
+                        }
+                        if(!this.pids[i].isPHAT)
+                        {
+                            res += `<td>${this.pids[i].arguments[0] ? trimPath(this.pids[i].arguments[0]) : ""}`;
+                        }
+                        if(this.pids[i].isPHATMain)
+                        {
+                            res += `<td>main.js</td>`;
+                        }
+                        if(this.pids[i].isPHATRenderer)
+                        {
+                            res += `<td>${trimPath(this.pids[i].url)}</td>`;
+                        }
+                        res += `<td>${this.pids[i].cpu ? this.pids[i].cpu : ""}</th>`;
+                        res += `<td>${this.pids[i].memory ? formatByteString(this.pids[i].memory) : ""}</th>`;
+                        res += `</tr>`;
+                    }
+                    return res;
+                })()}
+                </table>
+            </div>
+        `;
     }
     public postRender() : void{}
     public dataChanged() : void{}
