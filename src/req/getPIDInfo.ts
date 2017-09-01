f4821640825440b10474b50393fd1f74b555053a
Chris Gibb <chris.gibb@outlook.com>
2017-09-01 11:09:25 -0400

    added method for resource usage

--- src/req/getPIDInfo.ts
+++ src/req/getPIDInfo.ts
@@ -3,10 +3,45 @@ const BrowserWindow = electron.remote.BrowserWindow;
 
 
 const psNode = require("ps-node");
+const pidUsage = require("pidusage");
 
 import {PIDInfo} from "./PIDInfo";
 
+/**
+ * Returns CPU % and RAM use (in bytes) for the specified pid
+ * 
+ * @export
+ * @param {number} pid 
+ * @returns {Promise<{cpu : number,memory : number}>} 
+ */
+export async function getPIDUsage(pid : number) : Promise<{cpu : number,memory : number}>
+{
+    return new Promise<{cpu : number,memory : number}>((resolve,reject) => {
+        pidUsage.stat(pid,function(err : Error,stat : any){
+            if(err)
+                reject(err);
+            pidUsage.unmonitor(pid);
+            //sometimes the process has already closed by the time we get a reply from main about pids being used
+            //this access will throw if the process we're tring to get usage for has already exited
+            try
+            {
+                resolve({cpu : stat.cpu,memory : stat.memory});
+            }
+            catch(err)
+            {
+                reject(err);
+            }
+        });
+    });
+}
 
+/**
+ * Returns PIDInfo results for the specified pid detailing pid, ppid, command and args
+ * 
+ * @export
+ * @param {number} pid 
+ * @returns {Promise<Array<PIDInfo>>} 
+ */
 export async function getPIDInfo(pid : number) : Promise<Array<PIDInfo>>
 {
     let res = new Array<PIDInfo>();
