0c996347060cae8044c84b32a1c5a38f078ec24d
Chris Gibb <chris.gibb@outlook.com>
2017-10-07 12:05:05 -0400

    fleshed out methods

--- src/req/renderer/circularGenomeBuilderRenderer/loadingSpinner.ts
+++ src/req/renderer/circularGenomeBuilderRenderer/loadingSpinner.ts
@@ -5,10 +5,16 @@
  */
 export function showGenericLoadingSpinnerInNavBar() : void
 {
-    
+    if(!document.getElementById("navBarLoadingSpinner").classList.contains("spinner-loader"))
+    {
+        document.getElementById("navBarLoadingSpinner").classList.add("spinner-loader");
+    }
 }
 
 export function hideSpinnerInNavBar() : void
 {
-    
+    if(document.getElementById("navBarLoadingSpinner").classList.contains("spinner-loader"))
+    {
+        document.getElementById("navBarLoadingSpinner").classList.remove("spinner-loader");
+    }
 }
