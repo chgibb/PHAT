8f7c89f9d7bc8345a8865471998ac8c0fb6c2cd3
Chris Gibb <chris.gibb@outlook.com>
2017-10-11 12:45:11 -0400

    skeleton open prompt operation

---
+++ src/req/operations/OpenNoSamHeaderPrompt.ts
@@ -0,0 +1,32 @@
+import * as atomic from "./atomicOperations";
+import * as winMgr from "./../main/winMgr";
+import {InputBamFile} from "./InputBamFile";
+export class OpenNoSamHeaderPrompt extends atomic.AtomicOperation
+{
+    public inputBamFile : InputBamFile;
+    constructor()
+    {
+        super();
+        this.ignoreScheduler = true;
+    }
+    public setData(data : InputBamFile)
+    {
+        this.inputBamFile = data;
+    }
+    public run() : void
+    {
+        this.logRecord = atomic.openLog("openNoSamHeaderPrompt","Open No Sam Header Prompt");
+        winMgr.windowCreators["noSamHeaderPrompt"].Create();
+
+        let prompts = winMgr.getWindowsByName("noSamHeaderPrompt");
+        let prompt = prompts[prompts.length - 1];
+        let self = this;
+        setTimeout(
+            function(){
+                prompt.webContents.send("noSamHeaderPrompt",self.inputBamFile);
+            }
+        );
+        this.setSuccess(this.flags);
+        this.update();
+    }
+}
