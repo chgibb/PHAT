fda54b160bb6d8d3dea47c232dea169de5ab3ffa
Chris Gibb <chris.gibb@outlook.com>
2017-10-12 09:53:24 -0400

    barebones ipc handles

--- src/noSamHeaderPromptRenderer.ts
+++ src/noSamHeaderPromptRenderer.ts
@@ -1,2 +1,28 @@
 import * as electron from "electron";
 const ipc = electron.ipcRenderer;
+
+import {GetKeyEvent,KeySubEvent} from "./req/ipcEvents";
+
+import * as viewMgr from "./req/renderer/viewMgr";
+
+const $ = require("jquery");
+(<any>window).$ = $;
+import "./req/renderer/commonBehaviour";
+
+$(function(){
+    ipc.send(
+        "getKey",
+        <GetKeyEvent>{
+            channel : "input",
+            key : "fastaInputs",
+            replyChannel : "noSamHeaderPrompt",
+            action : "getKey"
+        }
+    );
+
+    ipc.on("noSamHeaderPrompt",function(event : Electron.IpcMessageEvent,arg : any){
+        if(arg.action == "getKey" || arg.action == "keyChange")
+        {
+        }
+    });
+});
