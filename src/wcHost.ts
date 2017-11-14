dc314949c43eee02c8352e1ad4811e3e5ff5c48e
Chris Gibb <chris.gibb@outlook.com>
2017-11-14 14:43:32 -0500

    removed logging

--- src/wcHost.ts
+++ src/wcHost.ts
@@ -2,6 +2,5 @@ import * as electron from "electron";
 const ipc = electron.ipcRenderer;
 
 ipc.on("changeGuestInstance",function(event : Electron.IpcMessageEvent,arg : any){
-    console.log(arg);
     document.getElementsByTagName("webview")[0].setAttribute("guestinstance",arg.guestinstance);
 });
