00601c9976ee1f9d991b9948dbd46edfc612df4a
Chris Gibb <chris.gibb@outlook.com>
2017-11-15 09:19:27 -0500

    pass through console messages

--- src/wcHost.ts
+++ src/wcHost.ts
@@ -2,5 +2,9 @@ import * as electron from "electron";
 const ipc = electron.ipcRenderer;
 
 ipc.on("changeGuestInstance",function(event : Electron.IpcMessageEvent,arg : any){
-    document.getElementsByTagName("webview")[0].setAttribute("guestinstance",arg.guestinstance);
+    let webview = document.getElementsByTagName("webview")[0];
+    webview.setAttribute("guestinstance",arg.guestinstance);
+    webview.addEventListener("console-message",function(e : any){
+        console.log(`${e.message}`);
+    });
 });
