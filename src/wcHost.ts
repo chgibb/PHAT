db1ad4cf28e419ef880822ca5ab014743412b3b9
Chris Gibb <chris.gibb@outlook.com>
2017-11-16 13:07:43 -0500

    fixed resize

--- src/wcHost.ts
+++ src/wcHost.ts
@@ -1,9 +1,11 @@
 import * as electron from "electron";
 const ipc = electron.ipcRenderer;
 
+let webview = document.getElementsByTagName("webview")[0];
+
 //on message to attach new wc
 ipc.on("changeGuestInstance",function(event : Electron.IpcMessageEvent,arg : any){
-    let webview = document.getElementsByTagName("webview")[0];
+    
     //attach new wc
     webview.setAttribute("guestinstance",arg.guestinstance);
     //poll until attach has completed
