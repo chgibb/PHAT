ab27dcac0c303f004bf7a546a551b63a8e5dd0b7
Chris Gibb <chris.gibb@outlook.com>
2017-12-27 18:01:40 -0500

    broke ref selecting into module

--- src/guiTests/4alignReads.ts
+++ src/guiTests/4alignReads.ts
@@ -5,6 +5,7 @@ import {openProjectsView} from "./req/projectSelection/openProjectsView";
 import {openFirstProject} from "./req/projectSelection/openFirstProject";
 import {openAlignWindow} from "./req/align/openAlignWindow";
 import {selectFirstTwoReads} from "./req/align/selectFirstTwoReads";
+import {selectFirstRef} from "./req/align/selectFirstRef";
 import {closeToolBar} from "./req/closeToolBar";
 
 async function runTest() : Promise<void>
@@ -13,6 +14,7 @@ async function runTest() : Promise<void>
     await openFirstProject();
     await openAlignWindow();
     await selectFirstTwoReads();
+    await selectFirstRef();
     await closeToolBar();
 }
 setTimeout(function(){
--- src/guiTests/req/align/selectFirstRef.ts
+++ src/guiTests/req/align/selectFirstRef.ts
@@ -1,26 +1,25 @@
 import * as winMgr from "./../../../req/main/winMgr";
 import * as dataMgr from "./../../../req/main/dataMgr";
-import Fastq from "./../../../req/fastq";
-export async function selectFirstTwoReads() : Promise<void>
+import {Fasta} from "./../../../req/fasta";
+export async function selectFirstRef() : Promise<void>
 {
     return new Promise<void>((resolve,reject) => {
         setTimeout(function(){
-            console.log("selecting first two reads");
+            console.log("selecting first ref seq");
             let align = winMgr.getFreeWebContents();
             if(!align || align.length == 0)
             {
                 console.log("Failed to open align window");
                 process.exit(1);
             }
-            let fastqs : Array<Fastq> = dataMgr.getKey("input","fastqInputs");
-            if(!fastqs)
+            let fasta : Fasta = dataMgr.getKey("input","fastaInputs")[0];
+            if(!fasta)
             {
-                console.log("failed to input reads");
+                console.log("failed to input ref seq");
                 process.exit(1);
             }
             align[0].executeJavaScript(`
-                document.getElementById("${fastqs[0].uuid}").click();
-                document.getElementById("${fastqs[1].uuid}").click();
+                document.getElementById("${fasta.uuid}").click();
             `);
             resolve();
         },500);
