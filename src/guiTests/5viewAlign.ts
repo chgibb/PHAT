606ed3ff007408048624580f0929db7d168faac8
Chris Gibb <chris.gibb@outlook.com>
2017-12-28 14:20:27 -0500

    renamed module and added checkbox toggles

--- src/guiTests/5viewAlign.ts
+++ src/guiTests/5viewAlign.ts
@@ -4,7 +4,9 @@ require("./../req/main/main");
 import {openProjectsView} from "./req/projectSelection/openProjectsView";
 import {openFirstProject} from "./req/projectSelection/openFirstProject";
 import {openOutputWindow} from "./req/output/openOutputWindow";
-import {openOptionsPanel} from "./req/output/openOptionsPanel";
+import {toggleOptionsPanel} from "./req/output/toggleOptionsPanel";
+import {toggleMinimumCoverageCheckBox} from "./req/output/toggleMinimumCoverageCheckBox";
+import {toggleMinimumVariableFrequencyCheckBox} from "./req/output/toggleMinimumVariableFrequencyCheckBox";
 
 import {closeToolBar} from "./req/closeToolBar";
 
@@ -13,7 +15,9 @@ async function runTest() : Promise<void>
     await openProjectsView();
     await openFirstProject();
     await openOutputWindow();
-    await openOptionsPanel();
+    await toggleOptionsPanel();
+    await toggleMinimumCoverageCheckBox();
+    await toggleMinimumVariableFrequencyCheckBox();
 
     await closeToolBar();
 }
--- src/guiTests/req/output/toggleMinimumCoverageCheckBox.ts
+++ src/guiTests/req/output/toggleMinimumCoverageCheckBox.ts
@@ -1,16 +1,16 @@
 import * as winMgr from "./../../../req/main/winMgr";
 
 /**
- * Opens the options slide out panel in the first output window
+ * Toggles the Minimum Coverage checkbox in the first output window
  * 
  * @export
  * @returns {Promise<void>} 
  */
-export async function openOptionsPanel() : Promise<void>
+export async function toggleMinimumCoverageCheckBox() : Promise<void>
 {
     return new Promise<void>((resolve,reject) => {
         setTimeout(function(){
-            console.log("opening options panel");
+            console.log("toggling minimum coverage checkbox")
             let output = winMgr.getFreeWebContents();
             if(!output || output.length == 0)
             {
@@ -18,7 +18,7 @@ export async function openOptionsPanel() : Promise<void>
                 process.exit(1);
             }
             output[0].executeJavaScript(`
-                document.getElementById("rightPanel").click();
+                document.getElementById("minimumCoverage").click();
             `);
             resolve();
         },500);
