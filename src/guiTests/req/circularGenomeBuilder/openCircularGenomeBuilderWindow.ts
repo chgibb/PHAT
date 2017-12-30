14dff3c952b0ec95ce845629c9e017088f71cc7d
Chris Gibb <chris.gibb@outlook.com>
2017-12-30 13:34:56 -0500

    gui test to open genome builder

---
+++ src/guiTests/req/circularGenomeBuilder/openCircularGenomeBuilderWindow.ts
@@ -0,0 +1,23 @@
+import * as winMgr from "./../../../req/main/winMgr";
+
+/**
+ * Opens a circularGenomeBuilder window
+ * 
+ * @export
+ * @returns {Promise<void>} 
+ */
+export async function openCircularGenomeBuilderWindow() : Promise<void>
+{
+    return new Promise<void>((resolve,reject) => {
+        setTimeout(function(){
+            console.log("opening circularGenomeBuilder window");
+            setTimeout(function(){
+                let toolBar = winMgr.getWindowsByName("toolBar");
+                toolBar[0].webContents.executeJavaScript(`
+                    document.getElementById("circularGenomeBuilder").click();
+                `);
+                resolve();
+            },500);
+        },500);
+    });
+}
