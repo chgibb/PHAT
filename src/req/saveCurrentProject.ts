bf21a49b8307c2d3ac47044f63def4ffaff12e06
Chris Gibb <chris.gibb@outlook.com>
2017-12-30 14:15:43 -0500

    progress callback interval

--- src/req/saveCurrentProject.ts
+++ src/req/saveCurrentProject.ts
@@ -14,19 +14,29 @@ export function saveCurrentProject(
     return new Promise<void>((resolve,reject) => {
         mkdirp.sync(getReadableAndWritable("projects"));
         let projectTarBall = getTarBallPath(proj);
+
         const ostream = fs.createWriteStream(projectTarBall);
+
+        let cbInterval : NodeJS.Timer = setInterval(function(){
+            cb(0,ostream.bytesWritten);
+        },1000);
+        
         ostream.on("error",(error : string) => {
+            clearInterval(cbInterval);
             reject(error);
         });
         ostream.on("close",() => {
+            clearInterval(cbInterval);
             resolve();
         });
       
         let pack = tarfs.pack(getReadableAndWritable("rt")).pipe(ostream);
         pack.on("error",function(error : string){
+            clearInterval(cbInterval);
             reject(error);
         });
         pack.on("close",function(){
+            clearInterval(cbInterval);
             resolve();
         })
 
