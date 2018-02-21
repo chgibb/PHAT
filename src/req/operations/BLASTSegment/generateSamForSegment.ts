6398f57184ad0c83ac0461ddf0978dc23b0e0175
Chris Gibb <chris.gibb@outlook.com>
2018-02-21 09:52:11 -0500

    populate reads prop on success

--- src/req/operations/BLASTSegment/generateSamForSegment.ts
+++ src/req/operations/BLASTSegment/generateSamForSegment.ts
@@ -44,6 +44,7 @@ export function generateSamForSegment(
                         setTimeout(
                             function(){
                                 samStream.end();
+                                segmentResult.reads = totalReads;
                                 resolve(totalReads);
                             },
                             500
