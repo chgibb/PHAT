c8b35e305eb436fd04fe913d71d5889aba730766
Chris Gibb <chris.gibb@outlook.com>
2018-03-17 13:54:07 -0400

    expanded fragment test

--- __tests__/getFragments.test.ts
+++ __tests__/getFragments.test.ts
@@ -13,6 +13,21 @@ it(`should extract fragments 1`,async () => {
         {
             expect(unMappedFragments).toBeUndefined();
         }
+
+        else if(read.SEQ == "CTACACGACGCTCTTCCGATCTCTTGTGTAACTATTGTGTGATGCAACATAAATAAACTTATTGTTTCAACACCTACTAATTGTGTTGTGGTTATTCATTGTATATAAACTATATTTGCTACAATCTGTTTTTGTTTTATATATACTATAT")
+        {
+            expect(unMappedFragments).toBeDefined();
+            expect(unMappedFragments!.length).toBe(1);
+            expect(unMappedFragments![0]).toBe("ACGCTCTTCCGATCT");
+        }
+
+        else if(read.SEQ == "ACGACGCTCTTCCGATCTAATAAACCTTATTGGTTACAACGAGCACAGGGCCACAATAATGGCATTTGTTGGGGTAACCAACTATTTGTTACTGTTGTTGATACTACACGCAGTACAAATATGTCATTATGTGCTGCCATATCTACTTCAG")
+        {
+            expect(unMappedFragments).toBeDefined();
+            expect(unMappedFragments!.length).toBe(2);
+            expect(unMappedFragments![0]).toBe("CG");
+            expect(unMappedFragments![1]).toBe("CGATCT");
+        }
     });
 
     expect(total).toBe(5378);
