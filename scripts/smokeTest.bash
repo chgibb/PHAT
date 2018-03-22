1e0099858c2f04b016ca129f086f344b6860a60e
Chris Gibb <chris.gibb@outlook.com>
2018-03-22 10:30:30 -0400

    reworked test preping and running

--- scripts/smokeTest.bash
+++ scripts/smokeTest.bash
@@ -1,22 +1,9 @@
  #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 
-mv node_modules not_node_modules
-
-cd tests
-
-node tests.js
-if [ $? != 0 ]; then
-	cat jobVerboseLog.txt
-	cat jobErrorLog.txt
-	exit 1
-fi
-
-cd ../
 
 for f in guiTests/*.js
 do
-	#bash scripts/build.bash
 
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		cd phat-linux-x64
@@ -51,5 +38,3 @@ do
 	cd ../
 
 done
-
-
