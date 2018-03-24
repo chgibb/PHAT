254980d1595136bf39b1785317374169fa3d5623
Chris Gibb <chris.gibb@outlook.com>
2018-03-24 17:51:43 -0400

    added publish script

---
+++ scripts/publish.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compatibility
+
+rm lib/*.js
+rm __tests__/*.js
+
+npm version $1
+git push origin --tags
+npm publish 
+npm pack
