87df2ae5db241d17dd60a86deebaf4020b901fec
Chris Gibb <chris.gibb@outlook.com>
2018-03-24 11:42:51 -0400

    fixed precision

--- src/req/renderer/formatByteString.ts
+++ src/req/renderer/formatByteString.ts
@@ -10,6 +10,16 @@
 //Adapted from https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
 export default function formatByteString(bytes : number) : string
 {
+    if(!bytes || bytes <= 0)
+        return "0";
     let i = Math.floor( Math.log(bytes) / Math.log(1024) );
-    return (bytes / parseFloat(Math.pow(1024, i).toFixed(2)) * 1 + ['B', 'kB', 'MB', 'GB', 'TB'][i]);
+    let res = (bytes / parseInt(Math.pow(1024, i).toFixed(2)) * 1).toFixed(2);
+
+    //if the result ends with ".00", trim it off
+    if(res[res.length-1] == "0" && res[res.length-2] == "0" && res[res.length-3] == ".")
+        res = res.substring(0,res.length-3);
+
+    res +=  ['B', 'kB', 'MB', 'GB', 'TB'][i];
+
+    return res;
 }
