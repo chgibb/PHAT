d1c87cb5417ae388178f7e6d06d63060c2ab1737
Chris Gibb <chris.gibb@outlook.com>
2018-06-20 17:24:48 -0400

    updated function use

--- src/req/operations/BLASTSegment/getReadsWithLargeUnMappedFragments.ts
+++ src/req/operations/BLASTSegment/getReadsWithLargeUnMappedFragments.ts
@@ -1,20 +1,20 @@
 /// <reference path="./../../../../node_modules/@chgibb/unmappedcigarfragments/lib/lib.ts" />
 
-import {getReads,SAMRead} from "./../../../../node_modules/@chgibb/unmappedcigarfragments/lib/lib";
+import {getReads,SAMRead,ReadFragment} from "./../../../../node_modules/@chgibb/unmappedcigarfragments/lib/lib";
 
 export function getReadsWithLargeUnMappedFragments(file : string,start : number,stop : number,progress : (parsedReads : number) => void) : Promise<Array<SAMRead>>
 {
     return new Promise<Array<SAMRead>>(async (resolve,reject) => {
         let res : Array<SAMRead> = new Array<SAMRead>();
         let parsedReads = 0;
-        await getReads(file,start,stop,function(read : SAMRead,unMappedFragments : Array<string>){
+        await getReads(file,start,stop,function(read : SAMRead,fragments : Array<ReadFragment>){
             parsedReads++;
             progress(parsedReads);
-            if(!unMappedFragments || unMappedFragments.length == 0)
+            if(!fragments || fragments.length == 0)
                 return;
-            for(let i = 0; i != unMappedFragments.length; ++i)
+            for(let i = 0; i != fragments.length; ++i)
             {
-                if(unMappedFragments[i].length >= 15)
+                if(fragments[i].type == "unmapped" && fragments[i].seq.length >= 15)
                 {
                     res.push(read);
                     break;
