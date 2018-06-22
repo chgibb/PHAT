b5650e7b029a03068f7e123bb958d76a7ca8ee30
Chris Gibb <chris.gibb@outlook.com>
2018-06-22 17:06:20 -0400

    more fragment tests

--- src/req/tests/testBLASTSegment0To1000L6R1HPV16Alignment.ts
+++ src/req/tests/testBLASTSegment0To1000L6R1HPV16Alignment.ts
@@ -1,6 +1,6 @@
 import * as atomic from "./../operations/atomicOperations";
 import {BLASTSegment} from "./../operations/BLASTSegment";
-import {getBLASTReadResults} from "./../BLASTSegmentResult";
+import {getBLASTReadResults,getBLASTFragmentResults} from "./../BLASTSegmentResult";
 
 export async function testBLASTSegment0To1000L6R1HPV16Alignment() : Promise<void>
 {
@@ -22,21 +22,48 @@ export async function testBLASTSegment0To1000L6R1HPV16Alignment() : Promise<void
 
             else if(op.flags.success)
             {
-                let results = await getBLASTReadResults(op.blastSegmentResult,0,0);
-                if(results.length == 2)
+                let readResults = await getBLASTReadResults(op.blastSegmentResult,0,0);
+                if(readResults.length == 2)
                     console.log(`BLAST segment has correct number of results in whole file`);
                 else
                     return reject();
                 
-                results = await getBLASTReadResults(op.blastSegmentResult,0,1000);
-                if(results.length == 2)
+                readResults = await getBLASTReadResults(op.blastSegmentResult,0,1000);
+                if(readResults.length == 2)
                     console.log(`BLAST segment has correct number of results in range`);
                 else
                     return reject();
                 
-                results = await getBLASTReadResults(op.blastSegmentResult,1001,3000);
-                if(results.length == 0)
+                readResults = await getBLASTReadResults(op.blastSegmentResult,1001,3000);
+                if(readResults.length == 0)
                     console.log(`BLAST segment has correct number of results in range`);
+                else
+                    return reject();
+
+                let fragmentResults = await getBLASTFragmentResults(op.blastSegmentResult);
+                
+                if(fragmentResults.length == 2)
+                    console.log(`BLAST segment has correct number of fragments`);
+                else
+                    return reject();
+                
+                if(fragmentResults[0].seq == "ACGCTCTTCCGATCT")
+                    console.log(`First fragment has correct sequence`);
+                else 
+                    return reject();
+                
+                if(fragmentResults[0].results.noHits == true)
+                    console.log(`First fragment has correct results`);
+                else
+                    return reject();
+
+                if(fragmentResults[1].seq == "AATAATACTAAACTAC")
+                    console.log(`Second fragment has correct sequence`);
+                else 
+                    return reject();
+                
+                if(fragmentResults[1].results.noHits == true)
+                    console.log(`Second fragment has correct results`);
                 else
                     return reject();
 
