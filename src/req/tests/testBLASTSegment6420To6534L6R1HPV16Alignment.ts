205fb2f5358a21b70d60420ee7d02ff326f85c96
Chris Gibb <chris.gibb@outlook.com>
2018-06-27 14:34:05 -0400

    stop testing for fake duplicates

--- src/req/tests/testBLASTSegment6420To6534L6R1HPV16Alignment.ts
+++ src/req/tests/testBLASTSegment6420To6534L6R1HPV16Alignment.ts
@@ -20,7 +20,7 @@ export async function testBLASTSegment6420To6534L6R1HPV16Alignment() : Promise<v
             else if(op.flags.success)
             {
                 let readResults = await getBLASTReadResults(op.blastSegmentResult,0,0);
-                if(readResults.length == 2)
+                if(readResults.length == 1)
                     console.log(`BLAST segment has correct number of results in whole file`);
                 else
                     return reject();
@@ -66,50 +66,9 @@ export async function testBLASTSegment6420To6534L6R1HPV16Alignment() : Promise<v
                     return reject();
 
 
-                if(readResults[1].readWithFragments.read.SEQ == "TTAATAGGGCTGGTGCTGTTGGTGAAAATGTACCAGACGATTTATACATTAAAGGCTCTGGGTCTACTGCAAATTTAGCCAGTTCAAATTATTTTCCTACACCTAGTGAGATCGGAAGAGCACACGTCTGAACTCCAGTCACCGATGTATC")
-                    console.log(`Second read has correct sequence`);
-                else
-                    return reject();
-                
-                if(readResults[1].readWithFragments.read.POS == 6420)
-                    console.log(`Second read has correct start position`);
-                else
-                    return reject();
-                
-                if(!readResults[1].results.noHits)
-                    console.log(`Second read had hits`);
-                else
-                    return reject();
-                
-                if(readResults[1].readWithFragments.fragments.length == 2)
-                    console.log(`Second read has correct number of fragments`);
-                else
-                    return reject();
-                
-                if(readResults[1].readWithFragments.fragments[0].type == "mapped")
-                    console.log(`Second read, first fragment has correct type`);
-                else
-                    return reject();
-                
-                if(readResults[1].readWithFragments.fragments[0].seq == "TTAATAGGGCTGGTGCTGTTGGTGAAAATGTACCAGACGATTTATACATTAAAGGCTCTGGGTCTACTGCAAATTTAGCCAGTTCAAATTATTTTCCTACACCTAGTG")
-                    console.log(`Second read, first fragment has correct sequence`);
-                else
-                    return reject();
-                
-                if(readResults[1].readWithFragments.fragments[1].type == "unmapped")
-                    console.log(`Second read, second fragment has correct type`);
-                else
-                    return reject();
-                
-                if(readResults[1].readWithFragments.fragments[1].seq == "AGATCGGAAGAGCACACGTCTGAACTCCAGTCACCGATGTATC")
-                    console.log(`Second read, second fragment has correct sequence`);
-                else
-                    return reject();
-
-
                 let fragmentResults = await getBLASTFragmentResults(op.blastSegmentResult);
                 
-                if(fragmentResults.length == 2)
+                if(fragmentResults.length == 1)
                     console.log(`BLAST segment has correct number of fragments`);
                 else
                     return reject();
@@ -126,21 +85,6 @@ export async function testBLASTSegment6420To6534L6R1HPV16Alignment() : Promise<v
                 
                 if(fragmentResults[0].readuuid == readResults[0].uuid)
                     console.log(`First fragment is from first read`);
-                else
-                    return reject();
-                
-                if(fragmentResults[1].seq == "AGATCGGAAGAGCACACGTCTGAACTCCAGTCACCGATGTATC")
-                    console.log(`Second fragment has correct sequence`);
-                else
-                    return reject();
-                
-                if(!fragmentResults[1].results.noHits)
-                    console.log(`Second fragment had hits`);
-                else
-                    return reject();
-                
-                if(fragmentResults[1].readuuid == readResults[1].uuid)
-                    console.log(`Second fragment is from second read`);
                 else
                     return reject();
 
