5c77c30f5880d11fd501066206e6b4a161378fca
Chris Gibb <chris.gibb@outlook.com>
2018-10-19 09:41:43 -0400

    commented decorator

--- src/req/mangle.ts
+++ src/req/mangle.ts
@@ -1,3 +1,12 @@
+/**
+ * Mark a symbol as being mangleable. All reference to this symbol will be mangled in the output JS.
+ * The mangling is not type safe and no deeper analysis is performed. May therefore break unrelated code.
+ *
+ * @export
+ * @param {*} target
+ * @param {*} [key]
+ * @returns
+ */
 export function Mangle(target : any,key? : any)
 {
     return target;
