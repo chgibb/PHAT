3d7ebf924af7832975f1ada2b1f95b958cf06cb8
Chris Gibb <chris.gibb@outlook.com>
2018-10-26 11:20:48 -0400

    specify commonjs for unit test files

--- scripts/unitTest.bash
+++ scripts/unitTest.bash
@@ -1,10 +1,14 @@
  #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 
-./node_modules/.bin/tsc
+cd src/unitTests
+./../../node_modules/.bin/tsc
 if [ $? != 0 ]; then
     exit 1
 fi
+
+cd ../
+cd ../
 
 ./node_modules/.bin/jest
 if [ $? != 0 ]; then
