0f1e7b9596f6d39339b04a02c7a38a4ff1d35827
Chris Gibb <chris.gibb@outlook.com>
2019-06-10 17:59:57 -0400

    removed old code

--- src/req/renderer/inputRenderer/fastqView.tsx
+++ src/req/renderer/inputRenderer/fastqView.tsx
@@ -5,14 +5,7 @@ import * as pub from "./publish";
 import {FastqTable} from "../../components/fastqTable";
 import {Fastq} from "./../../fastq";
 import {inputFastqDialog} from "./inputFastqDialog";
-import {activeHover,activeHoverButton} from "./../styles/activeHover";
-import {selected} from "./../styles/selected";
-import {getReadable}  from './../../getAppPath';
-import {fullWidth} from './../styles/fullWidth';
-import {threeQuartersLoader} from "./../styles/threeQuartersLoader";
-import { tableIcons } from '../../components/tableIcons';
-import { tableCell } from '../styles/tableCell';
-import { Button } from '../../components/button';
+import {Button} from '../../components/button';
 
 export interface FastqViewProps
 {
@@ -50,60 +43,6 @@ export class FastqView extends Component<FastqViewProps,{}>
                 }
             </React.Fragment>
         );
-        /*return (
-            <div>
-                <img 
-                    className={`${activeHover} ${activeHoverButton}`} 
-                    src={getReadable("img/browseButton.png")}
-                    onClick={() => {
-                        inputFastqDialog();
-                    }}
-                />
-                <div className={fullWidth}>
-                    <table className={fullWidth}>
-                        <tbody>
-                            <tr>
-                                <th>Sample Name</th>
-                                <th>Path</th>
-                                <th>Size</th>
-                            </tr>
-                            {this.props.fastqInputs ? this.props.fastqInputs.map((val : Fastq,i : number) => {
-                                return (
-                                    <tr 
-                                        key={val.uuid}
-                                        className={`${activeHover} ${val.checked ? selected : ""}`}
-                                        onClick={(event : React.MouseEvent<HTMLTableRowElement,MouseEvent>) => {
-                                            event.persist();
-                                            
-                                            let target = event.target as HTMLTableCellElement;
-
-                                            if(target.classList.contains(`${val.uuid}Class`))
-                                            {
-                                                val.checked = !val.checked;
-                                                pub.publishFastqInputs(this.props.fastqInputs);
-                                            }
-                                        }}
-                                    >
-                                        <td className={`${val.uuid}Class`}>{val.alias}</td>
-                                        <td className={`${val.uuid}Class`}>{val.imported ? "In Project" : val.path}</td>
-                                        <td className={`${val.uuid}Class`}>{val.sizeString}</td>
-                                    </tr>
-                                )
-                            }) : undefined}
-                        </tbody>
-                    </table>
-                    {
-                        this.props.fastqInputs ? this.props.fastqInputs.length > 0 ?
-                        <img 
-                            className={`${activeHover} ${activeHoverButton}`}
-                            src={getReadable("img/import.png")}
-                            onClick={() => {
-                                pub.importSelectedFastqs(this.props.fastqInputs);
-                            }}
-                        /> : "" : ""
-                    }
-                </div>
-            </div>
-        );*/
     }
 }
+
