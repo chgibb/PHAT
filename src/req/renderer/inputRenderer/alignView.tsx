f24fc6272f0feec960f67030d198da3f77268420
Chris Gibb <chris.gibb@outlook.com>
2019-06-12 20:19:23 -0400

    sketched out align table

--- src/req/renderer/inputRenderer/alignView.tsx
+++ src/req/renderer/inputRenderer/alignView.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import * as pub from "./publish";
 import { AlignData } from '../../alignData';
 import { Button } from '../components/button';
+import { inputAlignDialog } from './inputAlignDialog';
+import { Table } from '../components/table';
 
 export interface AlignViewProps
 {
@@ -21,7 +23,32 @@ export class AlignView extends React.Component<AlignViewProps,{}>
         return (
             <React.Fragment>
                 <Button
+                    onClick={() => {
+                        inputAlignDialog();
+                    }}
                     label="Browse"
+                />
+                <Table<AlignData>
+                    title=""
+                    toolbar={false}
+                    columns={[
+                        {
+                            title : "File Name",
+                            field : "alias"
+                        },
+                        {
+                            title : "Size",
+                            field : "sizeString"
+                        },
+                        {
+                            title : "Ref Seq",
+                            field : "",
+                            render : (row : AlignData) => {
+                                return row.fasta ? row.fasta.alias : "Not Linked"
+                            }
+                        }
+                    ]}
+                    data={this.props.aligns}
                 />
             </React.Fragment>
         )
