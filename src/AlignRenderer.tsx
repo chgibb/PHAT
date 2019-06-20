511f87958769e8b9abc22bf896b789c94f9e9c45
chgibb <chris.gibb@outlook.com>
2019-06-19 22:28:28 -0400

    initial mui rewrite

---
+++ src/AlignRenderer.tsx
@@ -0,0 +1,72 @@
+import * as electron from "electron";
+const ipc = electron.ipcRenderer;
+import * as React from "react";
+import { render } from "react-dom";
+
+import { GetKeyEvent, KeySubEvent } from "./req/ipcEvents";
+import { makeWindowDockable } from "./req/renderer/dock";
+import "./req/renderer/commonBehaviour";
+import "./req/renderer/styles/defaults";
+import { AlignRendererApp } from './req/renderer/AlignRenderer/app';
+
+render(
+    <AlignRendererApp />,
+    document.getElementById("app")
+);
+
+makeWindowDockable("align");
+
+ipc.send(
+    "getKey",
+    {
+        action: "getKey",
+        channel: "input",
+        key: "fastqInputs",
+        replyChannel: "align"
+    } as GetKeyEvent
+);
+ipc.send(
+    "getKey",
+    {
+        action: "getKey",
+        channel: "input",
+        key: "fastaInputs",
+        replyChannel: "align"
+    } as GetKeyEvent
+);
+ipc.send(
+    "getKey",
+    {
+        action: "getKey",
+        channel: "application",
+        key: "operations",
+        replyChannel: "align"
+    } as GetKeyEvent
+);
+ipc.send(
+    "keySub",
+    {
+        action: "keySub",
+        channel: "input",
+        key: "fastqInputs",
+        replyChannel: "align"
+    } as KeySubEvent
+);
+ipc.send(
+    "keySub",
+    {
+        action: "keySub",
+        channel: "input",
+        key: "fastaInputs",
+        replyChannel: "align"
+    } as KeySubEvent
+);
+ipc.send(
+    "keySub",
+    {
+        action: "keySub",
+        channel: "application",
+        key: "operations",
+        replyChannel: "align"
+    } as KeySubEvent
+);
---
+++ src/req/renderer/AlignRenderer/app.tsx
@@ -0,0 +1,86 @@
+import * as electron from "electron";
+const ipc = electron.ipcRenderer;
+import * as React from "react";
+import {Component} from "react";
+
+import { Fastq } from '../../fastq';
+import { Fasta } from '../../fasta';
+import { AtomicOperation } from '../../operations/atomicOperations';
+
+export interface AlignRendererAppState
+{
+    fastqs? : Array<Fastq>;
+    fastas? : Array<Fasta>;
+    shouldAllowTriggeringOps : boolean;
+}
+
+export class AlignRendererApp extends Component<{},AlignRendererAppState>
+{
+    public state : AlignRendererAppState;
+    public constructor()
+    {
+        super(undefined);
+        this.state = {
+
+        } as AlignRendererAppState;
+
+        ipc.on
+        (
+            "align",(event : Electron.IpcMessageEvent,arg : any) =>
+            {
+                if(arg.action == "getKey" || arg.action == "keyChange")
+                {
+                    if(arg.key == "fastqInputs")
+                    {
+                        if(arg.val !== undefined)
+                        {
+                            this.setState({fastqs: arg.val});
+                        }
+                    }
+                    if(arg.key == "fastaInputs")
+                    {
+                        if(arg.val !== undefined)
+                        {
+                            this.setState({fastas: arg.val});
+                        }
+                    }
+                    let found = false;
+                    if(arg.key == "operations")
+                    {
+                        if(arg.val !== undefined)
+                        {
+                            let ops : Array<AtomicOperation> = arg.val;
+                            try
+                            {
+                                for(let i = 0; i != ops.length; ++i)
+                                {
+                                    if(ops[i].name == "indexFastaForBowtie2" || ops[i].name == "runBowtie2Alignment" || ops[i].name == "indexFastaForHisat2" || ops[i].name == "runHisat2Alignment")
+                                    {
+                                        this.setState({
+                                            shouldAllowTriggeringOps: false
+                                        });
+                                    }
+                                }
+                            }
+                            catch(err)
+                            {}
+                        }
+                    }
+                    if(!found)
+                    {
+                        this.setState({
+                            shouldAllowTriggeringOps: true
+                        });
+                    }
+                }
+            }
+        );
+    }
+
+    public render() : JSX.Element
+    {
+        return (
+            <div></div>
+        );
+    }
+}
