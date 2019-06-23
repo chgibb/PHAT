66089822b0a205a070fca65e5cc3f156fac81968
chgibb <chris.gibb@outlook.com>
2019-06-23 10:11:25 -0400

    generic array reorder function

---
+++ src/req/reOrder.ts
@@ -0,0 +1,9 @@
+export function reOrder<T>(list : Array<T>, startIndex : number, endIndex : number) : Array<T>
+ {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+  
+    return result;
+  };
+  
