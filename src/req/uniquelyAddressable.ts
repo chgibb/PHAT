3d40a5c9a4ce8e62c5076ec70f3e8d83e7a32683
Chris Gibb <chris.gibb@outlook.com>
2019-06-24 19:11:05 -0400

    added doc comments

--- src/req/uniquelyAddressable.ts
+++ src/req/uniquelyAddressable.ts
@@ -3,6 +3,15 @@ export interface UniquelyAddressable
     uuid : string;
 }
 
+/**
+ * Retrieve a single object reference from a collection of objects, given a unique handle
+ *
+ * @export
+ * @template T
+ * @param {(Array<T> | undefined)} arr
+ * @param {(string | undefined)} uuid
+ * @returns {(T | undefined)}
+ */
 export function getReferenceFromUuid<T extends UniquelyAddressable>(
     arr : Array<T> | undefined,
     uuid : string | undefined
@@ -22,6 +31,16 @@ export function getReferenceFromUuid<T extends UniquelyAddressable>(
     return undefined;
 }
 
+
+/**
+ * Retrieve a collection of object references from a collection of objects given a collection of unique handles
+ *
+ * @export
+ * @template T
+ * @param {(Array<T> | undefined)} arr
+ * @param {(Array<string> | undefined)} uuids
+ * @returns {(Array<T> | undefined)}
+ */
 export function getReferencesFromUuids<T extends UniquelyAddressable>(
     arr : Array<T> | undefined,
     uuids : Array<string> | undefined
@@ -47,6 +66,18 @@ export function getReferencesFromUuids<T extends UniquelyAddressable>(
     return undefined;
 }
 
+
+/**
+ * Retrive a collection of object properties from a collection of objects given a collection of their uniquq handles
+ *
+ * @export
+ * @template T
+ * @template K
+ * @param {Array<T>} arr
+ * @param {Array<string>} uuids
+ * @param {K} prop
+ * @returns {(Array<T[K]> | undefined)}
+ */
 export function getPropertiesOfReferencesFromUuids<T extends UniquelyAddressable,K extends keyof T>(
     arr : Array<T>,
     uuids : Array<string>,
