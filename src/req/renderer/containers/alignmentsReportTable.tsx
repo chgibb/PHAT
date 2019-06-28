d4274379d6e3d8c59ebdb3d3fa1c949b75df5bd5
Chris Gibb <chris.gibb@outlook.com>
2019-06-28 18:17:33 -0400

    override mui styles

--- src/req/renderer/containers/alignmentsReportTable.tsx
+++ src/req/renderer/containers/alignmentsReportTable.tsx
@@ -4,126 +4,162 @@ import { Table } from '../components/table';
 import { sweepToBottom } from '../styles/sweepToBottom';
 
 
-export interface AlignmentsReportTable
-{
-    aligns? : Array<AlignData>;
+export interface AlignmentsReportTableProps {
+    aligns?: Array<AlignData>;
 }
 
-export function AlignmentsReportTable(props : AlignmentsReportTable) : JSX.Element
+export class AlignmentsReportTable extends React.Component<AlignmentsReportTableProps, {}>
 {
-    return (
-        <Table<AlignData>
-            toolbar={true}
-            title="Alignment Reports"
-            data={props.aligns}
-            columns={[
+    public ref = React.createRef<HTMLDivElement>();
+    private cellHoverClass = "cellHover";
+    public constructor(props: AlignmentsReportTableProps) {
+        super(props);
+    }
+
+    public updateStyles() : void
+    {
+        /*
+            Mui-table will only register row clicks when rowHover is set. This also forces the cursor to be a pointer on the entire row.
+            We want to enable cell-clicks, not row clicks. It is currently impossible to style entirety of specific table cells.
+        */
+
+        if(this.ref.current)
+        {
+            let tableRows : HTMLCollectionOf<HTMLTableRowElement> = this.ref.current.getElementsByTagName("tr");
+
+            //disable pointer cursor on rows
+            for(let i = 0; i != tableRows.length; ++i)
+            {
+                if(tableRows[i].style.cursor == "pointer")
                 {
-                    title : "Alias",
-                    render : (row : AlignData) =>
+                    tableRows[i].style.cursor = "inherit";
+                }
+            }
+
+            let selectableCells : HTMLCollectionOf<Element> = this.ref.current.getElementsByClassName(this.cellHoverClass);
+
+            //any cell with the special cellHoveClass applied will have it's parent styled as hoverable
+            for(let i = 0; i != selectableCells.length; ++i)
+            {
+                selectableCells[i].parentElement.classList.add(sweepToBottom);
+                selectableCells[i].parentElement.style.cursor = "pointer";
+            }
+
+        }
+    }
+
+    public componentDidMount() : void
+    {
+        this.updateStyles();
+    }
+
+    public componentDidUpdate() : void
+    {
+        this.updateStyles();
+    }
+
+    public render(): JSX.Element {
+        return (
+            <div ref={this.ref}>
+            <Table<AlignData>
+                toolbar={true}
+                title="Alignment Reports"
+                data={this.props.aligns}
+                columns={[
                     {
-                        return row.alias;
-                    }
-                },
-                {
-                    title : "Aligner",
-                    render : (row : AlignData) =>
+                        title: "Alias",
+                        render: (row: AlignData) => {
+                            return row.alias;
+                        }
+                    },
                     {
-                        return row.alignerUsed;
-                    }
-                }, 
-                {
-                    title : "Size In Bytes",
-                    render : (row : AlignData) =>
+                        title: "Aligner",
+                        render: (row: AlignData) => {
+                            return row.alignerUsed;
+                        }
+                    },
                     {
-                        return row.size;
-                    }
-                }, 
-                {
-                    title : "Formatted Size",
-                    render : (row : AlignData) =>
+                        title: "Size In Bytes",
+                        render: (row: AlignData) => {
+                            return row.size;
+                        }
+                    },
                     {
-                        return row.sizeString;
-                    }
-                }, 
-                {
-                    title : "Reads",
-                    render : (row : AlignData) =>
+                        title: "Formatted Size",
+                        render: (row: AlignData) => {
+                            return row.sizeString;
+                        }
+                    },
                     {
-                        return !row.isExternalAlignment ? row.summary.reads : row.flagStatReport.reads;
-                    }
-                }, 
-                {
-                    title : "Mates",
-                    render : (row : AlignData) =>
+                        title: "Reads",
+                        render: (row: AlignData) => {
+                            return !row.isExternalAlignment ? row.summary.reads : row.flagStatReport.reads;
+                        }
+                    },
                     {
-                        return !row.isExternalAlignment ? row.summary.mates : "Unknown";
-                    }
-                }, 
-                {
-                    title : "Overall Alignment Rate %",
-                    render : (row : AlignData) =>
+                        title: "Mates",
+                        render: (row: AlignData) => {
+                            return !row.isExternalAlignment ? row.summary.mates : "Unknown";
+                        }
+                    },
                     {
-                        return !row.isExternalAlignment ? row.summary.overallAlignmentRate : row.flagStatReport.overallAlignmentRate;
-                    }
-                }, 
-                {
-                    title : "Minimum Coverage",
-                    render : (row : AlignData) =>
+                        title: "Overall Alignment Rate %",
+                        render: (row: AlignData) => {
+                            return !row.isExternalAlignment ? row.summary.overallAlignmentRate : row.flagStatReport.overallAlignmentRate;
+                        }
+                    },
                     {
-                        return row.varScanSNPSummary ? row.varScanSNPSummary.minCoverage : "Unknown";
-                    }
-                }, 
-                {
-                    title : "Minimum Variable Frequency",
-                    render : (row : AlignData) =>
+                        title: "Minimum Coverage",
+                        render: (row: AlignData) => {
+                            return row.varScanSNPSummary ? row.varScanSNPSummary.minCoverage : "Unknown";
+                        }
+                    },
                     {
-                        return row.varScanSNPSummary ? row.varScanSNPSummary.minVarFreq : "Unknown";
-                    }
-                }, 
-                {
-                    title : "Minimum Average Quality",
-                    render : (row : AlignData) =>
+                        title: "Minimum Variable Frequency",
+                        render: (row: AlignData) => {
+                            return row.varScanSNPSummary ? row.varScanSNPSummary.minVarFreq : "Unknown";
+                        }
+                    },
                     {
-                        return row.varScanSNPSummary ? row.varScanSNPSummary.minAvgQual : "Unknown";
-                    }
-                }, 
-                {
-                    title : "P-Value Threshold",
-                    render : (row : AlignData) =>
+                        title: "Minimum Average Quality",
+                        render: (row: AlignData) => {
+                            return row.varScanSNPSummary ? row.varScanSNPSummary.minAvgQual : "Unknown";
+                        }
+                    },
                     {
-                        return row.varScanSNPSummary ? row.varScanSNPSummary.pValueThresh : "Unknown";
-                    }
-                }, 
-                {
-                    title : "SNPs Predicted",
-                    cellStyle : sweepToBottom as any,
-                    render : (row : AlignData) =>
+                        title: "P-Value Threshold",
+                        render: (row: AlignData) => {
+                            return row.varScanSNPSummary ? row.varScanSNPSummary.pValueThresh : "Unknown";
+                        }
+                    },
                     {
-                        return row.varScanSNPSummary ? row.varScanSNPSummary.SNPsReported : "Unknown";
-                    }
-                }, 
-                {
-                    title : "Indels Predicted",
-                    render : (row : AlignData) =>
+                        title: "SNPs Predicted",
+                        cellStyle: sweepToBottom as any,
+                        render: (row: AlignData) => {
+                            return row.varScanSNPSummary ? (<div className={this.cellHoverClass}>{row.varScanSNPSummary.SNPsReported}</div>) : "Unknown";
+                        }
+                    },
                     {
-                        return row.varScanSNPSummary ? row.varScanSNPSummary.indelsReported : "Unknown";
-                    }
-                },  
-                {
-                    title : "BLAST Runs",
-                    render : (row : AlignData) =>
+                        title: "Indels Predicted",
+                        render: (row: AlignData) => {
+                            return row.varScanSNPSummary ? row.varScanSNPSummary.indelsReported : "Unknown";
+                        }
+                    },
                     {
-                        return row.BLASTSegmentResults ? row.BLASTSegmentResults.length : 0;
-                    }
-                }, 
-                {
-                    title : "Date Ran",
-                    render : (row : AlignData) =>
+                        title: "BLAST Runs",
+                        render: (row: AlignData) => {
+                            return row.BLASTSegmentResults ? row.BLASTSegmentResults.length : 0;
+                        }
+                    },
                     {
-                        return row.dateStampString;
-                    }
-                },
-            ]}
-        />
-    );
+                        title: "Date Ran",
+                        render: (row: AlignData) => {
+                            return row.dateStampString;
+                        }
+                    },
+                ]}
+            />
+            </div>
+        );
+    }
 }
