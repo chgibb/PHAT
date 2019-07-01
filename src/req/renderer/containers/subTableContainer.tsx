bf52e7b133665b0b6a77cac3b88395e4e648bc84
Chris Gibb <chris.gibb@outlook.com>
2019-07-01 15:46:08 -0400

    wrapper for nested table

---
+++ src/req/renderer/containers/subTableContainer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import { Paper } from '../components/paper';
+
+export function SubTableContainer(props : any) : JSX.Element
+{
+    return (
+        <Paper {...props} style={{maxWidth: "100%",display: "grid"}} />
+    );
+}
