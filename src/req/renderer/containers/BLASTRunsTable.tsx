05dfb36826de5ea66d7070dd933aefa5fa03c22e
Chris Gibb <chris.gibb@outlook.com>
2019-07-04 22:47:31 -0400

    started blast runs table

---
+++ src/req/renderer/containers/BLASTRunsTable.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { AlignData } from '../../alignData';
+import { BLASTSegmentResult } from '../../BLASTSegmentResult';
+import { Table } from '../components/table';
+
+export interface BLASTRunsTableProps
+{
+    align : AlignData;
+    isSubTable? : boolean;
+}
+
+export function BLASTRunsTable(props : BLASTRunsTableProps) : JSX.Element
+{
+    return (
+        <Table<BLASTSegmentResult>
+            toolbar={false}
+            title=""
+            data={props.align.BLASTSegmentResults}
+            columns={[
+                {
+                    title : "Start",
+                    render : (row) => {
+                        return row.start;
+                    }
+                },
+                {
+                    title
+                }
+            ]}
+        />
+    );
+}
