aa381be5037e7eb3af6d16677aa4e4e9561a91c2
chgibb <chris.gibb@outlook.com>
2019-08-16 22:53:07 -0400

    set cache locations seperately

--- scripts/fix.bash
+++ scripts/fix.bash
@@ -1,3 +1,3 @@
-./node_modules/.bin/eslint "src/**/*.ts" --cache --fix
+./node_modules/.bin/eslint "src/**/*.tsx" --fix --cache-location ".eslintcache-tsx"
 
-./node_modules/.bin/eslint "src/**/*.tsx" --cache --fix
+./node_modules/.bin/eslint "src/**/*.ts" --fix --cache-location ".eslintcache-ts"
