a7e2e5bba1f8c5dce5b8c59783701c54f478b2a3
chgibb <chris.gibb@outlook.com>
2019-08-19 18:12:17 -0400

    added start button and fixed spacing

--- src/req/renderer/containers/forms/BLASTRunForm/step2.tsx
+++ src/req/renderer/containers/forms/BLASTRunForm/step2.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 
-import {BLASTRunForm} from "../BLASTRunForm";
-import {FullWidthFormStep} from "../../fullWidthStepperForm";
-import {GridWrapper} from "../../gridWrapper";
-import {Grid} from "../../../components/grid";
-import {Paper} from "../../../components/paper";
-import {Typography} from "../../../components/typography";
+import { BLASTRunForm } from "../BLASTRunForm";
+import { FullWidthFormStep } from "../../fullWidthStepperForm";
+import { GridWrapper } from "../../gridWrapper";
+import { Grid } from "../../../components/grid";
+import { Paper } from "../../../components/paper";
+import { Typography } from "../../../components/typography";
+import { Button } from '../../../components/button';
 
-export function step2(form: BLASTRunForm): FullWidthFormStep 
-{
+export function step2(form: BLASTRunForm): FullWidthFormStep {
     let label = "Identify Reads and Fragments to BLAST";
 
     return {
@@ -20,28 +20,41 @@ export function step2(form: BLASTRunForm): FullWidthFormStep
                         <Grid item>
                             <Paper>
                                 <GridWrapper>
-                                    <div style={{marginTop:"1vh"}}>
-                                    <Grid container spacing={4} justify="center">
-                                        <Grid item>
-                                            <Typography>
-                                                Scanned {form.state.readsScanned ? form.state.readsScanned : 0} reads
+                                    <div style={{ marginTop: "1vh" }}>
+                                        <Grid container spacing={4} justify="center">
+                                            <Grid item>
+                                                <Typography>
+                                                    Scanned {form.state.readsScanned ? form.state.readsScanned : 0} reads
                                             </Typography>
+                                            </Grid>
                                         </Grid>
-                                    </Grid>
                                     </div>
                                 </GridWrapper>
                                 {form.state.readsWithFragments ?
-                                    <GridWrapper>
-                                        <div style={{marginRight:"1vh",marginLeft:"1vh",marginBottom:"1vh"}}>
-                                        <Grid container spacing={4} justify="center">
-                                            <Grid item>
-                                                <Typography variant="h5" component="h3">
-                                                    {form.state.readsWithFragments.length} potential BLAST candidates
+                                    <React.Fragment>
+                                        <GridWrapper>
+                                            <div style={{ marginRight: "1vh", marginLeft: "1vh", marginBottom: "1vh" }}>
+                                                <Grid container spacing={4} justify="center">
+                                                    <Grid item>
+                                                        <Typography variant="h5" component="h3">
+                                                            {form.state.readsWithFragments.length} potential BLAST candidates
                                                 </Typography>
+                                                    </Grid>
+                                                </Grid>
+                                            </div>
+                                        </GridWrapper>
+                                        <GridWrapper>
+                                            <Grid container spacing={4} justify="center">
+                                                <Grid item>
+                                                    <Button
+                                                        type="remain"
+                                                        label="Start BLASTing"
+                                                    />
+                                                </Grid>
                                             </Grid>
-                                        </Grid>
-                                        </div>
-                                    </GridWrapper> : ""}
+                                        </GridWrapper>
+                                    </React.Fragment>
+                                    : ""}
                             </Paper>
                         </Grid>
                     </Grid>
