f9c432d607f4feade74f9a9ae940d73e34af0eaa
chgibb <chris.gibb@outlook.com>
2019-08-29 18:12:13 -0400

    converted to class

--- src/req/renderer/components/outlinedInput.tsx
+++ src/req/renderer/components/outlinedInput.tsx
@@ -7,36 +7,79 @@ const MuiOutlinedInput: typeof import("@material-ui/core/OutlinedInput").default
 
 type MuiOutlinedInputProps = import("@material-ui/core/OutlinedInput/index").OutlinedInputProps;
 
+export interface OutlinedInputState
+{
+    labelWidth : number;
+}
+
 export interface OutlinedInputProps {
     label: string;
     inputProps: Omit<MuiOutlinedInputProps, "labelWidth">;
 }
 
-export function OutlinedInput(props: OutlinedInputProps): JSX.Element 
+export class OutlinedInput extends React.Component<OutlinedInputProps,OutlinedInputState>
 {
-    const [labelWidth, setLabelWidth] = React.useState(0);
-    const labelRef = React.useRef<HTMLLabelElement>(null);
-
-    React.useEffect(() => 
+    private labelRef = React.createRef<HTMLLabelElement>();
+    public constructor(props : OutlinedInputProps)
     {
-        setLabelWidth(labelRef.current!.offsetWidth);
-    }, []);
+        super(props);
 
+        this.state = {
+            labelWidth : 0
+        };
+    }
+
+    public updateLabelWidth()
+    {
+        if(this.labelRef.current)
+        {
+            this.setState({
+                labelWidth : this.labelRef.current.offsetWidth
+            });
+        }
+    }
+
+    public componentDidMount()
+    {
+        this.updateLabelWidth();
+    }
+
+    public componentDidUpdate(prevProps : Readonly<OutlinedInputProps>)
+    {
+        if(prevProps.label != this.props.label)
+            this.updateLabelWidth();
+    }
+
+    public componentWillReceiveProps()
+    {
+        this.updateLabelWidth();
+    }
+
+    public render() : JSX.Element
+    {
     return (
         <div style={{
             display: "flex",
             flexWrap: "wrap"
         }}>
             <FormControl variant="outlined">
-                <InputLabel ref={labelRef}>
-                    {props.label}
+                <InputLabel ref={this.labelRef}>
+                    {this.props.label}
                 </InputLabel>
                 <MuiOutlinedInput
-                    {...props.inputProps}
-
-                    labelWidth={labelWidth}
+                    {...this.props.inputProps}
+                    onChange={(event) => {
+                        this.updateLabelWidth();
+                        if(this.props.inputProps.onChange)
+                            this.props.inputProps.onChange(event);
+                    }}
+                    labelWidth={this.state.labelWidth}
                 />
             </FormControl>
         </div>
     );
+
+    }
+
+    
 }
