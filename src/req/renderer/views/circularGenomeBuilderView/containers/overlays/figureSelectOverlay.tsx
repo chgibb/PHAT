c8ea20dcc33b5f013ba88e7cbf5bb7e16639a864
chgibb <chris.gibb@outlook.com>
2019-09-02 18:00:31 -0400

    lint

--- src/req/renderer/views/circularGenomeBuilderView/containers/overlays/figureSelectOverlay.tsx
+++ src/req/renderer/views/circularGenomeBuilderView/containers/overlays/figureSelectOverlay.tsx
@@ -1,18 +1,16 @@
 import * as React from "react";
 
+import {TreeView} from "../../../../components/treeView";
+import {TreeItem} from "../../../../components/treeItem";
 import {CircularGenomeBuilderView} from "../../circularGenomeBuilderView";
 import {GridWrapper} from "../../../../containers/gridWrapper";
 import {Grid} from "../../../../components/grid";
 import {Typography} from "../../../../components/typography";
-import {IconButton} from "../../../../components/iconButton";
-import {AddBox} from "../../../../components/icons/addBox";
+import {ChevronRight} from "../../../../components/icons/chevronRight";
+import {blue} from "../../../../styles/colours";
+import {ExpandMore} from "../../../../components/icons/expandMore";
 
 import {Overlay} from "./overlay";
-import TreeView from '@material-ui/lab/TreeView';
-import TreeItem from '@material-ui/lab/TreeItem';
-import { ChevronRight } from '../../../../components/icons/chevronRight';
-import { blue } from '../../../../styles/colours';
-import { ExpandMore } from '../../../../components/icons/expandMore';
 
 export interface FigureSelectOverlayProps {
     builder: CircularGenomeBuilderView;
@@ -44,32 +42,32 @@ export function FigureSelectOverlay(props: FigureSelectOverlayProps): JSX.Elemen
                     {
                         return (
                             <GridWrapper>
-                                    <div style={{marginLeft: "1vh", marginBottom: "1vh"}}>
-                                        <Grid container direction="row" spacing={1} justify="center">
-                                            <Grid item>
-                            <TreeView
-                                defaultExpandIcon={<ChevronRight color="primary" classes={{colorPrimary:blue}} />}
-                                defaultCollapseIcon={<ExpandMore color="primary" classes={{colorPrimary:blue}} />}
-                            >
-                                <TreeItem nodeId={`${fastaIndex}`} label={fasta.alias}>
-                                {
-                                    !props.builder.props.figures.find((x) => fasta.uuid == x.uuidFasta) ? <TreeItem nodeId={`${fastaIndex}-999`} label={"No Figures For This Reference"} /> : 
-                                    props.builder.props.figures.map((figure,figureIndex) => 
-                                    {
-                                        if (fasta.uuid == figure.uuidFasta) 
-                                        {
-                                            return (
-                                                <TreeItem nodeId={`${fastaIndex}-${figureIndex}`} label={figure.name} />
-                                            );
-                                        }
-                                        return null;
-                                    })
-                                }
-                                </TreeItem>
-                            </TreeView>
-                            </Grid>
-                            </Grid>
-                            </div>
+                                <div style={{marginLeft: "1vh", marginBottom: "1vh"}}>
+                                    <Grid container direction="row" spacing={1} justify="center">
+                                        <Grid item>
+                                            <TreeView
+                                                defaultExpandIcon={<ChevronRight color="primary" classes={{colorPrimary:blue}} />}
+                                                defaultCollapseIcon={<ExpandMore color="primary" classes={{colorPrimary:blue}} />}
+                                            >
+                                                <TreeItem nodeId={`${fastaIndex}`} label={fasta.alias}>
+                                                    {
+                                                        !props.builder.props.figures.find((x) => fasta.uuid == x.uuidFasta) ? <TreeItem nodeId={`${fastaIndex}-999`} label={"No Figures For This Reference"} /> : 
+                                                            props.builder.props.figures.map((figure,figureIndex) => 
+                                                            {
+                                                                if (fasta.uuid == figure.uuidFasta) 
+                                                                {
+                                                                    return (
+                                                                        <TreeItem nodeId={`${fastaIndex}-${figureIndex}`} label={figure.name} />
+                                                                    );
+                                                                }
+                                                                return null;
+                                                            })
+                                                    }
+                                                </TreeItem>
+                                            </TreeView>
+                                        </Grid>
+                                    </Grid>
+                                </div>
                             </GridWrapper>
                         );
                     })
