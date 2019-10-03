eb3ca2e10bf02c7c895f1dc32f6fd6a760076623
chgibb <chris.gibb@outlook.com>
2019-10-03 18:16:03 -0400

    lint

--- src/req/renderer/containers/forms/newCoverageTrackForm/newCoverageTrackForm.tsx
+++ src/req/renderer/containers/forms/newCoverageTrackForm/newCoverageTrackForm.tsx
@@ -6,7 +6,7 @@ import {Form, FullWidthStepperForm} from "../../fullWidthStepperForm";
 
 import {step1} from "./step1";
 import {step2} from "./step2";
-import { step3 } from './step3';
+import {step3} from "./step3";
 
 export interface NewCoverageTrackFormProps
 {
--- src/req/renderer/containers/forms/newCoverageTrackForm/step3.tsx
+++ src/req/renderer/containers/forms/newCoverageTrackForm/step3.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
+import {style} from "typestyle";
 
-import { NewCoverageTrackForm } from './newCoverageTrackForm';
-import { FullWidthFormStep } from '../../fullWidthStepperForm';
-import { GridWrapper } from '../../gridWrapper';
-import { Grid } from '../../../components/grid';
-import { Typography } from '../../../components/typography';
-import { ColorLensRounded } from '../../../components/icons/colorLensRounded';
-import { style } from 'typestyle';
+import {FullWidthFormStep} from "../../fullWidthStepperForm";
+import {GridWrapper} from "../../gridWrapper";
+import {Grid} from "../../../components/grid";
+import {Typography} from "../../../components/typography";
+import {ColorLensRounded} from "../../../components/icons/colorLensRounded";
+
+import {NewCoverageTrackForm} from "./newCoverageTrackForm";
+
 
 export function step3(this : NewCoverageTrackForm) : FullWidthFormStep
 {
@@ -18,15 +20,15 @@ export function step3(this : NewCoverageTrackForm) : FullWidthFormStep
                     <Grid container spacing={4} justify="center">
                         <Grid item>
                           
-                                <Typography>{this.state.log10Scale ? "Log10 Scale, " : ""}scale by {this.state.scaleFactor}</Typography>
-                                </Grid>
-                                <Grid item>
-                                <ColorLensRounded
+                            <Typography>{this.state.log10Scale ? "Log10 Scale, " : ""}scale by {this.state.scaleFactor}</Typography>
+                        </Grid>
+                        <Grid item>
+                            <ColorLensRounded
                                 color="primary"
                                 classes={{colorPrimary: style({color: this.state.color})}}
-                                /></Grid>
+                            /></Grid>
                             
-                        </Grid>
+                    </Grid>
                     
                 </GridWrapper>
             </div>
