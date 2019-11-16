e5cc7dc4d8543079cebc089e80819f66567ec29c
chgibb <chris.gibb@outlook.com>
2019-11-16 14:10:07 -0500

    lint

--- src/req/renderer/containers/circularGenome/circularGenome.tsx
+++ src/req/renderer/containers/circularGenome/circularGenome.tsx
@@ -113,13 +113,17 @@ export class CircularGenome extends React.Component<CircularGenomeProps,Circular
         return undefined;
     }
 
-    public async updateCanvas() {
-        for (let i = 0; i != this.props.figure.visibleLayers.length; ++i) {
+    public async updateCanvas() 
+    {
+        for (let i = 0; i != this.props.figure.visibleLayers.length; ++i) 
+        {
             let layer = this.props.figure.visibleLayers[i];
-            if (this.ref.current) {
+            if (this.ref.current) 
+            {
                 let canvasArr = this.ref.current.getElementsByTagName("canvas");
 
-                while (canvasArr.length < this.props.figure.visibleLayers.length) {
+                while (canvasArr.length < this.props.figure.visibleLayers.length) 
+                {
                     this.ref.current.appendChild(document.createElement("canvas"));
                 }
 
@@ -127,20 +131,24 @@ export class CircularGenome extends React.Component<CircularGenomeProps,Circular
 
                 let canvas = canvasArr[i];
 
-                let plasmid = this.plasmidCache.find((x) => {
+                let plasmid = this.plasmidCache.find((x) => 
+                {
                     if (x.uuid == layer)
                         return true;
                     return false;
                 });
 
-                if (!plasmid) {
+                if (!plasmid) 
+                {
                     plasmid = await this.loadPlasmid(layer);
                 }
 
-                if (plasmid && canvas) {
+                if (plasmid && canvas) 
+                {
                     let ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
 
-                    if (ctx) {
+                    if (ctx) 
+                    {
                         canvas.style.position = "absolute";
                         canvas.setAttribute("width", `${this.props.width}`);
                         canvas.setAttribute("height", `${this.props.height}`);
@@ -149,7 +157,7 @@ export class CircularGenome extends React.Component<CircularGenomeProps,Circular
 
                         ctx.clearRect(0, 0, this.props.width, this.props.height);
 
-                        let scope = { genome: this.props.figure };
+                        let scope = {genome: this.props.figure};
                         plasmid.plasmid.$scope = scope;
 
                         await renderSVGToCanvas(plasmid.plasmid.renderStart() + plasmid.plasmid.renderEnd(), ctx);
@@ -166,49 +174,48 @@ export class CircularGenome extends React.Component<CircularGenomeProps,Circular
 
     public shouldComponentUpdate(prevProps : Readonly<CircularGenomeProps>,prevState : Readonly<CircularGenomeState>) : boolean
     {
-            return true;
+        return true;
     }
 
     public async componentDidUpdate(prevProps : Readonly<CircularGenomeProps>,prevState : Readonly<CircularGenomeState>)
     {
-       // this.updateCanvas();
-
         let shouldUpdateCanvasDueToResize = false;
         
-            if(this.ref.current)
+        if(this.ref.current)
+        {
+            let canvasArr = this.ref.current.getElementsByTagName("canvas");
+
+            for(let i = 0; i != canvasArr.length; ++i)
             {
-                let canvasArr = this.ref.current.getElementsByTagName("canvas");
+                let canvas = canvasArr[i];
 
-                for(let i = 0; i != canvasArr.length; ++i)
+                canvas.style.position = "absolute";
+
+                if(prevProps.width != this.props.width)
                 {
-                    let canvas = canvasArr[i];
-
-                    canvas.style.position = "absolute";
-
-                    if(prevProps.width != this.props.width)
-                    {
-                        shouldUpdateCanvasDueToResize = true;
-                        canvas.setAttribute("width",`${this.props.width}`);
-                    }
-
-                    if(prevProps.height != this.props.height)    
-                    {
-                        shouldUpdateCanvasDueToResize = true;
-                        canvas.setAttribute("height",`${this.props.height}`);
-                    }
-
-                    if(prevProps.x != this.props.x)
-                        canvas.style.left = `${this.props.x}px`;
-
-                    if(prevProps.y != this.props.y)
-                        canvas.style.top = `${this.props.y}px`;
-                }
-            }
-
-                if(this.props.shouldUpateCanvas)
                     shouldUpdateCanvasDueToResize = true;
+                    canvas.setAttribute("width",`${this.props.width}`);
+                }
 
-        if(shouldUpdateCanvasDueToResize || this.props.shouldUpateCanvas){
+                if(prevProps.height != this.props.height)    
+                {
+                    shouldUpdateCanvasDueToResize = true;
+                    canvas.setAttribute("height",`${this.props.height}`);
+                }
+
+                if(prevProps.x != this.props.x)
+                    canvas.style.left = `${this.props.x}px`;
+
+                if(prevProps.y != this.props.y)
+                    canvas.style.top = `${this.props.y}px`;
+            }
+        }
+
+        if(this.props.shouldUpateCanvas)
+            shouldUpdateCanvasDueToResize = true;
+
+        if(shouldUpdateCanvasDueToResize || this.props.shouldUpateCanvas)
+        {
             await this.updateCanvas();
         }
         
